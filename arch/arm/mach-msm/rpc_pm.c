725ec2e2f78d6ee0a81b73a8f65119ead3ed5efb
San Mehat <san@android.com>
2007-12-11 10:36:35 -0800

    msm: Initial support for voltage regulator RPC request driver Signed-off-by: San Mehat <san@android.com>

---
+++ arch/arm/mach-msm/rpc_pm.c
@@ -0,0 +1,100 @@
+/* arch/arm/mach-msm/rpc_pmapp.c
+**
+** Copyright (C) 2007 Google, Inc.
+**
+** This software is licensed under the terms of the GNU General Public
+** License version 2, as published by the Free Software Foundation, and
+** may be copied, distributed, and modified under those terms.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** Author: San Mehat <san@google.com>
+**
+*/
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/string.h>
+#include <linux/errno.h>
+#include <linux/cdev.h>
+#include <linux/init.h>
+#include <linux/device.h>
+#include <linux/types.h>
+#include <linux/fs.h>
+#include <asm/uaccess.h>
+#include <linux/delay.h>
+#include <linux/msm_rpcrouter.h>
+#include <asm/arch/msm_rpcrouter.h>
+#include <asm/arch/rpc_pm.h>
+#include "rpc_pm.h"
+
+static rpcrouterclient_t *rpc_client;
+static rpcrouter_address pmsvc_addr;
+
+static int rpc_pm_vote_vreg_switch(int enable, uint32_t vreg_id,
+	                           uint32_t app_mask)
+{
+	pm_rpc_vregswitch_msg msg;
+	oncrpc_reply_hdr *rep;
+	int rc;
+
+	rpcrouter_kernapi_setup_request(&msg.hdr, APP_PM_PROG, APP_PM_VER,
+					PM_PROCEEDURE_VOTEVREGSWITCH,
+					sizeof(pm_rpc_vregswitch_req_args));
+	if (enable)
+		msg.args.cmd = cpu_to_be32(1);
+	else
+		msg.args.cmd = cpu_to_be32(0);
+	msg.args.vreg_id = cpu_to_be32(vreg_id);
+	msg.args.app_mask = cpu_to_be32(app_mask);
+
+	rc = rpcrouter_kernapi_write(rpc_client, &pmsvc_addr, &msg, sizeof(msg));
+	if (rc < 0)
+		return rc;
+
+	rc = rpcrouter_kernapi_read(rpc_client, (void **) &rep, (5*HZ));
+	if (rc < 0)
+		return rc;
+
+	kfree(rep);
+	return 0;
+}
+
+static int __init rpc_pm_init(void)
+{
+	rpcrouter_xport_address	xport_addr;
+	int rc;
+
+	xport_addr.xp = RPCROUTER_XPORT_SMD;
+	xport_addr.port = 2;
+
+	rc = rpcrouter_kernapi_openxport(&xport_addr);
+	if (rc < 0) {
+		printk(KERN_ERR "rpc_pm: Error opening SMD xport (%d)\n", rc);
+		return rc;
+	}
+
+	rc = rpcrouter_kernapi_open(RPCPM_CLIENT_ID, &rpc_client);
+	if (rc < 0) {
+		printk(KERN_ERR "rpc_pm: Error opening SMD client (%d)\n", rc);
+		return rc;
+	}
+
+	rc = rpcrouter_kernapi_getdest(rpc_client, APP_PM_PROG, APP_PM_VER,
+				       (5 * HZ), &pmsvc_addr);
+	if (rc < 0) {
+		printk(KERN_ERR "rpc_pm: Unable to find pm service (%d)\n", rc);
+		return rc;
+	}
+
+	return 0;
+}
+
+module_init(rpc_pm_init);
+
+MODULE_DESCRIPTION("RPC Power Management");
+MODULE_AUTHOR("San Mehat <san@android.com>");
+MODULE_LICENSE("GPL");
---
+++ arch/arm/mach-msm/rpc_pm.h
@@ -0,0 +1,57 @@
+/* arch/arm/mach-msm/rpc_pm.h
+**
+** Copyright (C) 2007 Google, Inc.
+** Copyright (c) 2007 QUALCOMM Incorporated
+**
+** This software is licensed under the terms of the GNU General Public
+** License version 2, as published by the Free Software Foundation, and
+** may be copied, distributed, and modified under those terms.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** Author: San Mehat <san@google.com>
+**
+*/
+
+#ifndef __ARCH_ARM_MACH_MSM_RPC_PM_H
+#define __ARCH_ARM_MACH_MSM_RPC_PM_H
+
+
+/* ===========================
+ * Low level RPC PM control
+ * ===========================
+ */
+#define RPCPM_CLIENT_ID 0x80000002
+#define APP_PM_PROG 0x30000060
+#define APP_PM_VER 0
+
+#define PM_PROCEEDURE_NULL 0
+#define PM_PROCEEDURE_SETPWRKEYDUR 1
+#define PM_PROCEEDURE_VOTEVREGSWITCH 2
+
+/*
+ *  The RPC request/response structures aren't combined as one might
+ *  stylistically hope because this data is derrived from XDR definitions.
+ */
+
+/*
+ * PM_PROCEEDURE_VREGSWITCH
+ */
+
+typedef struct
+{
+	uint32_t	cmd; /* 0 == off, 1 = on */
+	uint32_t	vreg_id;
+	uint32_t	app_mask;
+} pm_rpc_vregswitch_req_args;
+
+typedef struct
+{
+        oncrpc_request_hdr hdr;
+        pm_rpc_vregswitch_req_args args;
+} pm_rpc_vregswitch_msg;
+
+#endif
