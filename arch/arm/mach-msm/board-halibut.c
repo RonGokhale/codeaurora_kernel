b2c114553271e3b8e274478520a72e2dd5736a82
Arve Hjønnevåg <arve@android.com>
2008-02-04 18:21:48 -0800

    msm: halibut: Suspend mddi bridge chip and turn backlight off.
    Signed-off-by: Brian Swetland <swetland@google.com>

--- arch/arm/mach-msm/board-halibut.c
+++ arch/arm/mach-msm/board-halibut.c
@@ -28,9 +28,11 @@
 #include <asm/arch/board.h>
 #include <asm/arch/clock.h>
 #include <asm/arch/msm_iomap.h>
+#include <asm/arch/msm_fb.h>
 
 #include <asm/io.h>
 #include <asm/delay.h>
+#include <linux/delay.h>
 
 #include <linux/mtd/nand.h>
 #include <linux/mtd/partitions.h>
@@ -60,7 +62,32 @@ static struct platform_device smc91x_device = {
 	.resource	= smc91x_resources,
 };
 
+#define MDDI_CLIENT_CORE_BASE  0x108000
+#define LCD_CONTROL_BLOCK_BASE 0x110000
+#define PWM_BLOCK_BASE         0x140000
+#define DPSUS       (MDDI_CLIENT_CORE_BASE|0x24)
+#define SYSCLKENA   (MDDI_CLIENT_CORE_BASE|0x2C)
+#define START       (LCD_CONTROL_BLOCK_BASE|0x08)
+#define PWM0OFF     (PWM_BLOCK_BASE|0x1C)
+
+static void mddi0_panel_power(struct mddi_panel_info *panel, int on)
+{
+	if(on) {
+		mddi_remote_write(panel->mddi, 0, DPSUS);
+		udelay(122);
+		mddi_remote_write(panel->mddi, 1, SYSCLKENA);
+		mddi_remote_write(panel->mddi, halibut_ffa ? 0 : 0x00001387, PWM0OFF);
+	}
+	else {
+		mddi_remote_write(panel->mddi, halibut_ffa ? 0x00001387 : 0, PWM0OFF);
+		udelay(122);
+		mddi_remote_write(panel->mddi, 0, SYSCLKENA);
+		mddi_remote_write(panel->mddi, 1, DPSUS);
+	}
+}
+
 static struct msm_mddi_platform_data msm_mddi0_pdata = {
+	.panel_power	= mddi0_panel_power,
 	.has_vsync_irq	= 0,
 };
 
