f3abc7acce7ffb3c5fe4daa0ae3d5bf400a00a9c
San Mehat <san@android.com>
2008-04-14 15:22:49 -0700

    mmc: Add concept of an 'embedded' SDIO device.
    This is required to support chips which use SDIO for signaling/
    communication but do not implement the various card enumeration registers
    as required for full SD / SDIO cards.
    
    Signed-off-by: San Mehat <san@android.com>

--- include/asm-arm/mach/mmc.h
+++ include/asm-arm/mach/mmc.h
@@ -5,11 +5,21 @@
 #define ASMARM_MACH_MMC_H
 
 #include <linux/mmc/host.h>
+#include <linux/mmc/card.h>
+#include <linux/mmc/sdio_func.h>
+
+struct embedded_sdio_data {
+	struct sdio_cis	cis;
+	struct sdio_cccr cccr;
+	struct sdio_embedded_func *funcs;
+	int num_funcs;
+};
 
 struct mmc_platform_data {
 	unsigned int ocr_mask;			/* available voltages */
 	u32 (*translate_vdd)(struct device *, unsigned int);
 	unsigned int (*status)(struct device *);
+	struct embedded_sdio_data *embedded_sdio;
 };
 
 #endif
