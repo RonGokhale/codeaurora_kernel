1329f4cb3ce8fb343ff8e23e37bda5c9fd57eaf4
Dima Zavin <dima@android.com>
2009-11-14 18:48:57 -0800

    [ARM] msm: scorpion: Use MPLL for intermediate steps while freq scaling
    Change-Id: I19e0a411472225330e2dc68c5ef6ebbe4d8d4561
    Signed-off-by: Dima Zavin <dima@android.com>

--- arch/arm/mach-msm/acpuclock-scorpion.c
+++ arch/arm/mach-msm/acpuclock-scorpion.c
@@ -98,9 +98,9 @@ struct clkctl_acpu_speed acpu_freq_tbl[] = {
 /* select the standby clock that is used when switching scpll
  * frequencies
  *
- * Currently: TCXO
+ * Currently: MPLL
  */
-struct clkctl_acpu_speed *acpu_stby = &acpu_freq_tbl[0];
+struct clkctl_acpu_speed *acpu_stby = &acpu_freq_tbl[2];
 #define IS_ACPU_STANDBY(x)	(((x)->clk_cfg == acpu_stby->clk_cfg) && \
 				 ((x)->clk_sel == acpu_stby->clk_sel))
 
