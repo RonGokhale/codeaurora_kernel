890877c434eb6477e224555284def4500ad42aad
Jordan Crouse <jcrouse@quicinc.com>
2010-01-13 09:48:27 -0700

    drm:  Use kzalloc() where appropriate in platform init
    Move the platform initialization code to use kzalloc().
    
    Change-Id: I536073d78cdad61ff6494094e761fb4abad860a4
    Signed-off-by: Jordan Crouse <jcrouse@quicinc.com>

--- drivers/gpu/drm/drm_pci.c
+++ drivers/gpu/drm/drm_pci.c
@@ -79,7 +79,7 @@ int drm_get_pci_dev(struct pci_dev *pdev, const struct pci_device_id *ent,
 
 	DRM_DEBUG("\n");
 
-	dev = drm_calloc(1, sizeof(*dev), DRM_MEM_STUB);
+	dev = kzalloc(sizeof(*dev), GFP_KERNEL);
 	if (!dev)
 		return -ENOMEM;
 
@@ -110,7 +110,7 @@ int drm_get_pci_dev(struct pci_dev *pdev, const struct pci_device_id *ent,
 err_g2:
 	pci_disable_device(pdev);
 err_g1:
-	drm_free(dev, sizeof(*dev), DRM_MEM_STUB);
+	kfree(dev);
 	return ret;
 }
 
