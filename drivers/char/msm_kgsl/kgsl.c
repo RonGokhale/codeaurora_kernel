c525e3922500f08661ea60ec08e0c4a9f4700bbc
Shubhraprakash Das <sadas@codeaurora.org>
2010-06-08 23:46:18 -0600

    msm_kgsl: Only add an entry to memory list if allocation succeeds
    List of memory allocations made in kgsl is maintained in a list.
    The list consists of memory descriptor nodes representing each
    allocation. When a new allocation is made add a new memory descriptor
    for this allocation to this list only if the allocation is
    successful.
    
    Change-Id: Ib11ba863133e176ccc1175895ba1f56dedd4b09a
    Signed-off-by: Shubhraprakash Das <sadas@codeaurora.org>
    Signed-off-by: Jeremy Gebben <jgebben@codeaurora.org>

--- drivers/char/msm_kgsl/kgsl.c
+++ drivers/char/msm_kgsl/kgsl.c
@@ -1320,7 +1320,6 @@ static int kgsl_ioctl_sharedmem_from_pmem(struct kgsl_file_private *private,
 	}
 
 	entry->pmem_file = pmem_file;
-	list_add(&entry->list, &private->mem_list);
 
 	entry->memdesc.pagetable = private->pagetable;
 
@@ -1346,6 +1345,7 @@ static int kgsl_ioctl_sharedmem_from_pmem(struct kgsl_file_private *private,
 		result = -EFAULT;
 		goto error_unmap_entry;
 	}
+	list_add(&entry->list, &private->mem_list);
 	return result;
 
 error_unmap_entry:
