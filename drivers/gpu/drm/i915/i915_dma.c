0f86a2107efa416504706f7be71db9ef80aaa84a
Bryan Freed <bfreed@google.com>
2010-06-22 15:59:43 -0700

    CHROMIUM: Implement direct pineview backlight control.
    This adds a Kconfig variable CONFIG_DRM_I915_DIRECT_BACKLIGHT to
    optionally include the new source file i915_backlight.c.  This file
    registers a couple backlight set/get routines with the standard
    drivers/video/backlight interface to add a directory under
    /sys/class/backlight/.
    
    The big problem I see with this modification is that the i915 driver
    _already_ supports backlight level adjustment in i915_opregion.c.
    Unfortunately, that requires an IRQ to occur as the result of writing
    some ASLE (ACPI Source Language Event) register, and this requires
    BIOS support for the ACPI hook.  It looks like we will not have such
    support in our initial custom BIOS.
    
    So I copy a small amount of code from i915_opregion.c to make backlight
    adjustment directly available to the backlight framework.
    
    Another problem is that I do not cover all the cases handled by the
    i915_opregion.c code.  It would be too much untested code copying to
    do so.  I enforce the lack of support by checking for IS_PINEVIEW()
    in i915_backlight_init().
    
    Review URL: http://codereview.chromium.org/2830015

--- drivers/gpu/drm/i915/i915_dma.c
+++ drivers/gpu/drm/i915/i915_dma.c
@@ -2203,6 +2203,7 @@ int i915_driver_load(struct drm_device *dev, unsigned long flags)
 	i915_mch_dev = dev_priv;
 	dev_priv->mchdev_lock = &mchdev_lock;
 	spin_unlock(&mchdev_lock);
+	i915_backlight_init(dev);
 
 	return 0;
 
@@ -2228,6 +2229,7 @@ int i915_driver_unload(struct drm_device *dev)
 	spin_lock(&mchdev_lock);
 	i915_mch_dev = NULL;
 	spin_unlock(&mchdev_lock);
+	i915_backlight_exit(dev);
 
 	destroy_workqueue(dev_priv->wq);
 	del_timer_sync(&dev_priv->hangcheck_timer);
--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -35,6 +35,10 @@
 #include "intel_ringbuffer.h"
 #include <linux/io-mapping.h>
 
+#ifdef CONFIG_DRM_I915_DIRECT_BACKLIGHT
+#include <linux/backlight.h>
+#endif
+
 /* General customization:
  */
 
@@ -643,6 +647,10 @@ typedef struct drm_i915_private {
 
 	/* list of fbdev register on this device */
 	struct intel_fbdev *fbdev;
+#ifdef CONFIG_DRM_I915_DIRECT_BACKLIGHT
+	/* direct backlight interface */
+	struct backlight_device *backlight;
+#endif
 } drm_i915_private_t;
 
 /** driver private structure attached to each drm_gem_object */
@@ -1038,6 +1046,15 @@ static inline void intel_opregion_free(struct drm_device *dev, int suspend) { re
 static inline void opregion_asle_intr(struct drm_device *dev) { return; }
 static inline void ironlake_opregion_gse_intr(struct drm_device *dev) { return; }
 static inline void opregion_enable_asle(struct drm_device *dev) { return; }
+#endif
+
+#ifdef CONFIG_DRM_I915_DIRECT_BACKLIGHT
+/* i915_backlight.c */
+extern void i915_backlight_init(struct drm_device *dev);
+extern void i915_backlight_exit(struct drm_device *dev);
+#else
+extern inline void i915_backlight_init(struct drm_device *dev) { return; }
+extern inline void i915_backlight_exit(struct drm_device *dev) { return; }
 #endif
 
 /* modesetting */
