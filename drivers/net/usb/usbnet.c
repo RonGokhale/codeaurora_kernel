1a54ca2dddd8db5d593c4f6a55156cf835591c95
Elly Jones <ellyjones@google.com>
2010-07-26 08:53:00 -0400

    CHROMIUM: USB: Don't send URBs to devices that can't take them.
    This fixes the bug where Qualcomm Gobis use 100% of CPU when autosuspended by
    the USB stack. This patch has been sent upstream, but we have no response yet.
    
    TEST=Adhoc
    On a laptop with a Gobi, enable autosuspend for the device, then unplug power.
    Wait a few seconds; note ksoftirqd using 100% of CPU. Note unpleasant effects on
    battery life.
    
    BUG=None
    This is related to the (now-closed) 2888010.
    
    Signed-Off-By: Jason Glasgow <jglasgow@chromium.org>
    Signed-Off-By: Elly Jones <ellyjones@chromium.org>
    [Fixed up merge conflict when cherry-picking to 2.6.35-rc5]
    Signed-off-by: Olof Johansson <olofj@chromium.org>
    
    Review URL: http://codereview.chromium.org/2861075

--- drivers/net/usb/usbnet.c
+++ drivers/net/usb/usbnet.c
@@ -1170,10 +1170,11 @@ static void usbnet_bh (unsigned long param)
 		}
 
 	// or are we maybe short a few urbs?
-	} else if (netif_running (dev->net) &&
-		   netif_device_present (dev->net) &&
-		   !timer_pending (&dev->delay) &&
-		   !test_bit (EVENT_RX_HALT, &dev->flags)) {
+	} else if (netif_running (dev->net)
+			&& netif_device_present (dev->net)
+			&& dev->udev->can_submit
+			&& !timer_pending (&dev->delay)
+			&& !test_bit (EVENT_RX_HALT, &dev->flags)) {
 		int	temp = dev->rxq.qlen;
 		int	qlen = RX_QLEN (dev);
 
