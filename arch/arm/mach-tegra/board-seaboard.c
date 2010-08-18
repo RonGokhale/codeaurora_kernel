6c6c2c1dc3712d48601dc083b4ee6c65c5744642
Andrew Chew <achew@nvidia.com>
2010-08-18 17:48:08 -0500

    TEGRA: Initialize I2C on Seaboard
    Signed-off-by: Andrew Chew <achew@nvidia.com>
    Signed-off-by: Olof Johansson <olof@lixom.net>
    
    Review URL: http://codereview.chromium.org/3150020

--- arch/arm/mach-tegra/board-seaboard.c
+++ arch/arm/mach-tegra/board-seaboard.c
@@ -26,6 +26,7 @@
 #include <linux/clk.h>
 #include <linux/serial_8250.h>
 #include <linux/i2c.h>
+#include <linux/i2c-tegra.h>
 #include <linux/pda_power.h>
 #include <linux/dma-mapping.h>
 #include <linux/fsl_devices.h>
@@ -142,6 +143,56 @@ static struct platform_device tegra_gart_dev = {
     .resource = tegra_gart_resources
 };
 
+static struct tegra_i2c_platform_data seaboard_i2c1_platform_data = {
+	.adapter_nr	= 0,
+	.bus_count	= 1,
+	.bus_clk_rate	= { 400000, 0 },
+};
+
+static const struct tegra_pingroup_config i2c2_ddc = {
+	.pingroup	= TEGRA_PINGROUP_DDC,
+	.func		= TEGRA_MUX_I2C2,
+};
+
+static const struct tegra_pingroup_config i2c2_gen2 = {
+	.pingroup	= TEGRA_PINGROUP_PTA,
+	.func		= TEGRA_MUX_I2C2,
+};
+
+static struct tegra_i2c_platform_data seaboard_i2c2_platform_data = {
+	.adapter_nr	= 1,
+	.bus_count	= 2,
+	.bus_clk_rate	= { 400000, 100000 },
+	.bus_mux	= { &i2c2_ddc, &i2c2_gen2 },
+	.bus_mux_len	= { 1, 1 },
+};
+
+static struct tegra_i2c_platform_data seaboard_i2c3_platform_data = {
+	.adapter_nr	= 3,
+	.bus_count	= 1,
+	.bus_clk_rate	= { 400000, 0 },
+};
+
+static struct tegra_i2c_platform_data seaboard_dvc_platform_data = {
+	.adapter_nr	= 4,
+	.bus_count	= 1,
+	.bus_clk_rate	= { 400000, 0 },
+	.is_dvc		= true,
+};
+
+static void seaboard_i2c_init(void)
+{
+	tegra_i2c_device1.dev.platform_data = &seaboard_i2c1_platform_data;
+	tegra_i2c_device2.dev.platform_data = &seaboard_i2c2_platform_data;
+	tegra_i2c_device3.dev.platform_data = &seaboard_i2c3_platform_data;
+	tegra_i2c_device4.dev.platform_data = &seaboard_dvc_platform_data;
+
+	platform_device_register(&tegra_i2c_device1);
+	platform_device_register(&tegra_i2c_device2);
+	platform_device_register(&tegra_i2c_device3);
+	platform_device_register(&tegra_i2c_device4);
+}
+
 static struct platform_device *seaboard_devices[] __initdata = {
 	&debug_uart,
 	&tegra_otg,
@@ -162,6 +213,7 @@ static void __init tegra_seaboard_init(void)
 
 	platform_add_devices(seaboard_devices, ARRAY_SIZE(seaboard_devices));
 	seaboard_sdhci_init();
+	seaboard_i2c_init();
 }
 
 MACHINE_START(TEGRA_SEABOARD, "seaboard")
