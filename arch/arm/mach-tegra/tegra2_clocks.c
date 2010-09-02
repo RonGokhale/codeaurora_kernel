06142b9edfbd81c97a183826deda358341884069
Allen Martin <amartin@nvidia.com>
2010-09-01 19:16:36 -0700

    CHROMIUM: arm: tegra: fix incorrect CLK_DUPLICATE line for 2d clock
    Signed-off-by: Allen Martin <amartin@nvidia.com>
    
    Review URL: http://codereview.chromium.org/3307004

--- arch/arm/mach-tegra/tegra2_clocks.c
+++ arch/arm/mach-tegra/tegra2_clocks.c
@@ -1712,7 +1712,7 @@ struct clk_duplicate tegra_clk_duplicates[] = {
 	CLK_DUPLICATE("hdmi", "tegradc.0", "hdmi"),
 	CLK_DUPLICATE("hdmi", "tegradc.1", "hdmi"),
 	CLK_DUPLICATE("host1x", "tegra_grhost", "host1x"),
-	CLK_DUPLICATE("2d", "tegra_grhost", "gr3d"),
+	CLK_DUPLICATE("2d", "tegra_grhost", "gr2d"),
 	CLK_DUPLICATE("3d", "tegra_grhost", "gr3d"),
 	CLK_DUPLICATE("epp", "tegra_grhost", "epp"),
 };
