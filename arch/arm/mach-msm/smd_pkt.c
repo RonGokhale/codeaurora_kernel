c57387249871102c37266f7da2bb9b1171cb7c13
Karthikeyan Ramasubramanian <kramasub@codeaurora.org>
2010-09-10 17:30:31 -0600

    msm: smd_pkt: Add a channel allocation completion variable per device
    There is one completion variable for the entire SMD PKT driver to
    indicate the allocation of SMD channels corresponding to control ports.
    This leads to the race condition where some some SMD channel access
    happens before the allocation. So add the channel allocation
    completion variable for every SMD PKT device.
    
    Change-Id: I57e8f2d17be7a635087fe87d638fa5c074ea8b41
    Signed-off-by: Karthikeyan Ramasubramanian <kramasub@codeaurora.org>

--- arch/arm/mach-msm/smd_pkt.c
+++ arch/arm/mach-msm/smd_pkt.c
@@ -67,6 +67,7 @@ struct smd_pkt_dev {
 	struct notifier_block nb;
 	int has_reset;
 	struct mutex has_reset_lock;
+	struct completion ch_allocated;
 
 } *smd_pkt_devp[NUM_SMD_PKT_PORTS];
 
@@ -409,11 +410,16 @@ static char *smd_ch_name[] = {
 	"LOOPBACK",
 };
 
-static DECLARE_COMPLETION(modem_loaded);
-
 static int smd_pkt_dummy_probe(struct platform_device *pdev)
 {
-	complete_all(&modem_loaded);
+	int i;
+
+	for (i = 0; i < NUM_SMD_PKT_PORTS; i++) {
+		if (!strcmp(pdev->name, smd_ch_name[i])) {
+			complete_all(&smd_pkt_devp[i]->ch_allocated);
+			break;
+		}
+	}
 	return 0;
 }
 
@@ -461,7 +467,8 @@ int smd_pkt_open(struct inode *inode, struct file *file)
 		 * Wait for a packet channel to be allocated so we know
 		 * the modem is ready enough.
 		 */
-		r = wait_for_completion_interruptible_timeout(&modem_loaded,
+		r = wait_for_completion_interruptible_timeout(
+				&smd_pkt_devp->ch_allocated,
 				msecs_to_jiffies(60000));
 		if (r == 0) {
 			pr_err("Timed out waiting for SMD channel\n");
@@ -595,6 +602,7 @@ static int __init smd_pkt_init(void)
 		mutex_init(&smd_pkt_devp[i]->ch_lock);
 		mutex_init(&smd_pkt_devp[i]->rx_lock);
 		mutex_init(&smd_pkt_devp[i]->tx_lock);
+		init_completion(&smd_pkt_devp[i]->ch_allocated);
 
 		cdev_init(&smd_pkt_devp[i]->cdev, &smd_pkt_fops);
 		smd_pkt_devp[i]->cdev.owner = THIS_MODULE;
