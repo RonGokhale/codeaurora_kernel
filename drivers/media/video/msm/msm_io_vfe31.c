8f25afc53e7f1fcada915eaa6009f74b8277618a
Shuzhen Wang <shuzhenw@codeaurora.org>
2010-09-29 16:20:09 -0400

    camera: switch power supply for FLUID 12M camera and D02.
    LVSW1 doesn't have enough driving strength for the Sony 12M sensor,
    its voltage drops below the 1.7V required by the sensor. The solution
    is to modify the camera daughter board so that GP16 turns on as
    digital voltage for main camera and D02, and GP6 turns on secondary
    camera. Meantime, GPIO143 is driving high to put secondary camera in
    standby.
    
    Change-Id: Icbde0c6aaa9ad110775e50ee2719a19412f963a0
    CRs-Fixed: 253514
    Signed-off-by: Shuzhen Wang <shuzhenw@codeaurora.org>

--- drivers/media/video/msm/msm_io_vfe31.c
+++ drivers/media/video/msm/msm_io_vfe31.c
@@ -24,6 +24,7 @@
 #include <mach/camera.h>
 #include <mach/vreg.h>
 #include <mach/clk.h>
+#include <asm/mach-types.h>
 
 #define CAMIF_CFG_RMSK             0x1fffff
 #define CAM_SEL_BMSK               0x2
@@ -108,6 +109,8 @@ static struct clk *camio_jpeg_pclk;
 static struct clk *camio_vpe_clk;
 static struct vreg *vreg_gp2;
 static struct vreg *vreg_lvsw1;
+static struct vreg *vreg_gp6;
+static struct vreg *vreg_gp16;
 static struct msm_camera_io_ext camio_ext;
 static struct msm_camera_io_clk camio_clk;
 static struct resource *camifpadio, *csiio;
@@ -207,7 +210,7 @@ static void msm_camera_vreg_enable(void)
 	if (vreg_enable(vreg_gp2)) {
 		pr_err("%s: VREG GP2 enable failed\n", __func__);
 		goto gp2_put;
-		}
+	}
 
 	vreg_lvsw1 = vreg_get(NULL, "lvsw1");
 	if (IS_ERR(vreg_lvsw1)) {
@@ -220,11 +223,58 @@ static void msm_camera_vreg_enable(void)
 		pr_err("%s: VREG LVSW1 set failed\n", __func__);
 		goto lvsw1_put;
 	}
-	if (vreg_enable(vreg_lvsw1))
+	if (vreg_enable(vreg_lvsw1)) {
 		pr_err("%s: VREG LVSW1 enable failed\n", __func__);
+		goto lvsw1_put;
+	}
 
+	if (machine_is_msm7x30_fluid()) {
+		vreg_gp6 = vreg_get(NULL, "gp6");
+		if (IS_ERR(vreg_gp6)) {
+			pr_err("%s: VREG GP6 get failed %ld\n", __func__,
+				PTR_ERR(vreg_gp6));
+			vreg_gp6 = NULL;
+			goto lvsw1_disable;
+		}
+
+		if (vreg_set_level(vreg_gp6, 3050)) {
+			pr_err("%s: VREG GP6 set failed\n", __func__);
+			goto gp6_put;
+		}
+
+		if (vreg_enable(vreg_gp6)) {
+			pr_err("%s: VREG GP6 enable failed\n", __func__);
+			goto gp6_put;
+		}
+		vreg_gp16 = vreg_get(NULL, "gp16");
+		if (IS_ERR(vreg_gp16)) {
+			pr_err("%s: VREG GP16 get failed %ld\n", __func__,
+				PTR_ERR(vreg_gp16));
+			vreg_gp16 = NULL;
+			goto gp6_disable;
+		}
+
+		if (vreg_set_level(vreg_gp16, 1200)) {
+			pr_err("%s: VREG GP16 set failed\n", __func__);
+			goto gp6_disable;
+		}
+
+		if (vreg_enable(vreg_gp16)) {
+			pr_err("%s: VREG GP16 enable failed\n", __func__);
+			goto gp16_put;
+		}
+	}
 	return;
 
+gp16_put:
+	vreg_put(vreg_gp16);
+gp6_disable:
+	/* Voting off gp6 causes touchscreen i2c issues */
+	/* vreg_disable(vreg_gp6); */
+gp6_put:
+	vreg_put(vreg_gp6);
+lvsw1_disable:
+	vreg_disable(vreg_lvsw1);
 lvsw1_put:
 	vreg_put(vreg_lvsw1);
 gp2_disable:
@@ -242,6 +292,15 @@ static void msm_camera_vreg_disable(void)
 	if (vreg_lvsw1) {
 		vreg_disable(vreg_lvsw1);
 		vreg_put(vreg_lvsw1);
+	}
+	if (vreg_gp6) {
+		/* Voting off gp6 causes touchscreen i2c issues */
+		/* vreg_disable(vreg_gp6); */
+		vreg_put(vreg_gp6);
+	}
+	if (vreg_gp16) {
+		vreg_disable(vreg_gp16);
+		vreg_put(vreg_gp16);
 	}
 }
 
