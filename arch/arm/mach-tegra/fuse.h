dd4ae3ab91380a28d4b0426e62201d8d6a2e5b97
Colin Cross <ccross@android.com>
2010-10-17 15:00:08 -0700

    [ARM] tegra: fuse: Add function to get Tegra revision
    Change-Id: I11783f5784454fec143393336195db40c9aa3160
    Signed-off-by: Colin Cross <ccross@android.com>

--- arch/arm/mach-tegra/fuse.h
+++ arch/arm/mach-tegra/fuse.h
@@ -20,6 +20,14 @@
 #define SKU_ID_T20	8
 #define SKU_ID_T25	24
 
+enum tegra_revision {
+	TEGRA_REVISION_UNKNOWN = 0,
+	TEGRA_REVISION_A02,
+	TEGRA_REVISION_A03,
+	TEGRA_REVISION_A03p,
+	TEGRA_REVISION_MAX,
+};
+
 unsigned long long tegra_chip_uid(void);
 int tegra_sku_id(void);
 int tegra_cpu_process_id(void);
@@ -28,3 +36,4 @@ void tegra_init_fuse(void);
 void tegra_init_fuse_dma(void);
 u32 tegra_fuse_readl(unsigned long offset);
 void tegra_fuse_writel(u32 value, unsigned long offset);
+enum tegra_revision tegra_get_revision(void);
