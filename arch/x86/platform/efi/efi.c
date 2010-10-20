63a13121e97ffe8fe626e60a26fd7c04d68fbc06
Olof Johansson <olof@lixom.net>
2010-10-19 19:01:30 -0500

    Allow booting 32-bit kernel with 64-bit EFI BIOS
    This adds and enables a Kconfig option (CONFIG_EFI64) which lets 32-bit x86 kernels boot with our 64-bit EFI BIOS. I've tested it on both the EFI BIOS board and the EeePC's legacy BIOS, and the same image works on both.
    
    Review URL: http://codereview.chromium.org/1605022
    
    Conflicts:
    
    	arch/x86/configs/i386_defconfig
    	arch/x86/configs/x86_64_defconfig
    
    Conflicts:
    
    	chromeos/config/i386/config.common.i386

--- arch/x86/platform/efi/efi.c
+++ arch/x86/platform/efi/efi.c
@@ -499,7 +499,7 @@ void __init efi_init(void)
 	if (add_efi_memmap)
 		do_add_efi_memmap();
 
-#ifdef CONFIG_X86_32
+#if defined(CONFIG_X86_32) && !defined(CONFIG_EFI64)
 	x86_platform.get_wallclock = efi_get_time;
 	x86_platform.set_wallclock = efi_set_rtc_mmss;
 #endif
@@ -550,6 +550,7 @@ static void __init runtime_code_page_mkexec(void)
  */
 void __init efi_enter_virtual_mode(void)
 {
+#if !defined(CONFIG_EFI64)
 	efi_memory_desc_t *md, *prev_md = NULL;
 	efi_status_t status;
 	unsigned long size;
@@ -671,9 +672,10 @@ void __init efi_enter_virtual_mode(void)
 	efi.set_virtual_address_map = NULL;
 	if (__supported_pte_mask & _PAGE_NX)
 		runtime_code_page_mkexec();
+	kfree(new_memmap);
+#endif  /* CONFIG_EFI64 */
 	early_iounmap(memmap.map, memmap.nr_map * memmap.desc_size);
 	memmap.map = NULL;
-	kfree(new_memmap);
 }
 
 /*
