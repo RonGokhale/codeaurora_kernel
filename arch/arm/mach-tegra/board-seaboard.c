46db8c829644582827a2fa4aa27ecd71b69722e4
Andrew Chew <achew@nvidia.com>
2010-11-12 11:10:53 -0600

    CHROMIUM: seaboard: Add Tegra RTC to board files.
    This enables the Tegra real-time clock driver.
    
    Change-Id: Ief606972ce6e6b37527790ee989fa0eb62a9a8ba
    
    BUG=none
    TEST=Built kernel.
    Signed-off-by: Andrew Chew <achew@nvidia.com>
    Signed-off-by: Olof Johansson <olofj@chromium.org>
    
    Review URL: http://codereview.chromium.org/4403002

--- arch/arm/mach-tegra/board-seaboard.c
+++ arch/arm/mach-tegra/board-seaboard.c
@@ -341,6 +341,26 @@ static void __init seaboard_i2c_init(void)
 				ARRAY_SIZE(seaboard_i2c4_devices));
 }
 
+static struct resource tegra_rtc_resources[] = {
+	[0] = {
+		.start = TEGRA_RTC_BASE,
+		.end = TEGRA_RTC_BASE + TEGRA_RTC_SIZE - 1,
+		.flags = IORESOURCE_MEM,
+	},
+	[1] = {
+		.start = INT_RTC,
+		.end = INT_RTC,
+		.flags = IORESOURCE_IRQ,
+	},
+};
+
+static struct platform_device tegra_rtc_device = {
+	.name = "tegra_rtc",
+	.id   = -1,
+	.resource = tegra_rtc_resources,
+	.num_resources = ARRAY_SIZE(tegra_rtc_resources),
+};
+
 static struct gpio_keys_button seaboard_gpio_keys_buttons[] = {
 	{
 		.code		= SW_LID,
@@ -445,6 +465,7 @@ static void seaboard_kbc_init(void)
 static struct platform_device *seaboard_devices[] __initdata = {
 	&debug_uart,
 	&tegra_ehci3_device,
+	&tegra_rtc_device,
 	&pmu_device,
 	&pda_power_device,
 	&seaboard_gpio_keys_device,
