0cb991caa4eb96181006e9bdc0f0c8f362f77a4d
Jin Qian <jqian@nvidia.com>
2010-11-13 08:12:02 -0600

    CHROMIUM: tegra: add board support for ALSA SoC driver
    Change-Id: I1bf98450f9ba002cd9baa8fb07396134244bce47
    From: Gaurav Sarode <gsarode@nvidia.com>
    From: Vijay Mali <vmali@nvidia.com>
    From: Scott Peterson <speterson@nvidia.com>
    From: Jin Qian <jqian@nvidia.com>
    
    BUG=
    TEST=boot os
    
    Signed-off-by: Jin Qian <jqian@nvidia.com>
    Signed-off-by: Olof Johansson <olofj@chromium.org>
    
    Review URL: http://codereview.chromium.org/4958001

--- arch/arm/mach-tegra/board-harmony.c
+++ arch/arm/mach-tegra/board-harmony.c
@@ -33,6 +33,8 @@
 #include <asm/mach/time.h>
 #include <asm/setup.h>
 
+#include <mach/audio.h>
+#include <mach/i2s.h>
 #include <mach/iomap.h>
 #include <mach/irqs.h>
 #include <mach/nand.h>
@@ -165,6 +167,11 @@ static struct platform_device pda_power_device = {
 	},
 };
 
+static struct i2c_board_info __initdata harmony_i2c_bus1_board_info[] = {
+	{
+		I2C_BOARD_INFO("wm8903", 0x1a),
+	},
+};
 
 static struct tegra_i2c_platform_data harmony_i2c1_platform_data = {
 	.adapter_nr	= 0,
@@ -203,12 +210,26 @@ static struct tegra_i2c_platform_data harmony_dvc_platform_data = {
 	.is_dvc		= true,
 };
 
+static struct tegra_audio_platform_data tegra_audio_pdata = {
+	.i2s_master	= false,
+	.dsp_master	= false,
+	.dma_on		= true,  /* use dma by default */
+	.i2s_clk_rate	= 240000000,
+	.dap_clk	= "clk_dev1",
+	.audio_sync_clk = "audio_2x",
+	.mode		= I2S_BIT_FORMAT_I2S,
+	.fifo_fmt	= I2S_FIFO_16_LSB,
+	.bit_size	= I2S_BIT_SIZE_16,
+};
+
 static void harmony_i2c_init(void)
 {
 	tegra_i2c_device1.dev.platform_data = &harmony_i2c1_platform_data;
 	tegra_i2c_device2.dev.platform_data = &harmony_i2c2_platform_data;
 	tegra_i2c_device3.dev.platform_data = &harmony_i2c3_platform_data;
 	tegra_i2c_device4.dev.platform_data = &harmony_dvc_platform_data;
+
+	i2c_register_board_info(0, harmony_i2c_bus1_board_info, 1);
 
 	platform_device_register(&tegra_i2c_device1);
 	platform_device_register(&tegra_i2c_device2);
@@ -227,6 +248,7 @@ static struct platform_device *harmony_devices[] __initdata = {
 	&tegra_spi_device3,
 	&tegra_spi_device4,
 	&tegra_gart_device,
+	&tegra_i2s_device1,
 };
 
 static void __init tegra_harmony_fixup(struct machine_desc *desc,
@@ -242,6 +264,12 @@ static void __init tegra_harmony_fixup(struct machine_desc *desc,
 static __initdata struct tegra_clk_init_table harmony_clk_init_table[] = {
 	/* name		parent		rate		enabled */
 	{ "uartd",	"pll_p",	216000000,	true },
+	{ "pll_a",	NULL,		11289600,	true},
+	{ "pll_a_out0",	NULL,		11289600,	true},
+	{ "i2s1",	"pll_a_out0",	11289600,	true},
+	{ "i2s2",	"pll_a_out0",	11289600,	true},
+	{ "audio",	"pll_a_out0",	11289600,	true},
+	{ "audio_2x",	"audio",	22579200,	true},
 	{ NULL,		NULL,		0,		0},
 };
 
@@ -252,6 +280,8 @@ static void __init tegra_harmony_init(void)
 	tegra_clk_init_from_table(harmony_clk_init_table);
 
 	harmony_pinmux_init();
+
+	tegra_i2s_device1.dev.platform_data = &tegra_audio_pdata;
 
 	platform_add_devices(harmony_devices, ARRAY_SIZE(harmony_devices));
 
