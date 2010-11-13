71992d4b2961fe1d989f3d822c741c648af0e676
Jin Qian <jqian@nvidia.com>
2010-11-12 20:32:08 -0600

    CHROMIUM: tegra: add i2c platform device for harmony board
    Change-Id: I1bf98450f9ba002cd9baa8fb07396134244bce47
    From: Gaurav Sarode <gsarode@nvidia.com>
    From: Vijay Mali <vmali@nvidia.com>
    From: Scott Peterson <speterson@nvidia.com>
    From: Jin Qian <jqian@nvidia.com>
    
    Signed-off-by: Jin Qian <jqian@nvidia.com>
    Signed-off-by: Olof Johansson <olofj@chromium.org>
    
    Review URL: http://codereview.chromium.org/4934001

--- arch/arm/mach-tegra/board-harmony.c
+++ arch/arm/mach-tegra/board-harmony.c
@@ -23,6 +23,8 @@
 #include <linux/mtd/partitions.h>
 #include <linux/dma-mapping.h>
 #include <linux/pda_power.h>
+#include <linux/i2c.h>
+#include <linux/i2c-tegra.h>
 #include <linux/io.h>
 #include <linux/delay.h>
 
@@ -62,7 +64,6 @@ struct tag_tegra {
 
 static int __init parse_tag_nvidia(const struct tag *tag)
 {
-
 	return 0;
 }
 __tagtable(ATAG_NVIDIA, parse_tag_nvidia);
@@ -164,16 +165,63 @@ static struct platform_device pda_power_device = {
 	},
 };
 
+
+static struct tegra_i2c_platform_data harmony_i2c1_platform_data = {
+	.adapter_nr	= 0,
+	.bus_count	= 1,
+	.bus_clk_rate	= { 400000, 0 },
+};
+
+static const struct tegra_pingroup_config i2c2_ddc = {
+	.pingroup	= TEGRA_PINGROUP_DDC,
+	.func		= TEGRA_MUX_I2C2,
+};
+
+static const struct tegra_pingroup_config i2c2_gen2 = {
+	.pingroup	= TEGRA_PINGROUP_PTA,
+	.func		= TEGRA_MUX_I2C2,
+};
+
+static struct tegra_i2c_platform_data harmony_i2c2_platform_data = {
+	.adapter_nr	= 1,
+	.bus_count	= 2,
+	.bus_clk_rate	= { 400000, 100000 },
+	.bus_mux	= { &i2c2_ddc, &i2c2_gen2 },
+	.bus_mux_len	= { 1, 1 },
+};
+
+static struct tegra_i2c_platform_data harmony_i2c3_platform_data = {
+	.adapter_nr	= 3,
+	.bus_count	= 1,
+	.bus_clk_rate	= { 400000, 0 },
+};
+
+static struct tegra_i2c_platform_data harmony_dvc_platform_data = {
+	.adapter_nr	= 4,
+	.bus_count	= 1,
+	.bus_clk_rate	= { 400000, 0 },
+	.is_dvc		= true,
+};
+
+static void harmony_i2c_init(void)
+{
+	tegra_i2c_device1.dev.platform_data = &harmony_i2c1_platform_data;
+	tegra_i2c_device2.dev.platform_data = &harmony_i2c2_platform_data;
+	tegra_i2c_device3.dev.platform_data = &harmony_i2c3_platform_data;
+	tegra_i2c_device4.dev.platform_data = &harmony_dvc_platform_data;
+
+	platform_device_register(&tegra_i2c_device1);
+	platform_device_register(&tegra_i2c_device2);
+	platform_device_register(&tegra_i2c_device3);
+	platform_device_register(&tegra_i2c_device4);
+}
+
 static struct platform_device *harmony_devices[] __initdata = {
 	&debug_uart,
 	&pmu_device,
 	&tegra_nand_device,
 	&tegra_udc_device,
 	&pda_power_device,
-	&tegra_i2c_device1,
-	&tegra_i2c_device2,
-	&tegra_i2c_device3,
-	&tegra_i2c_device4,
 	&tegra_spi_device1,
 	&tegra_spi_device2,
 	&tegra_spi_device3,
@@ -209,6 +257,7 @@ static void __init tegra_harmony_init(void)
 
 	harmony_panel_init();
 	harmony_sdhci_init();
+	harmony_i2c_init();
 }
 
 MACHINE_START(HARMONY, "harmony")
