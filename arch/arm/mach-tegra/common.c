8413cea2aeba5c2cca3998e0441a80cd481fee56
Colin Cross <ccross@android.com>
2010-11-24 14:52:02 -0800

    ARM: tegra: Rearrange subsystem initialization order
    Fuses must be initialized before clocks, so clocks can
    determine the maximum cpu frequency.  Clocks must be
    initialized before DMA, so DMA can enable the clock.
    Once DMA is initialized, all fuses accesses must go
    through DMA to avoid a hardware bug.
    
    Change-Id: Id1f58e4f43152ec19000e02f2d1ea45abf141f9c
    Signed-off-by: Colin Cross <ccross@android.com>

--- arch/arm/mach-tegra/common.c
+++ arch/arm/mach-tegra/common.c
@@ -127,12 +127,11 @@ static void tegra_pm_restart(char mode, const char *cmd)
 void __init tegra_init_early(void)
 {
 	arm_pm_restart = tegra_pm_restart;
-#ifdef CONFIG_TEGRA_SYSTEM_DMA
-	tegra_dma_init();
-#endif
 	tegra_init_fuse();
 	tegra_init_clock();
 	tegra_clk_init_from_table(common_clk_init_table);
 	tegra_init_power();
 	tegra_init_cache();
+	tegra_dma_init();
+	tegra_init_fuse_dma();
 }
