e93bde5b755d01afc1eb3bf025bc8ca65fca9f46
Allen Martin <amartin@nvidia.com>
2010-12-13 17:14:48 -0800

    CHROMIUM: tegra: seaboard: Fix MACH_TEGRA_SEABOARD
    Change MACH_TEGRA_SEABOARD to MACH_SEABOARD for consistency with
    other tegra boards
    
    Signed-off-by: Allen Martin <amartin@nvidia.com>
    
    Review URL: http://codereview.chromium.org/5734004
    
    Change-Id: Id2c0316f8e1e5fafa0338721021a791a87a27926

--- arch/arm/mach-tegra/board-seaboard.c
+++ arch/arm/mach-tegra/board-seaboard.c
@@ -549,7 +549,7 @@ static void __init tegra_seaboard_init(void)
 	tegra_gpio_enable(TEGRA_GPIO_POWERKEY);
 }
 
-MACHINE_START(TEGRA_SEABOARD, "seaboard")
+MACHINE_START(SEABOARD, "seaboard")
 	.boot_params    = 0x00000100,
 	.phys_io        = IO_APB_PHYS,
 	.io_pg_offst    = ((IO_APB_VIRT) >> 18) & 0xfffc,
