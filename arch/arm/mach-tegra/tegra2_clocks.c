7b58332c18d0885d9242ff5af0ca44ec5ba1793f
Varun Wadekar <vwadekar@nvidia.com>
2010-12-27 15:34:59 +0530

    [ARM] tegra: clocks: add clock entry for bsev
    Change-Id: I7ec657c30b84c65705b38a390bdc44b64cd5ea36
    Signed-off-by: Varun Wadekar <vwadekar@nvidia.com>

--- arch/arm/mach-tegra/tegra2_clocks.c
+++ arch/arm/mach-tegra/tegra2_clocks.c
@@ -2088,6 +2088,7 @@ struct clk tegra_list_clks[] = {
 	PERIPH_CLK("sdmmc4",	"sdhci-tegra.3",	NULL,	15,	0x164,	52000000,  mux_pllp_pllc_pllm_clkm,	MUX | DIV_U71), /* scales with voltage */
 	PERIPH_CLK("vcp",	"vcp",			NULL,	29,	0,	250000000, mux_clk_m, 			0),
 	PERIPH_CLK("bsea",	"bsea",			NULL,	62,	0,	250000000, mux_clk_m, 			0),
+	PERIPH_CLK("bsev",	"tegra-aes",		"bsev",	63,	0,	250000000, mux_clk_m, 			0),
 	PERIPH_CLK("vde",	"vde",			NULL,	61,	0x1c8,	250000000, mux_pllp_pllc_pllm_clkm,	MUX | DIV_U71), /* scales with voltage and process_id */
 	PERIPH_CLK("csite",	"csite",		NULL,	73,	0x1d4,	144000000, mux_pllp_pllc_pllm_clkm,	MUX | DIV_U71), /* max rate ??? */
 	/* FIXME: what is la? */
