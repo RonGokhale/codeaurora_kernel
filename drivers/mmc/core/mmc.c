fe1070b4e698c031868c82b7d74a1a33d3aaeb14
Sujith Reddy Thumma <sthumma@codeaurora.org>
2011-01-10 11:55:01 +0530

    mmc: core: Determine correct access mode for eMMC cards by reading OCR
    MMC core reads SEC_COUNT information from EXT_CSD register and assumes
    that the card supports sector access mode. Some eMMC cards (<=2GB) do not
    support this mode even though they have SEC_COUNT value defined, causing
    failure while populating extended partitions. Sector/Byte access mode
    information is stored in OCR register. Hence, read OCR bit 30 and then
    confirm it with SEC_COUNT value to know  whether the card supports sector
    access mode or byte access mode.
    
    Change-Id: I301e2848d5208cd2907b860c2a176f4a264e4c35
    Signed-off-by: Sujith Reddy Thumma <sthumma@codeaurora.org>

--- drivers/mmc/core/mmc.c
+++ drivers/mmc/core/mmc.c
@@ -221,8 +221,6 @@ static int mmc_read_ext_csd(struct mmc_card *card)
 			ext_csd[EXT_CSD_SEC_CNT + 1] << 8 |
 			ext_csd[EXT_CSD_SEC_CNT + 2] << 16 |
 			ext_csd[EXT_CSD_SEC_CNT + 3] << 24;
-		if (card->ext_csd.sectors)
-			mmc_card_set_blockaddr(card);
 	}
 
 	switch (ext_csd[EXT_CSD_CARD_TYPE] &&
@@ -307,6 +305,7 @@ static int mmc_init_card(struct mmc_host *host, u32 ocr,
 	int err;
 	u32 cid[4];
 	unsigned int max_dtr;
+	u32 rocr;
 
 	BUG_ON(!host);
 	WARN_ON(!host->claimed);
@@ -320,7 +319,7 @@ static int mmc_init_card(struct mmc_host *host, u32 ocr,
 	mmc_go_idle(host);
 
 	/* The extra bit indicates that we support high capacity */
-	err = mmc_send_op_cond(host, ocr | (1 << 30), NULL);
+	err = mmc_send_op_cond(host, ocr | MMC_CARD_SECTOR_ADDR, &rocr);
 	if (err)
 		goto err;
 
@@ -408,6 +407,9 @@ static int mmc_init_card(struct mmc_host *host, u32 ocr,
 		err = mmc_read_ext_csd(card);
 		if (err)
 			goto free_card;
+
+		if (card->ext_csd.sectors && (rocr & MMC_CARD_SECTOR_ADDR))
+			mmc_card_set_blockaddr(card);
 	}
 
 	/*
