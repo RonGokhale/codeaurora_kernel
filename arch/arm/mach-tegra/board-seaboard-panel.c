494cebb11524fd3fe6bd2e9ab4823496cdca1054
Olof Johansson <olofj@chromium.org>
2011-01-14 15:50:04 -0800

    CHROMIUM: tegra: wario: back out disp1 resource changes
    Change-Id: I7919701c0a4d60a9bc544aaab1482050988b8cb5
    
    BUG=none
    TEST=build kernel, boot wario, see X on the screen.
    
    Signed-off-by: Olof Johansson <olofj@chromium.org>
    
    Review URL: http://codereview.chromium.org/6349002

--- arch/arm/mach-tegra/board-seaboard-panel.c
+++ arch/arm/mach-tegra/board-seaboard-panel.c
@@ -137,27 +137,6 @@ static struct resource seaboard_disp1_resources[] = {
 	},
 };
 
-static struct resource wario_disp1_resources[] = {
-	{
-		.name	= "irq",
-		.start	= INT_DISPLAY_GENERAL,
-		.end	= INT_DISPLAY_GENERAL,
-		.flags	= IORESOURCE_IRQ,
-	},
-	{
-		.name	= "regs",
-		.start	= TEGRA_DISPLAY_BASE,
-		.end	= TEGRA_DISPLAY_BASE + TEGRA_DISPLAY_SIZE-1,
-		.flags	= IORESOURCE_MEM,
-	},
-	{
-		.name	= "fbmem",
-		.start	= 0x18012000,
-		.end	= 0x18012000 + 0x3e8000 - 1, /* enough for 1280*800 16bpp */
-		.flags	= IORESOURCE_MEM,
-	},
-};
-
 static struct tegra_dc_mode seaboard_panel_modes[] = {
 	{
 		.pclk = 62200000,
@@ -297,7 +276,6 @@ int __init seaboard_panel_init(void)
 	if (machine_is_wario()) {
 		seaboard_disp1_out.modes = wario_panel_modes;
 		seaboard_disp1_pdata.fb = &wario_fb_data;
-		seaboard_disp1_device.resource = wario_disp1_resources;
 	}
 
 	err = platform_add_devices(seaboard_gfx_devices,
