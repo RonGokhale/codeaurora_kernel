2e2f82b1cdd15255746f993b372fc7b873674849
Liam Girdwood <lrg@slimlogic.co.uk>
2011-02-01 21:09:38 +0000

    ASoC: core - allow ASoC more flexible machine name
    Allow ASoC machine drivers to register a driver name
    and a longname. This allows user space to determine
    the flavour of machine driver.
    
    Change-Id: I7f63d1d0c1fb79477c3bbb1ec4292fea43611a6a
    Signed-off-by: Liam Girdwood <lrg@slimlogic.co.uk>
    Signed-off-by: Patrick Lai <plai@codeaurora.org>

--- sound/soc/soc-core.c
+++ sound/soc/soc-core.c
@@ -1801,9 +1801,11 @@ static void snd_soc_instantiate_card(struct snd_soc_card *card)
 	}
 
 	snprintf(card->snd_card->shortname, sizeof(card->snd_card->shortname),
-		 "%s",  card->name);
-	snprintf(card->snd_card->longname, sizeof(card->snd_card->longname),
 		 "%s", card->name);
+	snprintf(card->snd_card->longname, sizeof(card->snd_card->longname),
+		 "%s", card->long_name ? card->long_name : card->name);
+	snprintf(card->snd_card->driver, sizeof(card->snd_card->driver),
+		 "%s", card->driver_name);
 
 	ret = snd_card_register(card->snd_card);
 	if (ret < 0) {
