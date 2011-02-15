57d42d7c3cab08f80a8d4f7f4796c7fcc679ebeb
Olof Johansson <olofj@chromium.org>
2011-02-14 17:44:09 -0800

    CHROMIUM: HACK: tegra: workaround reboot regression
    Change-Id: Ia7998009f97a08fd45f0ae09c9475cbe1efaada5
    Signed-off-by: Olof Johansson <olofj@chromium.org>
    
    BUG=chrome-os:11605
    TEST=emerge-tegra2 kernel, update_kernel, reboot
    
    Review URL: http://codereview.chromium.org/6529002

--- arch/arm/mach-tegra/common.c
+++ arch/arm/mach-tegra/common.c
@@ -128,7 +128,7 @@ static void tegra_pm_flush_console(void)
 static void tegra_pm_restart(char mode, const char *cmd)
 {
 	tegra_pm_flush_console();
-	arm_machine_restart(mode, cmd);
+	arch_reset(mode, cmd);
 }
 
 void __init tegra_common_init(void)
