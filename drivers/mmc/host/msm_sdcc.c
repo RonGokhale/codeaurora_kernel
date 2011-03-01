3ae4f312bd4fa4ec8bff894116dee85d78cb8c65
Sujith Reddy Thumma <sthumma@codeaurora.org>
2011-03-01 10:30:26 +0530

    mmc: msm_sdcc: Add software command timeout support
    There might be cases where the controller may be bad and the
    interrupts might be missed due to which the requests will be
    stalled indefinitely. Also some bad cards don't come out of
    programming state holding the prog_done line asserted and
    mmcqd thread stall indefinitely waiting for prog_done interrupt.
    
    Handle these cases with software timeouts in the driver file.
    Specification defines the timeout value to be much less than 10secs
    (around 1 sec) but we set the software command timeout to this
    value to handle the controller/card which don't meet the standards.
    
    Change-Id: I20c990633b15fea1a48dc55c81f95fe2eb5293c7
    CRs-fixed: 279156
    Signed-off-by: Sujith Reddy Thumma <sthumma@codeaurora.org>

--- drivers/mmc/host/msm_sdcc.c
+++ drivers/mmc/host/msm_sdcc.c
@@ -160,6 +160,8 @@ msmsdcc_request_end(struct msmsdcc_host *host, struct mmc_request *mrq)
 	host->curr.mrq = NULL;
 	host->curr.cmd = NULL;
 
+	del_timer(&host->req_tout_timer);
+
 	if (mrq->data)
 		mrq->data->bytes_xfered = host->curr.data_xfered;
 	if (mrq->cmd->error == -ETIMEDOUT)
@@ -288,7 +290,7 @@ msmsdcc_dma_complete_tlet(unsigned long data)
 			host->curr.mrq = NULL;
 			host->curr.cmd = NULL;
 			mrq->data->bytes_xfered = host->curr.data_xfered;
-
+			del_timer(&host->req_tout_timer);
 			spin_unlock_irqrestore(&host->lock, flags);
 
 			mmc_request_done(host->mmc, mrq);
@@ -475,6 +477,13 @@ msmsdcc_start_command_deferred(struct msmsdcc_host *host,
 		       mmc_hostname(host->mmc));
 	}
 	host->curr.cmd = cmd;
+
+	/*
+	 * Kick the software command timeout timer here.
+	 * Timer expires in 10 secs.
+	 */
+	mod_timer(&host->req_tout_timer,
+			(jiffies + msecs_to_jiffies(MSM_MMC_REQ_TIMEOUT)));
 }
 
 static void
@@ -1420,6 +1429,25 @@ static void msmsdcc_late_resume(struct early_suspend *h)
 };
 #endif
 
+static void msmsdcc_req_tout_timer_hdlr(unsigned long data)
+{
+	struct msmsdcc_host *host = (struct msmsdcc_host *)data;
+	struct mmc_command *cmd;
+	unsigned long flags;
+
+	spin_lock_irqsave(&host->lock, flags);
+	cmd = host->curr.cmd;
+	if (cmd) {
+		pr_info("%s: %s CMD%d\n", mmc_hostname(host->mmc),
+			__func__, cmd->opcode);
+		if (!cmd->error)
+			cmd->error = -ETIMEDOUT;
+		msmsdcc_reset_and_restore(host);
+		msmsdcc_request_end(host, cmd->mrq);
+	}
+	spin_unlock_irqrestore(&host->lock, flags);
+}
+
 static int
 msmsdcc_probe(struct platform_device *pdev)
 {
@@ -1683,6 +1711,8 @@ msmsdcc_probe(struct platform_device *pdev)
 		pm_runtime_enable(&(pdev)->dev);
 	}
 #endif
+	setup_timer(&host->req_tout_timer, msmsdcc_req_tout_timer_hdlr,
+			(unsigned long)host);
 
 	mmc_add_host(mmc);
 
@@ -1732,6 +1762,7 @@ msmsdcc_probe(struct platform_device *pdev)
 	return 0;
 
  platform_irq_free:
+	del_timer_sync(&host->req_tout_timer);
 	pm_runtime_disable(&(pdev)->dev);
 	pm_runtime_set_suspended(&(pdev)->dev);
 
@@ -1788,6 +1819,7 @@ static int msmsdcc_remove(struct platform_device *pdev)
 	if (!plat->status_irq)
 		sysfs_remove_group(&pdev->dev.kobj, &dev_attr_grp);
 
+	del_timer_sync(&host->req_tout_timer);
 	tasklet_kill(&host->dma_tlet);
 	mmc_remove_host(mmc);
 
