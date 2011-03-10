8f1e3d832fdeb6bc348701a3d65559a5d14a59ed
Manu Gautam <mgautam@codeaurora.org>
2011-03-10 18:16:10 +0530

    usb: msm_otg: Handle missing interrupts during PHY RESET
    During USB h/w reset sequence, interrupts are kept disabled for
    around 100ms. If some event like cable plug-in/plug-out happens
    during this window, then no interrupt is issued by hardware.
    Hence, once UBS h/w reset is complete, explicitly check for any
    pending events from PHY.
    
    Change-Id: I9b3e3ae615d8829ce625d5110b69d83d7abfb8f3
    CRs-Fixed: 278430
    Signed-off-by: Manu Gautam <mgautam@codeaurora.org>

--- drivers/usb/otg/msm72k_otg.c
+++ drivers/usb/otg/msm72k_otg.c
@@ -1312,7 +1312,7 @@ static void otg_reset(struct otg_transceiver *xceiv, int phy_reset)
 {
 	struct msm_otg *dev = container_of(xceiv, struct msm_otg, otg);
 	unsigned long timeout;
-	u32 mode;
+	u32 mode, work = 0;
 
 	clk_enable(dev->hs_clk);
 
@@ -1359,13 +1359,48 @@ reset_link:
 		mode = USBMODE_SDIS | USBMODE_HOST;
 	writel(mode, USB_USBMODE);
 
-	if (dev->otg.gadget)
+	if (dev->otg.gadget) {
 		enable_sess_valid(dev);
+		/* Due to the above 100ms delay, interrupts from PHY are
+		 * sometimes missed during fast plug-in/plug-out of cable.
+		 * Check for such cases here.
+		 */
+		if (is_b_sess_vld() && !test_bit(B_SESS_VLD, &dev->inputs)) {
+			pr_debug("%s: handle missing BSV event\n", __func__);
+			set_bit(B_SESS_VLD, &dev->inputs);
+			work = 1;
+		} else if (!is_b_sess_vld() && test_bit(B_SESS_VLD,
+				&dev->inputs)) {
+			pr_debug("%s: handle missing !BSV event\n", __func__);
+			clear_bit(B_SESS_VLD, &dev->inputs);
+			work = 1;
+		}
+	}
+
 #ifdef CONFIG_USB_EHCI_MSM
-	if (dev->otg.host)
+	if (dev->otg.host) {
 		enable_idgnd(dev);
+		/* Handle missing ID_GND interrupts during fast PIPO */
+		if (is_host() && test_bit(ID, &dev->inputs)) {
+			pr_debug("%s: handle missing ID_GND event\n", __func__);
+			clear_bit(ID, &dev->inputs);
+			work = 1;
+		} else if (!is_host() && !test_bit(ID, &dev->inputs)) {
+			pr_debug("%s: handle missing !ID_GND event\n",
+						__func__);
+			set_bit(ID, &dev->inputs);
+			work = 1;
+		}
+	} else {
+		disable_idgnd(dev);
+	}
 #endif
 	enable_idabc(dev);
+
+	if (work) {
+		wake_lock(&dev->wlock);
+		queue_work(dev->wq, &dev->sm_work);
+	}
 }
 
 static void msm_otg_sm_work(struct work_struct *w)
@@ -1502,11 +1537,6 @@ static void msm_otg_sm_work(struct work_struct *w)
 
 			/* Workaround: Reset phy after session */
 			otg_reset(&dev->otg, 1);
-
-			/* come back later to put hardware in
-			 * lpm. This removes addition checks in
-			 * suspend routine for missing BSV
-			 */
 			work = 1;
 		} else if (test_bit(B_BUS_REQ, &dev->inputs) &&
 				dev->otg.gadget->b_hnp_enable &&
@@ -1532,11 +1562,6 @@ static void msm_otg_sm_work(struct work_struct *w)
 #endif
 			/* Workaround: Reset PHY in SE1 state */
 			otg_reset(&dev->otg, 1);
-			if (!is_b_sess_vld()) {
-				clear_bit(B_SESS_VLD, &dev->inputs);
-				work = 1;
-				break;
-			}
 			pr_debug("entering into lpm with wall-charger\n");
 			msm_otg_put_suspend(dev);
 			/* Allow idle power collapse */
