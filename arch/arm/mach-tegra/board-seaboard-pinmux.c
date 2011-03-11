be1aa4645b50b03543b539e6604cedd3cd2a30b1
Jon Kliegman <kliegs@chromium.org>
2011-03-11 14:13:28 -0500

    CHROMIUM: Enable i2c setup for atmel_mxt_ts
    The atmel_mxt_ts driver is an i2c driver and as such needs platform-specific
    data setup in the board_seaboard.c file to enable the i2c lines (and setup
    the IRQ/reset GPIO lines as well).
    
    BUG=chromium-os:11929
    TEST=Tested in conjunction with other CL's in the bug
      Specific tests:
        Booted with atmel_mxt_ts module disabled. Validated kernel and system
          work fine
        Booted with atmel_mxt_ts module enabled.  Validated kernel and system
          work fine and atmel_mxt_ts module is setup on i2c properly
    
    Review URL: http://codereview.chromium.org/6623033
    
    Change-Id: Ib7c76c93df43bfba12ade207bdcc8ec9e61228cf
    
    Signed-off-by: Jon Kliegman <kliegs@chromium.org>
    [olofj: fixups for .38]
    Signed-off-by: Olof Johansson <olofj@chromium.org>

--- arch/arm/mach-tegra/board-seaboard-pinmux.c
+++ arch/arm/mach-tegra/board-seaboard-pinmux.c
@@ -199,6 +199,8 @@ static struct tegra_gpio_table common_gpio_table[] = {
 	{ .gpio = TEGRA_GPIO_NCT1008_THERM2_IRQ,.enable = true },
 	{ .gpio = TEGRA_GPIO_WLAN_POWER,	.enable = true },
 	{ .gpio = TEGRA_GPIO_AC_ONLINE,		.enable = true },
+	{ .gpio = TEGRA_GPIO_MXT_RST,		.enable = true },
+	{ .gpio = TEGRA_GPIO_MXT_IRQ,		.enable = true },
 };
 
 static void __init update_pinmux(struct tegra_pingroup_config *newtbl, int size)
