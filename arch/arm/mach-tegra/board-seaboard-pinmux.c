c2f8e20e2b0fc1cc7450c4647473a81d76410a65
Olof Johansson <olof@lixom.net>
2011-03-18 02:42:41 -0700

    CHROMIUM: ARM: tegra: seaboard: add nct1008 device registration
    Signed-off-by: Olof Johansson <olof@lixom.net>

--- arch/arm/mach-tegra/board-seaboard-pinmux.c
+++ arch/arm/mach-tegra/board-seaboard-pinmux.c
@@ -196,6 +196,7 @@ static struct tegra_gpio_table common_gpio_table[] = {
 	{ .gpio = TEGRA_GPIO_ISL29018_IRQ,	.enable = true },
 	{ .gpio = TEGRA_GPIO_CDC_IRQ,		.enable = true },
 	{ .gpio = TEGRA_GPIO_USB1,		.enable = true },
+	{ .gpio = TEGRA_GPIO_NCT1008_THERM2_IRQ,.enable = true },
 };
 
 static void __init update_pinmux(struct tegra_pingroup_config *newtbl, int size)
