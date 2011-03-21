b6f666b419f44e46b4403b53440be32c155dfba8
Andrew Chew <achew@nvidia.com>
2011-03-21 15:00:27 -0700

    CHROMIUM: seaboard: Clock and pinmux changes for camera
    This corrects some clock and pinmux settings for the V4L2 camera stuff.
    
    Change-Id: Ibfb86677d75b76081fe1369105354c20afaa8e19
    
    BUG=none
    TEST=Builds cleanly.  Tested to work with Tegra's V4L2 stuff.
    Signed-off-by: Andrew Chew <achew@nvidia.com>
    Signed-off-by: Olof Johansson <olofj@chromium.org>
    
    Review URL: http://codereview.chromium.org/6591019

--- arch/arm/mach-tegra/board-seaboard.c
+++ arch/arm/mach-tegra/board-seaboard.c
@@ -89,10 +89,11 @@ static struct platform_device debug_uart = {
 static __initdata struct tegra_clk_init_table seaboard_clk_init_table[] = {
 	/* name		parent		rate		enabled */
 	{ "clk_m",	NULL,		12000000,	true},
+	{ "pll_m",	"clk_m",	600000000,	true},
 	{ "3d",		"pll_m",	300000000,	false},
 	{ "2d",		"pll_m",	300000000,	false},
-	{ "vi",		"pll_m",	50000000,	true},
-	{ "vi_sensor",	"pll_m",	100000000,	false},
+	{ "vi",		"pll_m",	150000000,	false},
+	{ "vi_sensor",	"pll_m",	18200000,	false},
 	{ "epp",	"pll_m",	300000000,	false},
 	{ "mpe",	"pll_m",	100000000,	false},
 	{ "emc",	"pll_m",	600000000,	true},
