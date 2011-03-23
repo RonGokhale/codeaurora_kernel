f8ace81e2a54a242445159afaa498813720540ff
Chris Wolfe <cwolfe@chromium.org>
2011-03-23 15:03:29 -0400

    CHROMIUM: arm: tegra: seaboard: Setup for mpu3050
    The mpu3050 device on the Seaboard platform needs explicit setup of i2c and interrupt line.
    
    Contributed by Chris Wolfe <cwolfe@chromium.org>
    
    Change-Id: Ibd1ecf74f66f72f423acf5917df4b8a87e169d9a
    
    BUG=chromium-os:13280
    TEST=On Seaboard with skeletal driver, device generates interrupts and data. Seaboard without driver behaves normally.
    
    Review URL: http://codereview.chromium.org/6711071
    
    Signed-off-by: Chris Wolfe <cwolfe@chromium.org>
    [olofj: fixups for .38]
    Signed-off-by: Olof Johansson <olofj@chromium.org>

--- arch/arm/mach-tegra/board-seaboard-pinmux.c
+++ arch/arm/mach-tegra/board-seaboard-pinmux.c
@@ -202,6 +202,7 @@ static struct tegra_gpio_table common_gpio_table[] = {
 	{ .gpio = TEGRA_GPIO_AC_ONLINE,		.enable = true },
 	{ .gpio = TEGRA_GPIO_MXT_RST,		.enable = true },
 	{ .gpio = TEGRA_GPIO_MXT_IRQ,		.enable = true },
+	{ .gpio = TEGRA_GPIO_MPU3050_IRQ,	.enable = true },
 };
 
 static void __init update_pinmux(struct tegra_pingroup_config *newtbl, int size)
