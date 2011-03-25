5f22c2fa750165a631c4b7969468b9d9b80518e7
Gustavo F. Padovan <padovan@profusion.mobi>
2011-03-25 00:39:48 -0300

    Bluetooth: Move bt_accept_enqueue() to outside __l2cap_chan_add
    bt_accept_enqueue() is not really a channel action, so do it outside.
    This patch is part of a set of patches to create an struct l2cap_chan to
    have a clear separation between the struct sock and the L2CAP channel
    stuff.
    
    Signed-off-by: Gustavo F. Padovan <padovan@profusion.mobi>
    (cherry picked from commit d1010240fa9aac93da56a683c1295e759ee69d10)
    
    Change-Id: I440e1fe389a457ed8fadbcc8e0243da688dd8d29
    Signed-off-by: Mat Martineau <mathewm@codeaurora.org>

--- net/bluetooth/l2cap_core.c
+++ net/bluetooth/l2cap_core.c
@@ -474,7 +474,7 @@ static inline void l2cap_chan_unlink(struct l2cap_chan_list *l, struct sock *sk)
 	__sock_put(sk);
 }
 
-static void __l2cap_chan_add(struct l2cap_conn *conn, struct sock *sk, struct sock *parent)
+static void __l2cap_chan_add(struct l2cap_conn *conn, struct sock *sk)
 {
 	struct l2cap_chan_list *l = &conn->chan_list;
 
@@ -513,9 +513,6 @@ static void __l2cap_chan_add(struct l2cap_conn *conn, struct sock *sk, struct so
 	 */
 
 	__l2cap_chan_link(l, sk);
-
-	if (parent)
-		bt_accept_enqueue(parent, sk);
 }
 
 /* Delete channel.
@@ -933,7 +930,9 @@ static void l2cap_le_conn_ready(struct l2cap_conn *conn)
 	bacpy(&bt_sk(sk)->src, conn->src);
 	bacpy(&bt_sk(sk)->dst, conn->dst);
 
-	__l2cap_chan_add(conn, sk, parent);
+	bt_accept_enqueue(parent, sk);
+
+	__l2cap_chan_add(conn, sk);
 
 	l2cap_sock_set_timer(sk, sk->sk_sndtimeo);
 
@@ -1087,11 +1086,11 @@ static void l2cap_conn_del(struct hci_conn *hcon, int err)
 	}
 }
 
-static inline void l2cap_chan_add(struct l2cap_conn *conn, struct sock *sk, struct sock *parent)
+static inline void l2cap_chan_add(struct l2cap_conn *conn, struct sock *sk)
 {
 	struct l2cap_chan_list *l = &conn->chan_list;
 	write_lock_bh(&l->lock);
-	__l2cap_chan_add(conn, sk, parent);
+	__l2cap_chan_add(conn, sk);
 	write_unlock_bh(&l->lock);
 }
 
@@ -1181,7 +1180,7 @@ int l2cap_do_connect(struct sock *sk)
 	/* Update source addr of the socket */
 	bacpy(src, conn->src);
 
-	l2cap_chan_add(conn, sk, NULL);
+	l2cap_chan_add(conn, sk);
 
 	BT_DBG("hcon->state %d", (int) hcon->state);
 
@@ -3730,7 +3729,9 @@ static inline int l2cap_connect_req(struct l2cap_conn *conn, struct l2cap_cmd_hd
 	l2cap_pi(sk)->psm  = psm;
 	l2cap_pi(sk)->dcid = scid;
 
-	__l2cap_chan_add(conn, sk, parent);
+	bt_accept_enqueue(parent, sk);
+
+	__l2cap_chan_add(conn, sk);
 	dcid = l2cap_pi(sk)->scid;
 
 	l2cap_sock_set_timer(sk, sk->sk_sndtimeo);
