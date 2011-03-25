045008770b5e07678a882bba9631d07b17bb96c9
Jessica Gonzalez <jgaona@codeaurora.org>
2011-03-25 11:28:53 -0700

    msm: kgsl: Don't panic if the size of a memdesc is zero
    Its okay to call kgsl_sharedmem_free with a empty memdesc
    struture, just quietly ignore it.
    
    (cherry picked from commit 261986cd37dd8fa24a522113eb23786196396526)
    
    Conflicts:
    
    	drivers/gpu/msm/kgsl_sharedmem.c
    
    Change-Id: Ic43db13437d1e8bda013d14e29c539636342266d
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>
    Signed-off-by: Jessica Gonzalez <jgaona@codeaurora.org>

--- drivers/gpu/msm/kgsl_sharedmem.c
+++ drivers/gpu/msm/kgsl_sharedmem.c
@@ -282,13 +282,16 @@ kgsl_sharedmem_free(struct kgsl_memdesc *memdesc)
 			shmem, memdesc, memdesc->physaddr, memdesc->size);
 
 	BUG_ON(memdesc == NULL);
-	BUG_ON(memdesc->size <= 0);
 
-	if (memdesc->priv & KGSL_MEMFLAGS_CONPHYS)
-		dma_free_coherent(NULL, memdesc->size, memdesc->hostptr,
-				  memdesc->physaddr);
-	else
-		gen_pool_free(shmem->pool, memdesc->physaddr, memdesc->size);
+	if (memdesc->size > 0) {
+		if (memdesc->priv & KGSL_MEMFLAGS_CONPHYS)
+			dma_free_coherent(NULL, memdesc->size, 
+					memdesc->hostptr,
+				  	memdesc->physaddr);
+		else
+			gen_pool_free(shmem->pool, memdesc->physaddr, 
+					memdesc->size);
+	}
 
 	memset(memdesc, 0, sizeof(struct kgsl_memdesc));
 	KGSL_MEM_VDBG("return\n");
