a5697c32d1fab26141cefe414adffc889f4f263b
Grant Grundler <grundler@chromium.org>
2011-03-28 18:12:04 -0700

    brcmfmac: Fix suspend/resume
    Change-Id: I0754b3c54e90aa6808c052c9ff19ba1409e2bb24
    Signed-off-by: Venkat Rao <vrao@broadcom.com>
    Signed-off-by: Grant Grundler <grundler@chromium.org>
    
    BUG=none
    TEST=built/booted on tegra2_seabord. WIFI suspend/resume doesn't panic.
    
    Review URL: http://codereview.chromium.org/6488018

--- drivers/staging/brcm80211/brcmfmac/wl_cfg80211.c
+++ drivers/staging/brcm80211/brcmfmac/wl_cfg80211.c
@@ -40,6 +40,8 @@
 #include <linux/firmware.h>
 #include <wl_cfg80211.h>
 
+void sdioh_sdio_set_host_pm_flags(int flag);
+
 static struct sdio_func *cfg80211_sdio_func;
 static struct wl_dev *wl_cfg80211_dev;
 static const u8 ether_bcast[ETH_ALEN] = {255, 255, 255, 255, 255, 255};
@@ -1978,8 +1980,6 @@ static s32 wl_cfg80211_suspend(struct wiphy *wiphy)
 	struct net_device *ndev = wl_to_ndev(wl);
 	s32 err = 0;
 
-	CHECK_SYS_UP();
-
 	set_bit(WL_STATUS_SCAN_ABORTING, &wl->status);
 	wl_term_iscan(wl);
 	if (wl->scan_request) {
@@ -1990,6 +1990,8 @@ static s32 wl_cfg80211_suspend(struct wiphy *wiphy)
 	}
 	clear_bit(WL_STATUS_SCANNING, &wl->status);
 	clear_bit(WL_STATUS_SCAN_ABORTING, &wl->status);
+
+	sdioh_sdio_set_host_pm_flags(MMC_PM_KEEP_POWER);
 
 	return err;
 }
