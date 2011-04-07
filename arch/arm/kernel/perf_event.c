68498f1b8dfade479b7720a47eea11e277664c6a
Neil Leeder <nleeder@codeaurora.org>
2011-04-07 13:41:39 -0400

    ARM: perfevents: Add support for MSM Krait performance counters
    Change-Id: I0eee715fe26d9877f027b2ddf84f4c88ba4a40ac
    Signed-off-by: Neil Leeder <nleeder@codeaurora.org>

--- arch/arm/kernel/perf_event.c
+++ arch/arm/kernel/perf_event.c
@@ -627,6 +627,7 @@ static struct pmu pmu = {
 #include "perf_event_v6.c"
 #include "perf_event_v7.c"
 #include "perf_event_msm.c"
+#include "perf_event_msm_krait.c"
 
 static int __init
 init_hw_perf_events(void)
@@ -672,6 +673,10 @@ init_hw_perf_events(void)
 			break;
 		case 0x02D0:    /* 8x60 */
 			armpmu = armv7_scorpionmp_pmu_init();
+			break;
+		case 0x0490:    /* 8960 sim */
+		case 0x04D0:    /* 8960 */
+			armpmu = armv7_krait_pmu_init();
 			break;
 		}
 	}
