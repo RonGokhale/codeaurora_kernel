29335f571f3769ceba47a4f556961490ef4ac9b5
Rajakumar Govindaram <crajaku@codeaurora.org>
2011-04-07 18:22:55 -0700

    msm: camera: Add support for platform specific mount angle
    The camera preview Fluid is rotated due to mount angle
    variation between fluid and FFA.
    
    Change-Id: I7bb8d5f11285ab5009793fa268a097ddd06a75e5
    Signed-off-by: Rajakumar Govindaram <crajaku@codeaurora.org>

--- arch/arm/mach-msm/board-msm7x30.c
+++ arch/arm/mach-msm/board-msm7x30.c
@@ -1051,6 +1051,11 @@ static struct platform_device msm_camera_sensor_mt9d112 = {
 #endif
 
 #ifdef CONFIG_WEBCAM_OV9726
+
+static struct msm_camera_sensor_platform_info ov9726_sensor_7630_info = {
+	.mount_angle = 90
+};
+
 static struct msm_camera_sensor_flash_data flash_ov9726 = {
 	.flash_type	= MSM_CAMERA_FLASH_LED,
 	.flash_src	= &msm_flash_src_pwm
@@ -1065,6 +1070,7 @@ static struct msm_camera_sensor_info msm_camera_sensor_ov9726_data = {
 	.resource	= msm_camera_resources,
 	.num_resources	= ARRAY_SIZE(msm_camera_resources),
 	.flash_data	= &flash_ov9726,
+	.sensor_platform_info = &ov9726_sensor_7630_info,
 	.csi_if		= 1
 };
 struct platform_device msm_camera_sensor_ov9726 = {
