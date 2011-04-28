a3fcdba4e202a81f32a068ca6e8017419075916a
Sunid Wilson <sunidw@codeaurora.org>
2011-04-28 12:07:41 -0700

    msm: gemini: Set IJpeg clock to 228MHz
    Set IJpeg clock to 228MHz from 144MHz.
    
    Change-Id: Ibb1dbc7890b50113c4b0318aa4669d7f04bc0cad
    Signed-off-by: Sunid Wilson <sunidw@codeaurora.org>

--- drivers/media/video/msm/msm_io_8x60.c
+++ drivers/media/video/msm/msm_io_8x60.c
@@ -464,7 +464,7 @@ int msm_camio_clk_enable(enum msm_camio_clk_type clktype)
 	case CAMIO_JPEG_CLK:
 		camio_jpeg_clk =
 		clk = clk_get(NULL, "ijpeg_clk");
-		clk_set_min_rate(clk, 144000000);
+		msm_camio_clk_rate_set_2(clk, 228571000);
 		break;
 
 	case CAMIO_JPEG_PCLK:
