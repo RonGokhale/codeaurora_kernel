79d9f16cc5368fa82b2aa62590b3529f33ad7d19
Rohit Vaswani <rvaswani@codeaurora.org>
2011-05-03 02:08:38 -0700

    msm: gpio-v2: Add support for SDC1 tlmm
    8960 uses SDC1 and SDC3 while 8660 uses SDC3 and SDC4.
    This patch adds support for SDC1 tlmm registers as required by 8960
    
    Change-Id: I90be4acf8e187ac343cb1ce6d7dad854931e2c42
    Signed-off-by: Rohit Vaswani <rvaswani@codeaurora.org>

--- arch/arm/mach-msm/gpio-v2.c
+++ arch/arm/mach-msm/gpio-v2.c
@@ -72,6 +72,7 @@ enum {
 enum msm_tlmm_register {
 	SDC4_HDRV_PULL_CTL = 0x20a0,
 	SDC3_HDRV_PULL_CTL = 0x20a4,
+	SDC1_HDRV_PULL_CTL = 0x20a0,
 };
 
 struct tlmm_field_cfg {
@@ -86,13 +87,20 @@ static const struct tlmm_field_cfg tlmm_hdrv_cfgs[] = {
 	{SDC3_HDRV_PULL_CTL, 6}, /* TLMM_HDRV_SDC3_CLK  */
 	{SDC3_HDRV_PULL_CTL, 3}, /* TLMM_HDRV_SDC3_CMD  */
 	{SDC3_HDRV_PULL_CTL, 0}, /* TLMM_HDRV_SDC3_DATA */
+	{SDC1_HDRV_PULL_CTL, 6}, /* TLMM_HDRV_SDC1_CLK  */
+	{SDC1_HDRV_PULL_CTL, 3}, /* TLMM_HDRV_SDC1_CMD  */
+	{SDC1_HDRV_PULL_CTL, 0}, /* TLMM_HDRV_SDC1_DATA */
 };
 
 static const struct tlmm_field_cfg tlmm_pull_cfgs[] = {
 	{SDC4_HDRV_PULL_CTL, 11}, /* TLMM_PULL_SDC4_CMD  */
 	{SDC4_HDRV_PULL_CTL, 9},  /* TLMM_PULL_SDC4_DATA */
+	{SDC3_HDRV_PULL_CTL, 14}, /* TLMM_PULL_SDC3_CLK  */
 	{SDC3_HDRV_PULL_CTL, 11}, /* TLMM_PULL_SDC3_CMD  */
 	{SDC3_HDRV_PULL_CTL, 9},  /* TLMM_PULL_SDC3_DATA */
+	{SDC1_HDRV_PULL_CTL, 13}, /* TLMM_PULL_SDC1_CLK  */
+	{SDC1_HDRV_PULL_CTL, 11}, /* TLMM_PULL_SDC1_CMD  */
+	{SDC1_HDRV_PULL_CTL, 9},  /* TLMM_PULL_SDC1_DATA */
 };
 
 /*
