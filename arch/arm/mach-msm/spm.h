79cc0d370201a5d7de08e188505031d228bbe2a4
Maheshkumar Sivasubramanian <msivasub@codeaurora.org>
2011-05-09 11:50:03 -0600

    msm: kconfig: config support for SPM version 1 and 2.
    Enable config support for the different versions of Subsystem Power
    Manager(SPM) in msm chipsets. SPM allows the CPU to enter low power
    modes when idle. MSM chipsets 7X30 and 8660 use version 1
    of the SPM hardware. Chipset 8960 supports version 2 of the SPM
    hardware.
    
    Change-Id: I1124cad93285331bd67fa4beb3e1abc4326522f4
    Signed-off-by: Maheshkumar Sivasubramanian <msivasub@codeaurora.org>

--- arch/arm/mach-msm/spm.h
+++ arch/arm/mach-msm/spm.h
@@ -36,7 +36,43 @@ enum {
 	MSM_SPM_MODE_NR
 };
 
-#if defined(CONFIG_ARCH_MSM8960)
+#if defined(CONFIG_MSM_SPM_V1)
+
+enum {
+	MSM_SPM_REG_SAW_AVS_CTL,
+	MSM_SPM_REG_SAW_CFG,
+	MSM_SPM_REG_SAW_SPM_CTL,
+	MSM_SPM_REG_SAW_SPM_SLP_TMR_DLY,
+	MSM_SPM_REG_SAW_SPM_WAKE_TMR_DLY,
+	MSM_SPM_REG_SAW_SLP_CLK_EN,
+	MSM_SPM_REG_SAW_SLP_HSFS_PRECLMP_EN,
+	MSM_SPM_REG_SAW_SLP_HSFS_POSTCLMP_EN,
+	MSM_SPM_REG_SAW_SLP_CLMP_EN,
+	MSM_SPM_REG_SAW_SLP_RST_EN,
+	MSM_SPM_REG_SAW_SPM_MPM_CFG,
+	MSM_SPM_REG_NR_INITIALIZE,
+
+	MSM_SPM_REG_SAW_VCTL = MSM_SPM_REG_NR_INITIALIZE,
+	MSM_SPM_REG_SAW_STS,
+	MSM_SPM_REG_SAW_SPM_PMIC_CTL,
+	MSM_SPM_REG_NR
+};
+
+struct msm_spm_platform_data {
+	void __iomem *reg_base_addr;
+	uint32_t reg_init_values[MSM_SPM_REG_NR_INITIALIZE];
+
+	uint8_t awake_vlevel;
+	uint8_t retention_vlevel;
+	uint8_t collapse_vlevel;
+	uint8_t retention_mid_vlevel;
+	uint8_t collapse_mid_vlevel;
+
+	uint32_t vctl_timeout_us;
+};
+
+#elif defined(CONFIG_MSM_SPM_V2)
+
 enum {
 	MSM_SPM_REG_SAW2_SECURE,
 	MSM_SPM_REG_SAW2_ID,
@@ -74,45 +110,9 @@ struct msm_spm_platform_data {
 	uint32_t num_modes;
 	struct msm_spm_seq_entry *modes;
 };
-
-#else
-enum {
-	MSM_SPM_REG_SAW_AVS_CTL,
-	MSM_SPM_REG_SAW_CFG,
-	MSM_SPM_REG_SAW_SPM_CTL,
-	MSM_SPM_REG_SAW_SPM_SLP_TMR_DLY,
-	MSM_SPM_REG_SAW_SPM_WAKE_TMR_DLY,
-	MSM_SPM_REG_SAW_SLP_CLK_EN,
-	MSM_SPM_REG_SAW_SLP_HSFS_PRECLMP_EN,
-	MSM_SPM_REG_SAW_SLP_HSFS_POSTCLMP_EN,
-	MSM_SPM_REG_SAW_SLP_CLMP_EN,
-	MSM_SPM_REG_SAW_SLP_RST_EN,
-	MSM_SPM_REG_SAW_SPM_MPM_CFG,
-	MSM_SPM_REG_NR_INITIALIZE,
-
-	MSM_SPM_REG_SAW_VCTL = MSM_SPM_REG_NR_INITIALIZE,
-	MSM_SPM_REG_SAW_STS,
-	MSM_SPM_REG_SAW_SPM_PMIC_CTL,
-	MSM_SPM_REG_NR
-};
-
-struct msm_spm_platform_data {
-	void __iomem *reg_base_addr;
-	uint32_t reg_init_values[MSM_SPM_REG_NR_INITIALIZE];
-
-	uint8_t awake_vlevel;
-	uint8_t retention_vlevel;
-	uint8_t collapse_vlevel;
-	uint8_t retention_mid_vlevel;
-	uint8_t collapse_mid_vlevel;
-
-	uint32_t vctl_timeout_us;
-};
 #endif
 
-#if defined(CONFIG_ARCH_MSM7X30) \
-		|| defined(CONFIG_ARCH_MSM8X60) \
-			|| defined(CONFIG_ARCH_MSM8960)
+#if defined(CONFIG_MSM_SPM_V1) || defined(CONFIG_MSM_SPM_V2)
 
 int msm_spm_set_low_power_mode(unsigned int mode, bool notify_rpm);
 int msm_spm_set_vdd(unsigned int cpu, unsigned int vlevel);
@@ -142,12 +142,6 @@ static inline void msm_spm_allow_x_cpu_set_vdd(bool allowed)
 	/* empty */
 }
 
-static inline int msm_spm_init(struct msm_spm_platform_data *data, int nr_devs)
-{
-	return -ENOSYS;
-}
-
-#endif  /* defined(CONFIG_ARCH_MSM7X30) || defined(CONFIG_ARCH_MSM8X60)
-	   || defined (CONFIG_ARCH_MSM8960) */
+#endif  /*defined(CONFIG_MSM_SPM_V1) || defined (CONFIG_MSM_SPM_V2) */
 
 #endif  /* __ARCH_ARM_MACH_MSM_SPM_H */
