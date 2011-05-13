418761a6368679a64c18fc5f19f9d77bffa54e99
Larry Bassel <lbassel@codeaurora.org>
2011-05-13 16:23:51 -0700

    msm: add portable APIs to allocate EBI memory
    On 7x30 and 8x55 EBI memory is EBI0 and on other
    platforms is EBI1. Add analogs of allocate_contiguous_memory()
    and allocate_contiguous_memory_nomap() which hide this
    difference so drivers don't need to special case calls
    to these APIs.
    
    Change-Id: Ifbb85a7c03afde25399fe44d1f161b729e7f7b1f
    Signed-off-by: Larry Bassel <lbassel@codeaurora.org>

--- arch/arm/mach-msm/include/mach/memory.h
+++ arch/arm/mach-msm/include/mach/memory.h
@@ -58,6 +58,8 @@
 
 #ifndef __ASSEMBLY__
 void *alloc_bootmem_aligned(unsigned long size, unsigned long alignment);
+void *allocate_contiguous_ebi(unsigned long, unsigned long, int);
+unsigned long allocate_contiguous_ebi_nomap(unsigned long, unsigned long);
 void clean_and_invalidate_caches(unsigned long, unsigned long, unsigned long);
 void clean_caches(unsigned long, unsigned long, unsigned long);
 void invalidate_caches(unsigned long, unsigned long, unsigned long);
