421076d99a8c7daf8b3d423a03c98ca096698a0d
Duncan Laurie <dlaurie@chromium.org>
2011-05-12 17:12:47 -0700

    CHROMIUM: i915: Add DMI override to skip CRT initialization on ZGB
    This is the same method used to override LVDS in intel_lvds and appears to be
    the most effective way to ensure that the driver does not enable VGA hotplug.
    
    Signed-off-by: Duncan Laurie <dlaurie@chromium.org>
    
    BUG=chrome-os-partner:117
    TEST=Check PORT_HOTPLUG_EN to see if hotplug interrupt is disabled.
    Run the following command as root, specifically looking at bit 9:
    mmio_read32 $[$(pci_read32 0 2 0 0x10) + 0x61110] = 0x00000000
    
    (cherry picked from commit 09ecdd5b43b9df930efd51a3fe317c18907a6c2d)
    Change-Id: I40f46b651e6434c98e81531c1643c9f0a04dadd2
    Reviewed-on: http://gerrit.chromium.org/gerrit/949
    Tested-by: Duncan Laurie <dlaurie@chromium.org>
    Reviewed-by: Puneet Kumar <puneetster@chromium.org>

--- drivers/gpu/drm/i915/intel_crt.c
+++ drivers/gpu/drm/i915/intel_crt.c
@@ -24,6 +24,7 @@
  *	Eric Anholt <eric@anholt.net>
  */
 
+#include <linux/dmi.h>
 #include <linux/i2c.h>
 #include "drmP.h"
 #include "drm.h"
@@ -499,12 +500,34 @@ static const struct drm_encoder_funcs intel_crt_enc_funcs = {
 	.destroy = intel_crt_enc_destroy,
 };
 
+static int __init intel_no_crt_dmi_callback(const struct dmi_system_id *id)
+{
+	DRM_DEBUG_KMS("Skipping CRT initialization for %s\n", id->ident);
+	return 1;
+}
+
+static const struct dmi_system_id intel_no_crt[] = {
+	{
+		.callback = intel_no_crt_dmi_callback,
+		.ident = "ACER ZGB",
+		.matches = {
+			DMI_MATCH(DMI_SYS_VENDOR, "ACER"),
+			DMI_MATCH(DMI_PRODUCT_NAME, "ZGB"),
+		},
+	},
+	{ }
+};
+
 void intel_crt_init(struct drm_device *dev)
 {
 	struct drm_connector *connector;
 	struct intel_output *intel_output;
 	struct drm_i915_private *dev_priv = dev->dev_private;
 	u32 i2c_reg;
+
+	/* Skip machines without VGA that falsely report hotplug events */
+	if (dmi_check_system(intel_no_crt))
+		return;
 
 	intel_output = kzalloc(sizeof(struct intel_output), GFP_KERNEL);
 	if (!intel_output)
