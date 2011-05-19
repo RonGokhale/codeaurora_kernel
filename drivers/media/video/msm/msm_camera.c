4f3eed7b231db08296a5663d5c22e4d39be3ec10
Nagesh Subba Reddy <nageshsreddy@codeaurora.org>
2011-05-19 16:03:36 -0700

    msm: camera: Release sensor and clock in error case.
    Release sensor and clock in error case.
    Clean up of the event queue while msm_release to handle special
    case where config thread was never opened.
    
    Change-Id: Ie581691fc7d8b60da10ecaf8a60e386bca6dfa5e
    CRs-Fixed: 287648
    Signed-off-by: Nagesh Subba Reddy <nageshsreddy@codeaurora.org>

--- drivers/media/video/msm/msm_camera.c
+++ drivers/media/video/msm/msm_camera.c
@@ -3052,6 +3052,7 @@ static int __msm_release(struct msm_sync *sync)
 			kfree(region);
 		}
 		msm_queue_drain(&sync->pict_q, list_pict);
+		msm_queue_drain(&sync->event_q, list_config);
 
 		wake_unlock(&sync->wake_lock);
 		sync->apps_id = NULL;
@@ -3720,6 +3721,7 @@ static int __msm_open(struct msm_sync *sync, const char *const apps_id,
 			if (rc < 0) {
 				pr_err("%s: sensor init failed: %d\n",
 					__func__, rc);
+				msm_camio_sensor_clk_off(sync->pdev);
 				goto msm_open_done;
 			}
 			rc = sync->vfefn.vfe_init(&msm_vfe_s,
@@ -3727,6 +3729,8 @@ static int __msm_open(struct msm_sync *sync, const char *const apps_id,
 			if (rc < 0) {
 				pr_err("%s: vfe_init failed at %d\n",
 					__func__, rc);
+				sync->sctrl.s_release();
+				msm_camio_sensor_clk_off(sync->pdev);
 				goto msm_open_done;
 			}
 		} else {
