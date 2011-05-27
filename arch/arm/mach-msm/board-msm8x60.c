fd450c9b1f311bb07c8d68828e041701a3d18dca
Jeff Ohlstein <johlstei@codeaurora.org>
2011-05-27 13:19:54 -0700

    msm: board-msm8x60: Make dmov an early device
    It is important that the datamover be registered before its clients are,
    so that it is suspended after they are. This way, they can clean up
    their transfers, which allows the datamover to turn off its clocks, thus
    allowing the system to enter XO shutdown.
    
    Change-Id: Ia579ced99b548d7e09fe3222cedb5154dedeab7d
    Signed-off-by: Jeff Ohlstein <johlstei@codeaurora.org>

--- arch/arm/mach-msm/board-msm8x60.c
+++ arch/arm/mach-msm/board-msm8x60.c
@@ -3644,6 +3644,8 @@ static struct platform_device *early_devices[] __initdata = {
 	&msm_bus_sys_fpb,
 	&msm_bus_cpss_fpb,
 #endif
+	&msm_device_dmov_adm0,
+	&msm_device_dmov_adm1,
 };
 
 #if (defined(CONFIG_MARIMBA_CORE)) && \
@@ -4486,8 +4488,6 @@ static struct platform_device *charm_devices[] __initdata = {
 static struct platform_device *surf_devices[] __initdata = {
 	&msm_device_smd,
 	&msm_device_uart_dm12,
-	&msm_device_dmov_adm0,
-	&msm_device_dmov_adm1,
 #ifdef CONFIG_I2C_QUP
 	&msm_gsbi3_qup_i2c_device,
 	&msm_gsbi4_qup_i2c_device,
