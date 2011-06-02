08258826631d1c6083c4870aaeac086cc42eb9e8
Matt Wagantall <mattw@codeaurora.org>
2011-06-02 13:48:33 -0700

    msm: clock: Expand CLK_MIN, CLK_MAX and CLK_MINMAX macros
    These were just defined to be the same as CLKFLAG_MIN,
    CLKFLAG_MAX and (CLKFLAG_MIN | CLKFLAG_MAX). Expand them
    to avoid the unnecessary level of redirection.
    
    Change-Id: I6264bf8233d909b98da77dcb449116fc8aa594b7
    Signed-off-by: Matt Wagantall <mattw@codeaurora.org>

--- arch/arm/mach-msm/clock-debug.c
+++ arch/arm/mach-msm/clock-debug.c
@@ -31,15 +31,15 @@ static int clock_debug_rate_set(void *data, u64 val)
 
 	/* Only increases to max rate will succeed, but that's actually good
 	 * for debugging purposes so we don't check for error. */
-	if (clock->flags & CLK_MAX)
+	if (clock->flags & CLKFLAG_MAX)
 		clk_set_max_rate(clock, val);
-	if (clock->flags & CLK_MIN)
+	if (clock->flags & CLKFLAG_MIN)
 		ret = clk_set_min_rate(clock, val);
 	else
 		ret = clk_set_rate(clock, val);
 	if (ret != 0)
 		printk(KERN_ERR "clk_set%s_rate failed (%d)\n",
-			(clock->flags & CLK_MIN) ? "_min" : "", ret);
+			(clock->flags & CLKFLAG_MIN) ? "_min" : "", ret);
 	return ret;
 }
 
