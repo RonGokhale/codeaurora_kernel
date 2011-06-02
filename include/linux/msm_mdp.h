cde0d4265de0662e2f0b119477381e7f8c10aa77
Zhang Chang Ken <kenz@codeaurora.org>
2011-06-01 20:22:24 -0400

    msm_fb: display: YUV 444 format support
    Signed-off-by: Zhang Chang Ken <kenz@codeaurora.org>
    (cherry picked from commit b11c3b46619d3eaee600223859041a51fa0af218)
    
    Change-Id: Ia57a4dd092811f1430367ba7285f15b4e37e6247
    Signed-off-by: Shruthi Krishna <skrish@codeaurora.org>

--- include/linux/msm_mdp.h
+++ include/linux/msm_mdp.h
@@ -80,6 +80,8 @@ enum {
 	MDP_Y_CBCR_H2V2_TILE,  /* Y and CbCr, pseudo planer tile */
 	MDP_Y_CR_CB_H2V2,  /* Y, Cr and Cb, planar */
 	MDP_Y_CB_CR_H2V2,  /* Y, Cb and Cr, planar */
+	MDP_Y_CRCB_H1V1,  /* Y and CrCb, pseduo planer w/ Cr is in MSB */
+	MDP_Y_CBCR_H1V1,  /* Y and CbCr, pseduo planer w/ Cb is in MSB */
 	MDP_IMGTYPE_LIMIT,
 	MDP_BGR_565 = MDP_IMGTYPE2_START,      /* BGR 565 planer */
 	MDP_FB_FORMAT,    /* framebuffer format */
