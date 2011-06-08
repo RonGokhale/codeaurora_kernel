c18d3ef334fe23dd174088faa12ba433fb9e4dbd
Praveen Chidambaram <pchidamb@codeaurora.org>
2011-06-08 13:19:36 -0600

    msm: no-pm: Implement no-pm implementation of hotplug fns
    Change-Id: I5192d80cedfa6a026cd4af989ec711d6b8b66b88
    Signed-off-by: Praveen Chidambaram <pchidamb@codeaurora.org>

--- arch/arm/mach-msm/pm.h
+++ arch/arm/mach-msm/pm.h
@@ -1,7 +1,7 @@
 /* arch/arm/mach-msm/pm.h
  *
  * Copyright (C) 2007 Google, Inc.
- * Copyright (c) 2009-2010, Code Aurora Forum. All rights reserved.
+ * Copyright (c) 2009-2011, Code Aurora Forum. All rights reserved.
  * Author: San Mehat <san@android.com>
  *
  * This software is licensed under the terms of the GNU General Public
@@ -52,12 +52,15 @@ struct msm_pm_platform_data {
 };
 
 void msm_pm_set_platform_data(struct msm_pm_platform_data *data, int count);
-void msm_pm_set_rpm_wakeup_irq(unsigned int irq);
 int msm_pm_idle_prepare(struct cpuidle_device *dev);
 int msm_pm_idle_enter(enum msm_pm_sleep_mode sleep_mode);
 
-#ifdef CONFIG_HOTPLUG_CPU
+#ifdef CONFIG_PM
+void msm_pm_set_rpm_wakeup_irq(unsigned int irq);
 int msm_pm_platform_secondary_init(unsigned int cpu);
+#else
+static inline void msm_pm_set_rpm_wakeup_irq(unsigned int irq) {}
+static inline int msm_pm_platform_secondary_init(unsigned int cpu)
+{ return -ENOSYS; }
 #endif
-
 #endif  /* __ARCH_ARM_MACH_MSM_PM_H */
