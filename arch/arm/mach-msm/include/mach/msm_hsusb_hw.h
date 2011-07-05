c342838cd573b2fbfade16cfb365a5af7df6cd12
Chiranjeevi Velempati <cvelempa@codeaurora.org>
2011-07-05 17:27:05 +0530

    msm: 7x27A: Differentiate 7x27 from 7x27A
    CONFIG_ARCH_MSM7X27 is defined for both 7x27 and 7x27A. In order
    to differentiate 7x27 and 7x27A, CONFIG_ARCH_MSM7X27A should be
    used, since CONFIG_ARCH_MSM7X27A is defined only for 7x27A and
    not defined for 7x27.
    
    CRs-Fixed: 291120
    CRs-Fixed: 292599
    Change-Id: Ia0c12350c76831c85e0955fb650b37d582c25ab6
    Signed-off-by: Chiranjeevi Velempati <cvelempa@codeaurora.org>

--- arch/arm/mach-msm/include/mach/msm_hsusb_hw.h
+++ arch/arm/mach-msm/include/mach/msm_hsusb_hw.h
@@ -161,7 +161,8 @@ struct ept_queue_item {
 #define CTRL_RXT_EP_TYPE_SHIFT 2
 
 #define ULPI_CONFIG_REG		0x31
-#if defined(CONFIG_ARCH_MSM7X27) || defined(CONFIG_ARCH_QSD8X50)
+#if (defined(CONFIG_ARCH_MSM7X27) && !defined(CONFIG_ARCH_MSM7X27A)) \
+					|| defined(CONFIG_ARCH_QSD8X50)
 #define ULPI_DIGOUT_CTRL	0X31
 #define ULPI_CDR_AUTORESET	(1 << 5)
 #else
