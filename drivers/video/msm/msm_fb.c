514b196c41f9e2c978b0d540f7d27a748f0264b7
kuogee hsieh <khsieh@codeaurora.org>
2011-07-21 15:06:59 -0700

    msm_fb: display: add ioctl for mixer info
    Add new ioctl to allow user to query mdp mixer detail configuration.
    
    Signed-off-by: Kuogee Hsieh <khsieh@codeaurora.org>
    (cherry picked from commit 51159ca5df7bb860abdda9e09eba8cb39e2e5d15)
    
    Change-Id: Ie22e272d2356289e0bf286464381e23ea9ef3581
    Signed-off-by: Shruthi Krishna <skrish@codeaurora.org>
    Signed-off-by: Raj Kushwaha <rajk@codeaurora.org>

--- drivers/video/msm/msm_fb.c
+++ drivers/video/msm/msm_fb.c
@@ -2535,8 +2535,7 @@ static int msmfb_overlay_blt(struct fb_info *info, unsigned long *argp)
 
 	ret = copy_from_user(&req, argp, sizeof(req));
 	if (ret) {
-		printk(KERN_ERR "%s:msmfb_overlay_blt ioctl failed\n",
-			__func__);
+		pr_err("%s: failed\n", __func__);
 		return ret;
 	}
 
@@ -2549,6 +2548,12 @@ static int msmfb_overlay_blt_off(struct fb_info *info, unsigned long *argp)
 {
 	int	ret;
 	struct msmfb_overlay_blt req;
+
+	ret = copy_from_user(&req, argp, sizeof(req));
+	if (ret) {
+		pr_err("%s: failed\n", __func__);
+		return ret;
+	}
 
 	ret = mdp4_overlay_blt_offset(info, &req);
 
@@ -2585,6 +2590,27 @@ static int msmfb_overlay_3d_sbys(struct fb_info *info, unsigned long *argp)
 	ret = mdp4_overlay_3d_sbys(info, &req);
 
 	return ret;
+}
+
+static int msmfb_mixer_info(struct fb_info *info, unsigned long *argp)
+{
+	int     ret, cnt;
+	struct msmfb_mixer_info_req req;
+
+	ret = copy_from_user(&req, argp, sizeof(req));
+	if (ret) {
+		pr_err("%s: failed\n", __func__);
+		return ret;
+	}
+
+	cnt = mdp4_mixer_info(req.mixer_num, req.info);
+	req.cnt = cnt;
+	ret = copy_to_user(argp, &req, sizeof(req));
+	if (ret)
+		pr_err("%s:msmfb_overlay_blt_off ioctl failed\n",
+		__func__);
+
+	return cnt;
 }
 
 #endif
@@ -2709,6 +2735,11 @@ static int msm_fb_ioctl(struct fb_info *info, unsigned int cmd,
 	case MSMFB_OVERLAY_3D:
 		down(&msm_fb_ioctl_ppp_sem);
 		ret = msmfb_overlay_3d_sbys(info, argp);
+		up(&msm_fb_ioctl_ppp_sem);
+		break;
+	case MSMFB_MIXER_INFO:
+		down(&msm_fb_ioctl_ppp_sem);
+		ret = msmfb_mixer_info(info, argp);
 		up(&msm_fb_ioctl_ppp_sem);
 		break;
 #endif
