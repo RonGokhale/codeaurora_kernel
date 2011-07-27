5c37647240bd18cf4d90948a6520d8f2be7df46d
Jeff Hugo <jhugo@codeaurora.org>
2011-07-27 14:15:06 -0600

    msm: rmnet: reschedule read tasklet in low memory conditions
    If the read tasklet is unable to allocate a skb due to low memory
    conditions, reschedule the tasklet to retry at some point in the future.
    
    Change-Id: I481c309c3e64052d0ee1fa24b8e2428d92518a5a
    CRs-Fixed: 295596
    Signed-off-by: Jeffrey Hugo <jhugo@codeaurora.org>

--- drivers/net/msm_rmnet.c
+++ drivers/net/msm_rmnet.c
@@ -237,6 +237,9 @@ static __be16 rmnet_ip_type_trans(struct sk_buff *skb, struct net_device *dev)
 	return protocol;
 }
 
+static void smd_net_data_handler(unsigned long arg);
+static DECLARE_TASKLET(smd_net_data_tasklet, smd_net_data_handler, 0);
+
 /* Called in soft-irq context */
 static void smd_net_data_handler(unsigned long arg)
 {
@@ -263,6 +266,10 @@ static void smd_net_data_handler(unsigned long arg)
 			skb = dev_alloc_skb(sz + NET_IP_ALIGN);
 			if (skb == NULL) {
 				pr_err("rmnet_recv() cannot allocate skb\n");
+				/* out of memory, reschedule a later attempt */
+				smd_net_data_tasklet.data = (unsigned long)dev;
+				tasklet_schedule(&smd_net_data_tasklet);
+				break;
 			} else {
 				skb->dev = dev;
 				skb_reserve(skb, NET_IP_ALIGN);
@@ -305,8 +312,6 @@ static void smd_net_data_handler(unsigned long arg)
 			pr_err("rmnet_recv() smd lied about avail?!");
 	}
 }
-
-static DECLARE_TASKLET(smd_net_data_tasklet, smd_net_data_handler, 0);
 
 static int _rmnet_xmit(struct sk_buff *skb, struct net_device *dev)
 {
