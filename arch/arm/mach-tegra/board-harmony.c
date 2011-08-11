33f0c4423c2063106617804b86099d55550630c1
Doug Anderson <dianders@chromium.org>
2011-08-11 14:51:11 -0700

    CHROMIUM: tegra: harmony: wifi: Fix harmony_wifi_init() return val.
    The previous patch to harmony added harmony_wifi_init(), but
    didn't realize that function installed with subsys_initcall_sync()
    should return a value.  This caused a warning in the kernel compile.
    
    BUG=None
    TEST=No more compile-time warning.
    
    Change-Id: Ic115a6d25f8c33e43957a00af240a67ac3552c64
    Signed-off-by: Doug Anderson <dianders@chromium.org>
    Reviewed-on: http://gerrit.chromium.org/gerrit/5792
    Tested-by: Mursalin Akon <makon@nvidia.com>
    Reviewed-by: Mursalin Akon <makon@nvidia.com>

--- arch/arm/mach-tegra/board-harmony.c
+++ arch/arm/mach-tegra/board-harmony.c
@@ -293,7 +293,7 @@ static struct tegra_sdhci_platform_data sdhci_pdata4 = {
 	.is_8bit	= 1,
 };
 
-static void __init harmony_wifi_init(void)
+static int __init harmony_wifi_init(void)
 {
 	int gpio_pwr, gpio_rst;
 
@@ -310,6 +310,8 @@ static void __init harmony_wifi_init(void)
 		gpio_direction_output(TEGRA_GPIO_WLAN_PWR_LOW, 1);
 		gpio_direction_output(TEGRA_GPIO_WLAN_RST_LOW, 1);
 	}
+
+	return 0;
 }
 /* make harmony_wifi_init to be invoked at subsys_initcall_sync
  * to ensure the required regulators (LDO3 supply of external
