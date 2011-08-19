d0d525984afdba20eb2f0186c5720638abc057a2
Stepan Moskovchenko <stepanm@codeaurora.org>
2011-08-19 13:47:22 -0700

    msm: 8064: Add UART device for APQ8064 RUMI3
    Add the device declaration and initialization code for the
    GSBI1 UART device on the APQ8064 RUMI3.
    
    Change-Id: I7cba2e3937ef86dd7d9532ecf00654519e249b48
    Signed-off-by: Stepan Moskovchenko <stepanm@codeaurora.org>

--- arch/arm/mach-msm/board-apq8064.c
+++ arch/arm/mach-msm/board-apq8064.c
@@ -159,7 +159,6 @@ static struct platform_device android_usb_device = {
 };
 
 static struct platform_device *common_devices[] __initdata = {
-	&apq8064_device_uart_gsbi3,
 	&apq8064_device_qup_i2c_gsbi4,
 	&apq8064_device_qup_spi_gsbi5,
 	&apq8064_device_ssbi_pmic1,
@@ -172,6 +171,11 @@ static struct platform_device *common_devices[] __initdata = {
 
 static struct platform_device *sim_devices[] __initdata = {
 	&apq8064_device_dmov,
+	&apq8064_device_uart_gsbi3,
+};
+
+static struct platform_device *rumi3_devices[] __initdata = {
+	&apq8064_device_uart_gsbi1,
 };
 
 static struct msm_spi_platform_data apq8064_qup_spi_gsbi5_pdata = {
@@ -262,6 +266,7 @@ static void __init apq8064_sim_init(void)
 static void __init apq8064_rumi3_init(void)
 {
 	apq8064_common_init();
+	platform_add_devices(rumi3_devices, ARRAY_SIZE(rumi3_devices));
 }
 
 MACHINE_START(APQ8064_SIM, "QCT APQ8064 SIMULATOR")
