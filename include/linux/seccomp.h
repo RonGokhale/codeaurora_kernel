10341b00625a27dc2ac128244d25920ba5ae339a
Will Drewry <wad@chromium.org>
2011-08-24 16:45:01 -0500

    CHROMIUM: seccomp_filter: make inherited filters composable
    Right now, inheritance just copies the parent ruleset.  This means
    that on inheritance, if a child wants to reduce privilege, it must
    walk its inherited set to drop those it doesn't want.
    
    With this change, inherited sets are always left intact and any process
    local rules are created in a child seccomp_filter object.  On evaluation,
    the child rules are checked first, then all ancestors.  If the system call
    is not allowed by any of them, the process will be terminated.
    
    The change makes PR_SECCOMP_GET_FILTER less intuitive since it won't
    coalesce all inherited values, but it assures that a child process
    can create its ruleset normally, then call prctl(PR_SET_SECCOMP, 13) to
    enable it on top of anything that is inherited.
    
    This is similar to namespacing behavior except that the copy-on-update
    methodology precludes the parent process from modifying a filter that
    has been inherited.
    
    TEST=minijail0 -S support and the seccomp_tests.c in the seccomp filters bugs
    BUG=chromium-os:19459
    
    Change-Id: Icd192b75d887fbee09d99fe86cd1a815a8ae673d
    Signed-off-by: Will Drewry <wad@chromium.org>
    Reviewed-on: http://gerrit.chromium.org/gerrit/7109
    Reviewed-by: Elly Jones <ellyjones@chromium.org>

--- include/linux/seccomp.h
+++ include/linux/seccomp.h
@@ -31,6 +31,8 @@ struct seccomp_struct {
 #endif
 };
 
+typedef struct seccomp_struct seccomp_t;
+
 extern void __secure_computing(int);
 static inline void secure_computing(int this_syscall)
 {
@@ -51,6 +53,8 @@ static inline int seccomp_mode(seccomp_t *s)
 #include <linux/errno.h>
 
 struct seccomp_struct { };
+typedef struct seccomp_struct seccomp_t;
+
 #define secure_computing(x) do { } while (0)
 
 static inline long prctl_get_seccomp(void)
@@ -77,18 +81,8 @@ static inline int seccomp_mode(seccomp_t *s)
 	(_tsk)->seccomp.filters = NULL; \
 } while (0)
 
-/* Do nothing unless seccomp filtering is active. If not, the execve boundary
- * can not be cleanly enforced and preset filters may leak across execve calls.
- */
-#define seccomp_filter_fork(_tsk, _orig) do { \
-	if ((_tsk)->seccomp.mode) { \
-		(_tsk)->seccomp.mode = (_orig)->seccomp.mode; \
-		mutex_lock(&(_orig)->seccomp.filters_guard); \
-		(_tsk)->seccomp.filters = \
-			get_seccomp_filters((_orig)->seccomp.filters); \
-		mutex_unlock(&(_orig)->seccomp.filters_guard); \
-	} \
-} while (0)
+extern void seccomp_filter_fork(struct task_struct *child,
+				struct task_struct *parent);
 
 /* No locking is needed here because the task_struct will
  * have no parallel consumers.
@@ -102,6 +96,8 @@ extern int seccomp_show_filters(struct seccomp_filters *filters,
 extern long seccomp_set_filter(int, char *);
 extern long seccomp_clear_filter(int);
 extern long seccomp_get_filter(int, char *, unsigned long);
+
+extern long seccomp_enable_filters(void);
 
 extern long prctl_set_seccomp_filter(unsigned long, unsigned long,
 				     char __user *);
@@ -119,7 +115,8 @@ extern void seccomp_filter_log_failure(int);
 
 struct seccomp_filters { };
 #define seccomp_filter_init_task(_tsk) do { } while (0)
-#define seccomp_filter_fork(_tsk, _orig) do { } while (0)
+static inline void seccomp_filter_fork(struct task_struct *child,
+				       struct task_struct *parent) { }
 #define seccomp_filter_free_task(_tsk) do { } while (0)
 
 static inline int seccomp_show_filters(struct seccomp_filters *filters,
