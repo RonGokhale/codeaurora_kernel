b8c3aa8bdd5974b75a8782cab641936bfa7eb3f6
Nagamalleswararao Ganji <nganji@codeaurora.org>
2011-09-02 12:06:37 -0700

    msm-fb: display: mdp clock scaling clean up
    clean up of clock scaling code to organize it better
    
    Change-Id: I056c15de115367a33209c876a0d64cea85a02655
    Signed-off-by: Nagamalleswararao Ganji <nganji@codeaurora.org>

--- drivers/video/msm/mdp.c
+++ drivers/video/msm/mdp.c
@@ -1141,8 +1141,6 @@ int mdp_set_core_clk(uint16 perf_level)
 			printk(KERN_ERR "%s invalid perf level\n", __func__);
 		else {
 			mutex_lock(&mdp_clk_lock);
-			if (mdp4_extn_disp)
-				perf_level = 1;
 			ret = clk_set_rate(mdp_clk,
 				mdp_pdata->
 				mdp_core_clk_table[mdp_pdata->num_mdp_clk
@@ -1178,8 +1176,6 @@ unsigned long mdp_perf_level2clk_rate(uint32 perf_level)
 			printk(KERN_ERR "%s invalid perf level\n", __func__);
 			clk_rate = mdp_get_core_clk();
 		} else {
-			if (mdp4_extn_disp)
-				perf_level = 1;
 			clk_rate = mdp_pdata->
 				mdp_core_clk_table[mdp_pdata->num_mdp_clk
 					- perf_level];
