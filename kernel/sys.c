e603f4753085078614ee4488f9ea533659f0395c
Sergiu Iordache <sergiu@chromium.org>
2011-09-02 10:51:11 -0700

    CHROMIUM: Remove preserved file from repository
    Removed the preserved related files from the repository since it is no
    longer being used. Also removed some preserved-related code from sleep.c
    and printk.c that was not being used either. Changed emergency_restart to
    non-weak (as upstream) as it is no longer overwritten in preserved.
    
    BUG=chromium-os:12059
    TEST=Preserved removed from repo, kernel still builds
    
    Change-Id: I3dcaad469d1b61c4eb6dca70e3b290133678893d
    Signed-off-by: Sergiu Iordache <sergiu@chromium.org>
    Reviewed-on: http://gerrit.chromium.org/gerrit/7186
    Reviewed-by: Simon Glass <sjg@chromium.org>
    Reviewed-by: Mandeep Singh Baines <msb@chromium.org>
    Tested-by: Mandeep Singh Baines <msb@chromium.org>

--- kernel/sys.c
+++ kernel/sys.c
@@ -307,7 +307,7 @@ out_unlock:
  *	trouble so this is our best effort to reboot.  This is
  *	safe to call in interrupt context.
  */
-void __weak emergency_restart(void)	/* defer to kernel/preserved.c */
+void emergency_restart(void)
 {
 	kmsg_dump(KMSG_DUMP_EMERG);
 	machine_emergency_restart();
