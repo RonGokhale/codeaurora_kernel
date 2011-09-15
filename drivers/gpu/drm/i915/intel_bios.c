79fc321c6f25330b5c5965f8f21dd2ec385bf1a1
Simon Que <sque@chromium.org>
2011-09-15 14:53:10 -0700

    drivers: i915: select non-alternate SSC frequency for some systems
    When initializing VBT default values, the alternate BIOS SSC frequency
    is selected.  This patch allows the non-alternate frequency to be
    selected as a default, for specified systems.  There is a DMI match list
    for systems that are to use the non-alternate frequency.  Currently,
    only Lumpy is on the list.
    
    BUG=chrome-os-partner:5525
    TEST=Run on Lumpy with dev switch off.  The display should be working,
    even if the backlight is not -- something should be visible.
    
    Change-Id: I3802b46d1b43d77063825c4b26ad072b4e384ef5
    Signed-off-by: Simon Que <sque@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/11339
    Reviewed-by: Bryan Freed <bfreed@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/11501
    Tested-by: Stéphane Marchesin <marcheu@chromium.org>
    Reviewed-by: Mandeep Singh Baines <msb@chromium.org>

--- drivers/gpu/drm/i915/intel_bios.c
+++ drivers/gpu/drm/i915/intel_bios.c
@@ -25,6 +25,7 @@
  *
  */
 #include <drm/drm_dp_helper.h>
+#include <linux/dmi.h>
 #include "drmP.h"
 #include "drm.h"
 #include "i915_drm.h"
@@ -589,6 +590,17 @@ parse_device_mapping(struct drm_i915_private *dev_priv,
 	return;
 }
 
+static const struct dmi_system_id lvds_do_not_use_alternate_frequency[] = {
+	{
+		.callback = NULL,
+		.ident = "Lumpy",
+		.matches = {
+			DMI_MATCH(DMI_PRODUCT_NAME, "Lumpy"),
+		}
+	},
+	{ }
+};
+
 static void
 init_vbt_defaults(struct drm_i915_private *dev_priv)
 {
@@ -609,7 +621,10 @@ init_vbt_defaults(struct drm_i915_private *dev_priv)
 
 	/* Default to using SSC */
 	dev_priv->lvds_use_ssc = 1;
-	dev_priv->lvds_ssc_freq = intel_bios_ssc_frequency(dev, 1);
+	if (dmi_check_system(lvds_do_not_use_alternate_frequency))
+		dev_priv->lvds_ssc_freq = intel_bios_ssc_frequency(dev, 0);
+	else
+		dev_priv->lvds_ssc_freq = intel_bios_ssc_frequency(dev, 1);
 	DRM_DEBUG("Set default to SSC at %dMHz\n", dev_priv->lvds_ssc_freq);
 
 	/* eDP data */
