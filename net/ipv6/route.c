17fadd6d4dca68acd4d1f109a9a7e42fd878eaad
Madalin Bucur <madalin.bucur@freescale.com>
2011-09-26 07:04:56 +0000

    ipv6: check return value for dst_alloc
    return value of dst_alloc must be checked before use
    
    Signed-off-by: Madalin Bucur <madalin.bucur@freescale.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    
    Conflicts:
    
    	net/ipv6/route.c
    
    Change-Id: I2c009c4a413d86e429c077cfcc2aebabd65b468a
    Signed-off-by: Anusha Somireddy <casomir@codeaurora.org>

--- net/ipv6/route.c
+++ net/ipv6/route.c
@@ -187,7 +187,13 @@ static struct rt6_info ip6_blk_hole_entry_template = {
 /* allocate dst with ip6_dst_ops */
 static inline struct rt6_info *ip6_dst_alloc(struct dst_ops *ops)
 {
-	return (struct rt6_info *)dst_alloc(ops);
+	struct rt6_info *rt = dst_alloc(ops);
+
+	if (rt != NULL)
+		memset(&rt->rt6i_table, 0,
+			sizeof(*rt) - sizeof(struct dst_entry));
+
+	return rt;
 }
 
 static void ip6_dst_destroy(struct dst_entry *dst)
