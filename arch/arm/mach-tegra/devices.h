0fbc81caa7019f351f941233bf014560bed2776d
Taylor Hutt <thutt@chromium.org>
2011-10-21 12:21:25 -0700

    ASoC: Tegra: Add tegra_spdif_device
    Details
    
      This change adds the spdif device which will be needed to enable
      HDMI output.
    
    Testing
    
      Built kernel
      Built chromium-os image.
      Booted on Kaen.
    
    Change-Id: Iacd960948cd3246a8f9ccf9dcd3458ec814c62a1
    Signed-off-by: Taylor Hutt <thutt@chromium.org>
    Reviewed-on: http://gerrit.chromium.org/gerrit/10504
    Reviewed-by: Stephen Warren <swarren@nvidia.com>

--- arch/arm/mach-tegra/devices.h
+++ arch/arm/mach-tegra/devices.h
@@ -50,5 +50,6 @@ extern struct platform_device tegra_das_device;
 extern struct platform_device tegra_pcm_device;
 extern struct platform_device tegra_kbc_device;
 extern struct platform_device tegra_rtc_device;
+extern struct platform_device tegra_spdif_device;
 
 #endif
