ffd8c68a85db636c955192ed279c03983ce5153b
Sujit Reddy Thumma <sthumma@codeaurora.org>
2011-12-14 21:17:08 +0530

    mmc: msm_sdcc: Fix deadlock in mmc_suspend_host and mmc_rescan
    SDCC runtime suspend can race with mmc_rescan causing deadlock.
    The call flow would be this:
    1) When runtime suspend is triggered:
    	msmsdcc_runtime_suspend()
    		-> mmc_suspend_host()
    			-> mmc_flush_scheduled_work()
    2) mmc_flush_schedule_work() waits for mmc_rescan() work
    to be completed if it is already in runqueue.
    3) When mmc_rescan() is scheduled to run:
    	mmc_claim_host()
    		-> msmsdcc_enable()
    			->pm_runtime_get_sync()
    4) pm_runtime_get_sync() waits for runtime_suspend to complete,
    and hence cause two threads to wait upon each other.
    
    Fix this deadlock by aborting runtime suspend when mmc_rescan
    is scheduled.
    
    CRs-Fixed: 326610
    Signed-off-by: Sujit Reddy Thumma <sthumma@codeaurora.org>
    (cherry picked from commit 19859ef3f6229e52ae3b10550443dbbf41e80c47)
    
    Change-Id: I9c74a5d875392925d0d568c1e77b1a5a77b26587
    Signed-off-by: Srivalli Oguri <oguri@codeaurora.org>

--- drivers/mmc/host/msm_sdcc.c
+++ drivers/mmc/host/msm_sdcc.c
@@ -3939,7 +3939,11 @@ msmsdcc_runtime_suspend(struct device *dev)
 		 * simple become pm usage counter increment operations.
 		 */
 		pm_runtime_get_noresume(dev);
-		rc = mmc_suspend_host(mmc);
+		/* If there is pending detect work abort runtime suspend */
+		if (unlikely(work_busy(&mmc->detect.work)))
+			rc = -EAGAIN;
+		else
+			rc = mmc_suspend_host(mmc);
 		pm_runtime_put_noidle(dev);
 
 		if (!rc) {
@@ -3970,7 +3974,7 @@ msmsdcc_runtime_suspend(struct device *dev)
 		if (rc && wake_lock_active(&host->sdio_suspend_wlock))
 			wake_unlock(&host->sdio_suspend_wlock);
 	}
-	pr_debug("%s: %s: end\n", mmc_hostname(mmc), __func__);
+	pr_debug("%s: %s: ends with err=%d\n", mmc_hostname(mmc), __func__, rc);
 	return rc;
 }
 
