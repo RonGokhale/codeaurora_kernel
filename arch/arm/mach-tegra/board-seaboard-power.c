astarasikov/iconia-gnu-kernel

CHROMIUM: tegra: power: Turn off VDD_1V5 to save power
For most tegra2 platforms, VDD_1V5 is not used so turn it off to
save power, it can save ~12mW for all use cases.

Signed-off-by: Bill Huang <bilhuang@nvidia.com>

BUG=none
TEST=Comparing the power difference after kernel init, it should save ~12mW.

Change-Id: I78ff996c8d2a814e21a26796765af084cfee8bfa
Reviewed-on: http://gerrit.chromium.org/gerrit/1101
Tested-by: Bill Huang <bilhuang@nvidia.com>
Reviewed-by: Olof Johansson <olofj@chromium.org>

arch/arm/mach-tegra/board-seaboard-power.c
@@ -103,6 +103,29 @@ static struct fixed_voltage_config wwan_pwr = {
 	.init_data		= &wwan_pwr_initdata,
 };
 
+static struct regulator_consumer_supply vdd_1v5_consumer_supply[] = {
+	REGULATOR_SUPPLY("vdd_1v5", NULL),
+};
+
+struct regulator_init_data vdd_1v5_initdata = {
+	.consumer_supplies = vdd_1v5_consumer_supply,
+	.num_consumer_supplies = 1,
+	.constraints = {
+		.valid_ops_mask = REGULATOR_CHANGE_STATUS,
+		.always_on = 0,
+	},
+};
+
+static struct fixed_voltage_config vdd_1v5 = {
+	.supply_name		= "vdd_1v5",
+	.microvolts		= 1500000, /* 1.5V */
+	.gpio			= TPS_GPIO_EN_1V5,
+	.startup_delay		= 0,
+	.enable_high		= 0,
+	.enabled_at_boot	= 0,
+	.init_data		= &vdd_1v5_initdata,
+};
+
 #define REGULATOR_INIT(_id, _minmv, _maxmv, _always_on)			\
 	{								\
 		.constraints = {					\
@@ -167,6 +190,7 @@ static struct tps6586x_subdev_info tps_devs[] = {
 	TPS_REG(LDO_8, &ldo8_data),
 	TPS_REG(LDO_9, &ldo9_data),
 	TPS_GPIO_FIXED_REG(0, &wwan_pwr),
+	TPS_GPIO_FIXED_REG(1, &vdd_1v5),
 	{
 		.id	= 0,
 		.name	= "tps6586x-rtc",

RonGokhale/codeaurora_kernel

arch/arm/mach-tegra/board-seaboard-power.c
@@ -103,6 +103,29 @@ static struct fixed_voltage_config wwan_pwr = {
 	.init_data		= &wwan_pwr_initdata,
 };
 
+static struct regulator_consumer_supply vdd_1v5_consumer_supply[] = {
+	REGULATOR_SUPPLY("vdd_1v5", NULL),
+};
+
+struct regulator_init_data vdd_1v5_initdata = {
+	.consumer_supplies = vdd_1v5_consumer_supply,
+	.num_consumer_supplies = 1,
+	.constraints = {
+		.valid_ops_mask = REGULATOR_CHANGE_STATUS,
+		.always_on = 0,
+	},
+};
+
+static struct fixed_voltage_config vdd_1v5 = {
+	.supply_name		= "vdd_1v5",
+	.microvolts		= 1500000, /* 1.5V */
+	.gpio			= TEGRA_GPIO_EN_1V5,
+	.startup_delay		= 0,
+	.enable_high		= 0,
+	.enabled_at_boot	= 0,
+	.init_data		= &vdd_1v5_initdata,
+};
+
 #define REGULATOR_INIT(_id, _minmv, _maxmv, _always_on)			\
 	{								\
 		.constraints = {					\
@@ -163,6 +186,7 @@ static struct tps6586x_subdev_info tps_devs[] = {
 	TPS_REG(LDO_8, &ldo8_data),
 	TPS_REG(LDO_9, &ldo9_data),
 	TPS_GPIO_FIXED_REG(0, &wwan_pwr),
+	TPS_GPIO_FIXED_REG(1, &vdd_1v5),
 };
 
 static struct tps6586x_platform_data tps_platform = {