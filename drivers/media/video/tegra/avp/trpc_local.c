antonialoytorrens-archive/marvin24s-kernel

build fixes for 3.2
Signed-off-by: Olof Johansson <olof@lixom.net>

drivers/media/video/tegra/avp/trpc_local.c
@@ -30,6 +30,7 @@
 #include <linux/types.h>
 #include <linux/uaccess.h>
 #include <linux/wait.h>
+#include <linux/export.h>
 
 #include "trpc.h"
 #include "trpc_sema.h"

drivers/media/video/tegra/avp/trpc_sema.c
@@ -26,6 +26,7 @@
 #include <linux/types.h>
 #include <linux/uaccess.h>
 #include <linux/wait.h>
+#include <linux/export.h>
 
 #include "trpc_sema.h"
 

RonGokhale/codeaurora_kernel

drivers/media/video/tegra/avp/trpc_local.c
@@ -30,6 +30,7 @@
 #include <linux/types.h>
 #include <linux/uaccess.h>
 #include <linux/wait.h>
+#include <linux/module.h>
 
 #include "trpc.h"
 #include "trpc_sema.h"

drivers/media/video/tegra/avp/trpc_sema.c
@@ -26,6 +26,7 @@
 #include <linux/types.h>
 #include <linux/uaccess.h>
 #include <linux/wait.h>
+#include <linux/module.h>
 
 #include "trpc_sema.h"
 