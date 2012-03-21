280c8bf03b604aa5eb759d257e2a5b5f065fa38c
Rajkumar Manoharan <rmanohar@qca.qualcomm.com>
2012-03-21 09:47:15 +0530

    UPSTREAM: ath9k: cleanup rx stuck detection logic
    In the following scenario, where the distance b/w STA and AP is ~10m
    or in a shield environment by placing an attenuator with reduced AP
    txpower, the station started reporting with beacon loss and got
    disconnected whenever the chariot endpoint was initiated with BiDi
    traffic. In such state, two different stuck cases were observed.
    
      * rx clear is stuck at low for more than 100ms
      * dcu chain and complete state is stuck.
    
    This patch triggers stuck detection if three consecutive beacons are
    missed. If any of the above conditions matches, it triggers a chip
    reset to recover. This issue was originally reported in 3.0 kernel with
    AR9382 chip by having two stations associated with two different APs in
    the same channel and was attenuated/controlled by Azimuth ADEPT-n box.
    
    Cc: Paul Stewart <pstew@google.com>
    Cc: Gary Morain <gmorain@google.com>
    Signed-off-by: Rajkumar Manoharan <rmanohar@qca.qualcomm.com>
    
    BUG=chrome-os-partner:4886
    TEST=Torture testing Azimuth with hidden nodes; manual over-the-air
    testing.
    
    Originally-Reviewed-on: https://gerrit.chromium.org/gerrit/17986
    Signed-off-by: Paul Stewart <pstew@chromium.org>
    (cherry picked from commit f831f081912a9abe0a8756549c5ae41dc5630918)
    
    Change-Id: I03b0b45444ab8848196e7a99f0a1372204e1688a
    Reviewed-on: https://gerrit.chromium.org/gerrit/18629
    Reviewed-by: Gary Morain <gmorain@chromium.org>
    Commit-Ready: Paul Stewart <pstew@chromium.org>
    Tested-by: Paul Stewart <pstew@chromium.org>

--- drivers/net/wireless/ath/ath9k/recv.c
+++ drivers/net/wireless/ath/ath9k/recv.c
@@ -1851,11 +1851,9 @@ int ath_rx_tasklet(struct ath_softc *sc, int flush, bool hp)
 		if (retval)
 			goto requeue_drop_frag;
 
-		if (rs.is_mybeacon) {
-			atomic_set(&sc->stop_rx_poll, 1);
-			mod_timer(&sc->rx_poll_timer,
-				  jiffies + msecs_to_jiffies(300));
-		}
+		if (rs.is_mybeacon)
+			ath_start_rx_poll(sc, 300);
+
 		rxs->mactime = (tsf & ~0xffffffffULL) | rs.rs_tstamp;
 		if (rs.rs_tstamp > tsf_lower &&
 		    unlikely(rs.rs_tstamp - tsf_lower > 0x10000000))
