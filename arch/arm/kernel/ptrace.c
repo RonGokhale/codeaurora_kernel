4a02fcb597ac01846923b3b2dac9ece2922112af
Will Drewry <wad@chromium.org>
2012-04-16 16:37:17 -0500

    CHROMIUM: arch/arm: move secure_computing into trace; respect return code
    (I will post this upstream after the 3.5 merge window)
    
    There is very little difference in the TIF_SECCOMP and TIF_SYSCALL_TRACE
    patsh in entry-common.S. In order to add support for
    CONFIG_HAVE_ARCH_SECCOMP_FILTER without mangling the assembly too
    badly, seccomp was moved into the syscall_trace() handler.
    
    Additionally, the return value for secure_computing() is now checked
    and a -1 value will result in the system call being skipped.
    
    Change-Id: I0b241909a1a0a13774cfb113c654420322dcb825
    Signed-off-by: Will Drewry <wad@chromium.org>
    
    BUG=chromium-os:27878
    TEST=compiles for arm. Need to test on a live machine.
    
    Change-Id: I61470515539427a3f61667710715af8ee9d754da
    Reviewed-on: https://gerrit.chromium.org/gerrit/21376
    Reviewed-by: Will Drewry <wad@chromium.org>
    Tested-by: Will Drewry <wad@chromium.org>

--- arch/arm/kernel/ptrace.c
+++ arch/arm/kernel/ptrace.c
@@ -908,6 +908,11 @@ asmlinkage int syscall_trace(int why, struct pt_regs *regs, int scno)
 {
 	unsigned long ip;
 
+	current_thread_info()->syscall = scno;
+
+	/* Only check seccomp on entry. */
+	if (why == 0 && secure_computing(scno) == -1)
+		return -1;
 	if (!test_thread_flag(TIF_SYSCALL_TRACE))
 		return scno;
 	if (!(current->ptrace & PT_PTRACED))
@@ -919,8 +924,6 @@ asmlinkage int syscall_trace(int why, struct pt_regs *regs, int scno)
 	 */
 	ip = regs->ARM_ip;
 	regs->ARM_ip = why;
-
-	current_thread_info()->syscall = scno;
 
 	/* the 0x80 provides a way for the tracing parent to distinguish
 	   between a syscall stop and SIGTRAP delivery */
