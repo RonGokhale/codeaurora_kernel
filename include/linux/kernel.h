666805f17bedb5b9b3611a1b1774b1f16eab2528
Sam Leffler <sleffler@chromium.org>
2012-04-26 16:54:35 -0700

    CHROMIUM: Add CLOCK_SYSTEM_TRACE to export the trace clock to user space.
    Add a new clock type CLOCK_SYSTEM_TRACE to retrieve the current trace
    clock value (if CONFIG_TRACING is not defined then CLOCK_SYSTEM_TRACE
    returns the same value as CLOCK_MONOTONIC_RAW).  This allows user event
    tracing to timestamp data with the same clock as the kernel.  This is
    in-lieu of sending event data through trace_write (which is not a viable
    option when there are lots of user-space events).
    
    BUG=chromium-os:27809
    TEST=manual:run test app to collect trace time and compare to data generated by the system (e.g. reading from trace)
    
    Signed-off-by: Sam Leffler <sleffler@chromium.org>
    
    Change-Id: If2167ed99291e05f8c298638f4118b10b3e710f8
    Reviewed-on: https://gerrit.chromium.org/gerrit/21404
    Reviewed-by: Olof Johansson <olofj@chromium.org>
    Commit-Ready: Sam Leffler <sleffler@chromium.org>
    Tested-by: Sam Leffler <sleffler@chromium.org>

--- include/linux/kernel.h
+++ include/linux/kernel.h
@@ -434,6 +434,9 @@ enum ftrace_dump_mode {
 extern void tracing_start(void);
 extern void tracing_stop(void);
 extern void ftrace_off_permanent(void);
+struct timespec;
+extern void trace_clock_gettime(struct timespec *);
+extern void trace_clock_getres(struct timespec *);
 
 static inline __printf(1, 2)
 void ____trace_printk_check_format(const char *fmt, ...)
@@ -514,6 +517,15 @@ int trace_printk(const char *fmt, ...);
 static inline void tracing_start(void) { }
 static inline void tracing_stop(void) { }
 static inline void ftrace_off_permanent(void) { }
+struct timespec;
+static inline void trace_clock_gettime(struct timespec *tp)
+{
+	getrawmonotonic(tp);
+}
+static inline void trace_clock_getres(struct timespec *tp)
+{
+	*tp = ktime_to_timespec(KTIME_LOW_RES);
+}
 static inline void trace_dump_stack(void) { }
 static inline int
 trace_printk(const char *fmt, ...)
