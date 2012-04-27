e8513bb892765fb2becbc6e728a48612ad7c84ae
Mike Frysinger <vapier@chromium.org>
2012-04-27 06:29:37 -0700

    Revert "UPSTREAM: ALSA: HDA: Jack: Export required functions from hda_jack.c"
    This reverts commit afd43b519b60af27fec28e020e4f99270d893a1a
    
    The x86/amd64 kernel builds are broken:
    
    ERROR: "snd_hda_jack_add_kctl" [sound/pci/hda/snd-hda-codec-hdmi.ko] undefined
    
    http://build.chromium.org/p/chromiumos/builders/amd64%20generic%20full/builds/2165/steps/cbuildbot/logs/stdio
    Change-Id: I781d8b5cf2527ed878fab0f531b1fe36ce2ab1f0
    Reviewed-on: https://gerrit.chromium.org/gerrit/21314
    Commit-Ready: Mike Frysinger <vapier@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Tested-by: Mike Frysinger <vapier@chromium.org>

--- sound/pci/hda/hda_jack.c
+++ sound/pci/hda/hda_jack.c
@@ -68,7 +68,6 @@ snd_hda_jack_tbl_new(struct hda_codec *codec, hda_nid_t nid)
 	jack->jack_dirty = 1;
 	return jack;
 }
-EXPORT_SYMBOL_HDA(snd_hda_jack_tbl_new);
 
 void snd_hda_jack_tbl_clear(struct hda_codec *codec)
 {
@@ -249,7 +248,6 @@ int snd_hda_jack_add_kctl(struct hda_codec *codec, hda_nid_t nid,
 	jack->kctl = kctl;
 	return 0;
 }
-EXPORT_SYMBOL_HDA(snd_hda_jack_add_kctl);
 
 static int add_jack_kctl(struct hda_codec *codec, hda_nid_t nid, int idx,
 			 const struct auto_pin_cfg *cfg)
