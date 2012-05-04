7f8e579f124cd457dabf6793470aa34f3749b78d
Grant Grundler <grundler@chromium.org>
2012-05-04 22:37:41 +0000

    mac80211: add support to abort a scan request on tx
    Add support for NL80211_SCAN_FLAG_TX_ABORT that indicates outbound traffic
    should abort a scan request.  This is can be used (e.g. by wpa_supplicant)
    to cause low-priority scan requests to be aborted when traffic is present
    (reducing throughput dips).
    
    Signed-off-by: sleffler@chromium.org
    
    BUG=chromium-os:11485
    TEST=manual:run iw event -f in the background; run ping -i .25 <host>; wait for wpa_supplicant to issue a bgscan; note that the scan is aborted due to the ping; issue iw dev wlan0 scan and note the scan is not aborted and the ping packets are noticeably delayed
    
    Change-Id: I8f5fe42acb25e69beaaa506a88f42ec6384be3f7
    Reviewed-on: http://gerrit.chromium.org/gerrit/5745
    Reviewed-by: Gary Morain <gmorain@chromium.org>
    Reviewed-by: Paul Stewart <pstew@chromium.org>
    Commit-Ready: Sam Leffler <sleffler@chromium.org>
    Tested-by: Sam Leffler <sleffler@chromium.org>
    
    Conflicts:
    
    	net/mac80211/scan.c

--- net/mac80211/scan.c
+++ net/mac80211/scan.c
@@ -453,6 +453,7 @@ static void ieee80211_scan_state_decision(struct ieee80211_local *local,
 	unsigned long min_beacon_int = 0;
 	struct ieee80211_sub_if_data *sdata;
 	struct ieee80211_channel *next_chan;
+	enum mac80211_scan_state next_scan_state;
 
 	/*
 	 * check if at least one STA interface is associated,
@@ -511,18 +512,25 @@ static void ieee80211_scan_state_decision(struct ieee80211_local *local,
 				usecs_to_jiffies(min_beacon_int * 1024) *
 				local->hw.conf.listen_interval);
 
-		if (associated && ( !tx_empty || bad_latency ||
-		    listen_int_exceeded))
-			local->next_scan_state = SCAN_ENTER_OPER_CHANNEL;
+
+		if (associated && !tx_empty) {
+			if (unlikely(local->scan_req->flags &
+				CFG80211_SCAN_FLAG_TX_ABORT)) {
+				/*
+				 * Scan request is marked to abort when there
+				 * is outbound traffic.  Mark state to return
+				 * the operating channel and then abort.  This
+				 * happens as soon as possible.
+				 */
+				next_scan_state = SCAN_ENTER_OPER_CHANNEL_ABORT;
+			} else
+				next_scan_state = SCAN_ENTER_OPER_CHANNEL;
+		} else if (associated && (bad_latency || listen_int_exceeded))
+			next_scan_state = SCAN_ENTER_OPER_CHANNEL;
 		else
-			local->next_scan_state = SCAN_SET_CHANNEL;
-	} else {
-		/*
-		 * we're on the operating channel currently, let's
-		 * leave that channel now to scan another one
-		 */
-		local->next_scan_state = SCAN_LEAVE_OPER_CHANNEL;
+			next_scan_state = SCAN_SET_CHANNEL;
 	}
+	local->next_scan_state = next_scan_state;
 
 	*next_delay = 0;
 }
@@ -565,8 +573,13 @@ static void ieee80211_scan_state_enter_oper_channel(struct ieee80211_local *loca
 
 	__clear_bit(SCAN_OFF_CHANNEL, &local->scanning);
 
-	*next_delay = HZ / 5;
-	local->next_scan_state = SCAN_DECISION;
+	if (local->next_scan_state == SCAN_ENTER_OPER_CHANNEL) {
+		*next_delay = HZ / 5;
+		local->next_scan_state = SCAN_DECISION;
+	} else {
+		*next_delay = 0;
+		local->next_scan_state = SCAN_ABORT;
+	}
 }
 
 static void ieee80211_scan_state_set_channel(struct ieee80211_local *local,
@@ -714,8 +727,12 @@ void ieee80211_scan_work(struct work_struct *work)
 			ieee80211_scan_state_leave_oper_channel(local, &next_delay);
 			break;
 		case SCAN_ENTER_OPER_CHANNEL:
+		case SCAN_ENTER_OPER_CHANNEL_ABORT:
 			ieee80211_scan_state_enter_oper_channel(local, &next_delay);
 			break;
+		case SCAN_ABORT:
+			aborted = true;
+			goto out_complete;
 		}
 	} while (next_delay == 0);
 
