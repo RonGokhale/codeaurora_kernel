000d1caf1390d041f7f8a7a9a9265913809edb5c
Gopal G Goberu <ggober@codeaurora.org>
2012-07-31 08:22:52 +0530

    Revert "msm: 7x27a: audio: set LPA mode for LPA session"
    This reverts commit a2504dc8e016365ee7bbbc9b397e6e795cd62ba6.
    
    Signed-off-by: Gopal G Goberu <ggober@codeaurora.org>
    (cherry picked from commit 2774048c03c350448e397c11a03faf4bc39fe71c)
    
    Change-Id: Iaf7672476ba1dbe417c219685234081b030c75ae
    Signed-off-by: Gopal G Goberu <ggober@codeaurora.org>

--- arch/arm/mach-msm/qdsp5/audio_lpa.c
+++ arch/arm/mach-msm/qdsp5/audio_lpa.c
@@ -463,8 +463,7 @@ static int auddec_dsp_config(struct audio *audio, int enable)
 	cfg_dec_cmd[0] = AUDPP_CMD_CFG_DEC_TYPE;
 	if (enable)
 		cfg_dec_cmd[1 + audio->dec_id] = AUDPP_CMD_UPDATDE_CFG_DEC |
-				AUDPP_CMD_ENA_DEC_V | AUDDEC_DEC_PCM |
-				AUDPP_CMD_LPA_MODE;
+				AUDPP_CMD_ENA_DEC_V | AUDDEC_DEC_PCM;
 	else
 		cfg_dec_cmd[1 + audio->dec_id] = AUDPP_CMD_UPDATDE_CFG_DEC |
 				AUDPP_CMD_DIS_DEC_V;
