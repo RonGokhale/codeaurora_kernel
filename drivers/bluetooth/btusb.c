d48e1a8535306e107f721acd7bfcc7ef301193e7
Scott James Remnant <keybuk@chromium.org>
2012-09-06 09:06:04 -0700

    CHROMIUM: Bluetooth: disable MD222 based adapters
    TEST='hciconfig hci0' does not show device on affected hardware
    BUG=chrome-os-partner:12560
    
    Change-Id: Ieaaf753c9034633536ac26013614f9863705d604
    Signed-off-by: Scott James Remnant <keybuk@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/32403
    Reviewed-by: mukesh agrawal <quiche@chromium.org>
    Tested-by: mukesh agrawal <quiche@chromium.org>

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -137,10 +137,14 @@ static struct usb_device_id blacklist_table[] = {
 	{ USB_DEVICE(0x0cf3, 0x311d), .driver_info = BTUSB_ATH3012 },
 	{ USB_DEVICE(0x13d3, 0x3375), .driver_info = BTUSB_ATH3012 },
 	{ USB_DEVICE(0x04ca, 0x3005), .driver_info = BTUSB_ATH3012 },
-	{ USB_DEVICE(0x04ca, 0x3006), .driver_info = BTUSB_ATH3012 },
 	{ USB_DEVICE(0x13d3, 0x3362), .driver_info = BTUSB_ATH3012 },
 	{ USB_DEVICE(0x0cf3, 0xe004), .driver_info = BTUSB_ATH3012 },
-	{ USB_DEVICE(0x0489, 0xe057), .driver_info = BTUSB_ATH3012 },
+
+	/* Atheros MD222 */
+	{ USB_DEVICE(0x0489, 0xe04e), .driver_info = BTUSB_IGNORE },
+	{ USB_DEVICE(0x0489, 0xe057), .driver_info = BTUSB_IGNORE },
+	{ USB_DEVICE(0x04ca, 0x3006), .driver_info = BTUSB_IGNORE },
+	{ USB_DEVICE(0x04ca, 0x700b), .driver_info = BTUSB_IGNORE },
 
 	/* Atheros AR5BBU12 with sflash firmware */
 	{ USB_DEVICE(0x0489, 0xe02c), .driver_info = BTUSB_IGNORE },
