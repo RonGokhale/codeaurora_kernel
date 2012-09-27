358ce80b143abebb7913914b46facdf00ac50203
Mandeep Singh Baines <msb@chromium.org>
2012-09-27 10:12:18 -0700

    drm/exynos: print an error message when flipping a non-kds buffer
    This message was handy in finding a bug.
    
    BUG=chrome-os-partner:12170
    TEST=compiles
    
    Signed-off-by: Mandeep Singh Baines <msb@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/34193
    Reviewed-by: Anush Elangovan <anush@chromium.org>
    Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>
    
    (cherry picked from commit b07674485c532f07b15a0ac2e666abe8c8cdaf48)
    
    Change-Id: I994cea8113f3bcfc5359711558b2902018b12c31
    Signed-off-by: Mandeep Singh Baines <msb@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/34311
    Reviewed-by: Anush Elangovan <anush@google.com>

--- drivers/gpu/drm/exynos/exynos_drm_crtc.c
+++ drivers/gpu/drm/exynos/exynos_drm_crtc.c
@@ -399,6 +399,7 @@ static int exynos_drm_crtc_page_flip(struct drm_crtc *crtc,
 				  &exynos_fb->kds_cb, crtc, fb, 1, shared,
 				  resource_list);
 	} else {
+		DRM_ERROR("flipping a non-kds buffer\n");
 		exynos_drm_kds_callback(crtc, fb);
 	}
 #endif
