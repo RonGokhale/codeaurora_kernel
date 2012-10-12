d4325e38d41703fc4eadce832c38d3bc23dcbffd
Srikanth Uyyala <suyyala@codeaurora.org>
2012-10-12 17:45:36 +0530

    msm: Adding platfrom driver entry for avtimer
    registering avtimer platfrom driver
    
    Signed-off-by: Srikanth Uyyala <suyyala@codeaurora.org>
    (cherry picked from commit 7d4f7216d4f2595fc9b8eebbaac088976870b391)
    
    Change-Id: Ie2c3e4f90e974ea49a07ae46df01aa8f51b3c499
    Signed-off-by: Sridhar Gujje <sgujje@codeaurora.org>
    
    Conflicts:
    
    	arch/arm/mach-msm/devices-8064.c
    	arch/arm/mach-msm/devices.h
    
    Signed-off-by: Amuru SaiDeepika <amurus@codeaurora.org>

--- arch/arm/mach-msm/devices-8064.c
+++ arch/arm/mach-msm/devices-8064.c
@@ -19,6 +19,7 @@
 #include <linux/clkdev.h>
 #include <linux/dma-mapping.h>
 #include <linux/coresight.h>
+#include <linux/avtimer.h>
 #include <mach/irqs-8064.h>
 #include <mach/board.h>
 #include <mach/msm_iomap.h>
@@ -108,6 +109,10 @@
 #define MSM8064_RPM_MASTER_STATS_BASE	0x10BB00
 #define MSM8064_PC_CNTR_PHYS	(APQ8064_IMEM_PHYS + 0x664)
 #define MSM8064_PC_CNTR_SIZE		0x40
+#define MSM8064_RPM_MASTER_STATS_BASE	0x10BB00
+/* avtimer */
+#define AVTIMER_MSW_PHYSICAL_ADDRESS 0x2800900C
+#define AVTIMER_LSW_PHYSICAL_ADDRESS 0x28009008
 
 static struct resource msm8064_resources_pccntr[] = {
 	{
