d475254f6607b85b01d50fe44ccd991454c40fb7
Taniya Das <tdas@codeaurora.org>
2012-11-29 12:56:38 +0530

    msm: mdm: Fix error value for SHUTDOWN_CHARM ioctl
    SHUTDOWN_CHARM should write the error value to the parameter
    passed, but in this case as no error value is updated.
    
    CRs-Fixed: 424291
    Signed-off-by: Taniya Das <tdas@codeaurora.org>
    (cherry picked from commit d1ed37b3fff7e67c933868f6dd263624c8d91e22)
    
    Change-Id: I7831fa22a14710bc18d64985eb593d79c4b06ea1
    Signed-off-by: Sridhar Gujje <sgujje@codeaurora.org>
    
    Conflicts:
    
    	arch/arm/mach-msm/mdm_common.c
    
    Signed-off-by: Sudhir Sharma <sudsha@codeaurora.org>

--- arch/arm/mach-msm/mdm_common.c
+++ arch/arm/mach-msm/mdm_common.c
@@ -297,7 +297,8 @@ long mdm_modem_ioctl(struct file *filp, unsigned int cmd,
 		ret = sysmon_send_shutdown(SYSMON_SS_EXT_MODEM);
 		if (ret)
 			pr_err("%s: Graceful shutdown of the external modem failed, ret = %d\n",
-			   __func__, ret);
+				   __func__, ret);
+		put_user(ret, (unsigned long __user *) arg);
 		break;
 	default:
 		pr_err("%s: invalid ioctl cmd = %d\n", __func__, _IOC_NR(cmd));
