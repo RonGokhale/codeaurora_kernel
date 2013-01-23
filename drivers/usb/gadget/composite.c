9e0e3cedd7dad96c49558342d7ca5bdd00c9d480
Manu Gautam <mgautam@codeaurora.org>
2013-01-23 12:44:33 +0530

    usb: gadget: composite: Fix list corruption when changing configuration
    Don't remove the config from the configs list from unbind_config
    as it is already deleted from the list. Calling list_del again
    causes list corruption warning as list->next points to LIST_POISON.
    
    CRs-Fixed: 444144
    Change-Id: Id4da0d0e18ead63e20cb236cd1d3e8e6d116acce
    Signed-off-by: Manu Gautam <mgautam@codeaurora.org>

--- drivers/usb/gadget/composite.c
+++ drivers/usb/gadget/composite.c
@@ -814,7 +814,6 @@ static int unbind_config(struct usb_composite_dev *cdev,
 			/* may free memory for "f" */
 		}
 	}
-	list_del(&config->list);
 	if (config->unbind) {
 		DBG(cdev, "unbind config '%s'/%p\n", config->label, config);
 		config->unbind(config);
