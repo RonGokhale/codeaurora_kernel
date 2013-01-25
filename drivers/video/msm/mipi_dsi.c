63436fc9bed23185e537a5f1e4d210c2b64d5cc3
Praveena Pachipulusu <pveena@codeaurora.org>
2013-01-25 11:39:23 +0530

    Revert "msm_fb: display: fix dsi_busy_wait race condition"
    This reverts commit 79382cafdb6a91c4377b78ccc55f83b279f44c57.
    
    Conflicts:
    
       drivers/video/msm/mipi_dsi.c
    
    Change-Id: Ibafb95e0879ab16d9334868815e3d14e37a8c000
    Signed-off-by: Praveena Pachipulusu <pveena@codeaurora.org>

--- drivers/video/msm/mipi_dsi.c
+++ drivers/video/msm/mipi_dsi.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2008-2012, Code Aurora Forum. All rights reserved.
+/* Copyright (c) 2008-2013, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -68,8 +68,6 @@ static int mipi_dsi_off(struct platform_device *pdev)
 	int ret = 0;
 	struct msm_fb_data_type *mfd;
 	struct msm_panel_info *pinfo;
-
-	pr_debug("Start of %s....:\n", __func__);
 
 	mfd = platform_get_drvdata(pdev);
 	pinfo = &mfd->panel_info;
@@ -150,8 +148,6 @@ static int mipi_dsi_on(struct platform_device *pdev)
 	u32 ystride, bpp, data;
 	u32 dummy_xres, dummy_yres;
 	int target_type = 0;
-
-	pr_debug("Start of %s:....\n", __func__);
 
 	mfd = platform_get_drvdata(pdev);
 	fbi = mfd->fbi;
