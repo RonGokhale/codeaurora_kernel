f395147037458df734744ded2f21c42d059fd099
Praneeth Paladugu <ppaladug@codeaurora.org>
2013-01-27 17:49:36 -0800

    msm: vidc: Add support for video decoder extradata
    Add support for timeinfo and aspect ratio extradata
    by setting the corresponding properties to Venus FW.
    
    Change-Id: Ie33a451c8088d4bfe169c770fa054a73cdfcf78f
    Signed-off-by: Praneeth Paladugu <ppaladug@codeaurora.org>

--- include/uapi/linux/videodev2.h
+++ include/uapi/linux/videodev2.h
@@ -717,6 +717,9 @@ struct v4l2_buffer {
 #define V4L2_BUF_FLAG_NO_CACHE_INVALIDATE	0x0800
 #define V4L2_BUF_FLAG_NO_CACHE_CLEAN		0x1000
 #define V4L2_BUF_FLAG_EOS		0x2000
+#define V4L2_QCOM_BUF_FLAG_CODECCONFIG  0x4000
+#define V4L2_QCOM_BUF_FLAG_EOSEQ  0x8000
+#define V4L2_QCOM_BUF_TIMESTAMP_INVALID 0x10000
 
 /**
  * struct v4l2_exportbuffer - export of video buffer as DMABUF file descriptor
@@ -1578,8 +1581,6 @@ struct v4l2_encoder_cmd {
 	};
 };
 
-#define V4L2_QCOM_BUF_FLAG_CODECCONFIG	0x4000
-#define V4L2_QCOM_BUF_FLAG_EOSEQ  0x8000
 
 /* Decoder commands */
 #define V4L2_DEC_CMD_START       (0)
