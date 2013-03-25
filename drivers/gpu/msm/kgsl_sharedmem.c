9acdf56951d42653ace4844511b38b82e0f1037c
Jordan Crouse <jcrouse@codeaurora.org>
2013-03-25 11:04:13 -0600

    msm: kgsl: Properly record the size of the sglist in the memdesc
    It was possible to fail out of kgsl_sharedmem_alloc after a sglist was
    allocated but before we saved the size of the allocation which caused
    problems when the sglist was freed during cleanup.
    
    CRs-fixed: 464289
    Change-Id: Ic0dedbadd57ec4d9422b5e3a873243d0ffed51f7
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>

--- drivers/gpu/msm/kgsl_sharedmem.c
+++ drivers/gpu/msm/kgsl_sharedmem.c
@@ -513,6 +513,7 @@ _kgsl_sharedmem_page_alloc(struct kgsl_memdesc *memdesc,
 	memdesc->priv = KGSL_MEMFLAGS_CACHED;
 	memdesc->ops = &kgsl_page_alloc_ops;
 
+	memdesc->sglen = sglen;
 	memdesc->sg = kgsl_sg_alloc(sglen);
 
 	if (memdesc->sg == NULL) {
@@ -540,7 +541,6 @@ _kgsl_sharedmem_page_alloc(struct kgsl_memdesc *memdesc,
 
 	kmemleak_not_leak(memdesc->sg);
 
-	memdesc->sglen = sglen;
 	sg_init_table(memdesc->sg, sglen);
 
 	for (i = 0; i < PAGE_ALIGN(size) / PAGE_SIZE; i++) {
