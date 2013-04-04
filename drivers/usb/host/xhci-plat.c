7c8989bb689d813ada5fff295f390968c78bc6a6
Julius Werner <jwerner@chromium.org>
2013-04-04 09:37:16 -0700

    Revert "HACK: DO NOT SHIP: Workaround for factory camera test"
    This reverts commit fba159baa860243b2b6d60b79b7e046d955e0c92.
    
    BUG=chrome-os-partner:18460
    TEST=None
    
    Change-Id: I02e1323199a7cf4e7339bc48aa14f9229a95ca19
    Signed-off-by: Julius Werner <jwerner@chromium.org>
    Reviewed-on: https://gerrit.chromium.org/gerrit/47326
    Reviewed-by: Pawel Osciak <posciak@chromium.org>
    Reviewed-by: Vincent Palatin <vpalatin@chromium.org>

--- drivers/usb/host/xhci-plat.c
+++ drivers/usb/host/xhci-plat.c
@@ -24,7 +24,7 @@ static void xhci_plat_quirks(struct device *dev, struct xhci_hcd *xhci)
 	 * here that the generic code does not try to make a pci_dev from our
 	 * dev struct in order to setup MSI
 	 */
-	xhci->quirks |= XHCI_BROKEN_MSI | XHCI_SPURIOUS_SUCCESS;
+	xhci->quirks |= XHCI_BROKEN_MSI;
 }
 
 /* called during probe() after chip reset completes */
