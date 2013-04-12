a8d4d5a883a9cdeff430925aa1c74e23cdfcc30b
Dinh Nguyen <dinguyen@altera.com>
2013-04-12 14:37:09 -0500

    FogBugz #113871: Add CIU clock in device tree entry for SD/MMC
    This patch aligns the SD/MMC driver with the community version. We no
    longer need to add "bus-hz" in the device tree entry and gets it correctly
    from the ciu clock device tree entry.
    
    Also adds dw_mmc-socfpga.c for platform specific functions such as
    getting the CIU divider, setting for pwr-en, and USE_HOLD_REG setting.
    
    Signed-off-by: Dinh Nguyen <dinguyen@altera.com>
    
    v5:
    - Fix up indentation/spacing
    - Remove clock phase shift for socfpga_vt.dts
    
    v4:
    - Move setting of drvsel and smplsel to a one-time called function.
    - Gate the SDMMC clock before setting drvsel and smplsel
    - Remove set_ios functionality
    
    v3:
    - Add "altr,dw-mshc-ciu-div" for CIU clock divider value
    - Add "altr,dw-mshc-sdr-timing" for drvsel and smplsel values for system
    manager control register
    - Sets the drvsel and smplsel bits in the system manager SDMMC group
    - Add documentation for altr,dw-mshc device tree bindings
    - Split out SOC and board specific bindings
    
    v2:
    - Read sysmgr sdmmc_ctrl group, if value > 0, use_hold_reg gets set
    - Removed "bus-hz" from socfpga.dtsi
    - Fixed divider of 4 for CIU clock
    - Introduce global variable socfpga_sdmmc_use_hold_reg to use because its
    more efficient than trying to check for use_hold_reg for every prepare_command.

--- drivers/mmc/host/dw_mmc-socfpga.c
+++ drivers/mmc/host/dw_mmc-socfpga.c
@@ -26,14 +26,15 @@
 
 #define SYSMGR_SDMMCGRP_CTRL_OFFSET		0x108
 #define DRV_CLK_PHASE_SHIFT_SEL_MASK	0x7
-#define SYSMGR_SDMMC_CTRL_SET(smplsel, drvsel)          \
-	((((smplsel) & 0x7) << 3) | (((drvsel) & 0x7) << 0))
+#define SYSMGR_SDMMC_CTRL_SET(smplsel, drvsel)		\
+	((((drvsel) << 0) & 0x7) | (((smplsel) << 3) & 0x38))
+
+extern void __iomem *sys_manager_base_addr;
 
 /* SOCFPGA implementation specific driver private data */
 struct dw_mci_socfpga_priv_data {
-	u8	ciu_div; /* card interface unit divisor */
-	u32	hs_timing; /* bitmask for CIU clock phase shift */
-	struct regmap   *sysreg; /* regmap for system manager register */
+	u8	ciu_div;
+	u32	hs_timing;
 };
 
 static int dw_mci_socfpga_priv_init(struct dw_mci *host)
