5572333d929c38e2b1558b4781aef9ebb39553a6
Vicky Sehrawat <vickys@codeaurora.org>
2013-04-16 14:54:55 -0700

    ASoC: msm: Fix for Voice Call and VoIP Call
    Add required mixers for Voice Call and Voip Call
    
    Signed-off-by: Vicky Sehrawat <vickys@codeaurora.org>
    Change-Id: I354c1436ba63224d4f121e6c5780c1ca0eb791e1

--- sound/soc/msm/msm8x10.c
+++ sound/soc/msm/msm8x10.c
@@ -414,6 +414,22 @@ static struct snd_soc_dai_link msm8x10_dai[] = {
 		.ignore_pmdown_time = 1,
 		.be_id = MSM_FRONTEND_DAI_MULTIMEDIA2,
 	},
+	{
+		.name = "Circuit-Switch Voice",
+		.stream_name = "CS-Voice",
+		.cpu_dai_name   = "CS-VOICE",
+		.platform_name  = "msm-pcm-voice",
+		.dynamic = 1,
+		.codec_dai_name = "snd-soc-dummy-dai",
+		.codec_name = "snd-soc-dummy",
+		.trigger = {SND_SOC_DPCM_TRIGGER_POST,
+			SND_SOC_DPCM_TRIGGER_POST},
+		.no_host_mode = SND_SOC_DAI_LINK_NO_HOST,
+		.ignore_suspend = 1,
+		/* this dainlink has playback support */
+		.ignore_pmdown_time = 1,
+		.be_id = MSM_FRONTEND_DAI_CS_VOICE,
+	},
 	/* Backend I2S DAI Links */
 	{
 		.name = LPASS_BE_SEC_MI2S_RX,
