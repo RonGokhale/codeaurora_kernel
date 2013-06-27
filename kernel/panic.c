c8781d67c0ffbdd01dd0eb5124fc2c28f0c614fe
Andrew Morton <akpm@linux-foundation.org>
2013-06-28 09:52:42 +1000

    panic-add-cpu-pid-to-warn_slowpath_common-in-warning-printks-fix
    remove stray quote
    
    Cc: Alex Thorlton <athorlton@sgi.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- kernel/panic.c
+++ kernel/panic.c
@@ -403,7 +403,7 @@ static void warn_slowpath_common(const char *file, int line, void *caller,
 	disable_trace_on_warning();
 
 	pr_warn("------------[ cut here ]------------\n");
-	pr_warn("WARNING: CPU: %d PID: %d at %s:%d %pS()\n",  '
+	pr_warn("WARNING: CPU: %d PID: %d at %s:%d %pS()\n",
 		raw_smp_processor_id(), current->pid, file, line, caller);
 
 	if (args)
