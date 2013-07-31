eab6f38ab23ce50acfb457820b6203becc8fb9b6
Christoph Lameter <cl@linux.com>
2013-07-31 17:12:52 +0000

    mm/sl[aou]b: Move kmalloc definitions to slab.h
    All the kmallocs are mostly doing the same. Unify them.
    
    slob_def.h becomes empty. So remove it.
    
    Signed-off-by: Christoph Lameter <cl@linux.com>
    Signed-off-by: Pekka Enberg <penberg@kernel.org>

--- include/linux/slob_def.h
+++
@@ -1,9 +0,0 @@
-#ifndef __LINUX_SLOB_DEF_H
-#define __LINUX_SLOB_DEF_H
-
-static __always_inline void *kmalloc(size_t size, gfp_t flags)
-{
-	return __kmalloc_node(size, flags, NUMA_NO_NODE);
-}
-
-#endif /* __LINUX_SLOB_DEF_H */
