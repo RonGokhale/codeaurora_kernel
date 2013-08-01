98e9ba63ce05bf80a1c30771069550a9d61d7d5e
Rob Herring <rob.herring@calxeda.com>
2013-08-01 15:50:55 -0500

    ARM: highbank: enable DMA zone for LPAE
    Some devices are restricted to 32-bit DMA. Thus the platform dma_zone_size
    needs to be set. Otherwise dma-mapping code is complaining, e.g.
    
    calxedaxgmac fff50000.ethernet: coherent DMA mask 0xffffffff is smaller
    than system GFP_DMA mask 0xffffffffffffffff
    
    Signed-off-by: Andreas Herrmann <andreas.herrmann@calxeda.com>
    Signed-off-by: Rob Herring <rob.herring@calxeda.com>

--- arch/arm/mach-highbank/highbank.c
+++ arch/arm/mach-highbank/highbank.c
@@ -176,6 +176,9 @@ static const char *highbank_match[] __initconst = {
 };
 
 DT_MACHINE_START(HIGHBANK, "Highbank")
+#ifdef CONFIG_ZONE_DMA
+	.dma_zone_size	= (4ULL * SZ_1G),
+#endif
 	.smp		= smp_ops(highbank_smp_ops),
 	.init_irq	= highbank_init_irq,
 	.init_time	= highbank_timer_init,
