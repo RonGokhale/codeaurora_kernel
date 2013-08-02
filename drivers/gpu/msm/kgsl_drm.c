7797e0ac704a838e4ce629ba6b46a3c5ce056a91
Eunchul Kim <chulspro.kim@samsung.com>
2013-08-03 04:11:56 +0900

    kgsl/drm: Support clock control the register control.
    This patch supports clock handling api from mdss to avoid unnormal
    state. When we control the register without clock, we got the problem.
    
    Change-Id: Ie91057cf28c6cdce559c40abcbf641177213027d
    Signed-off-by: Eunchul Kim <chulspro.kim@samsung.com>
    Signed-off-by: Mathew Karimpanal <kjmathew@codeaurora.org>

--- drivers/gpu/msm/kgsl_drm.c
+++ drivers/gpu/msm/kgsl_drm.c
@@ -1778,7 +1778,11 @@ kgsl_drm_irq_postinstall(struct drm_device *dev)
 {
 	struct drm_kgsl_private *dev_priv =
 		(struct drm_kgsl_private *)dev->dev_private;
-	u32 mask = readl_relaxed(dev_priv->regs +
+	u32 mask;
+
+	mdss_mdp_clk_ctrl(1, false);
+
+	mask = readl_relaxed(dev_priv->regs +
 		MDSS_MDP_REG_INTR_EN);
 
 	DRM_DEBUG("%s:regs[0x%x]\n", __func__, (int)dev_priv->regs);
@@ -1792,6 +1796,8 @@ kgsl_drm_irq_postinstall(struct drm_device *dev)
 	enable_irq(dev_priv->irq);
 	dev->irq_enabled = 1;
 
+	mdss_mdp_clk_ctrl(0, false);
+
 	return 0;
 }
 
@@ -1800,7 +1806,11 @@ kgsl_drm_irq_uninstall(struct drm_device *dev)
 {
 	struct drm_kgsl_private *dev_priv =
 		(struct drm_kgsl_private *)dev->dev_private;
-	u32 mask = readl_relaxed(dev_priv->regs +
+	u32 mask;
+
+	mdss_mdp_clk_ctrl(1, false);
+
+	mask = readl_relaxed(dev_priv->regs +
 		MDSS_MDP_REG_INTR_EN);
 
 	DRM_DEBUG("%s:regs[0x%x]\n", __func__, (int)dev_priv->regs);
@@ -1810,6 +1820,8 @@ kgsl_drm_irq_uninstall(struct drm_device *dev)
 
 	disable_irq(dev_priv->irq);
 	dev->irq_enabled = 0;
+
+	mdss_mdp_clk_ctrl(0, false);
 }
 
 int kgsl_gem_prime_handle_to_fd(struct drm_device *dev,
