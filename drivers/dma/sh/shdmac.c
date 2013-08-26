f7edcb56ff9454e51faaa47c3cbf862e3f2c5853
Jingoo Han <jg1.han@samsung.com>
2013-08-26 09:32:05 +0900

    dma: sh: remove unnecessary platform_set_drvdata()
    The driver core clears the driver data to NULL after device_release
    or on probe failure. Thus, it is not needed to manually clear the
    device driver data to NULL.
    
    Signed-off-by: Jingoo Han <jg1.han@samsung.com>
    Acked-by: Simon Horman <horms+renesas@verge.net.au>
    Acked-by: Guennadi Liakhovetski <g.liakhovetski+renesas@gmail.com>
    Signed-off-by: Vinod Koul <vinod.koul@intel.com>

--- drivers/dma/sh/shdmac.c
+++ drivers/dma/sh/shdmac.c
@@ -882,7 +882,6 @@ rst_err:
 	pm_runtime_put(&pdev->dev);
 	pm_runtime_disable(&pdev->dev);
 
-	platform_set_drvdata(pdev, NULL);
 	shdma_cleanup(&shdev->shdma_dev);
 eshdma:
 	synchronize_rcu();
@@ -910,8 +909,6 @@ static int sh_dmae_remove(struct platform_device *pdev)
 
 	sh_dmae_chan_remove(shdev);
 	shdma_cleanup(&shdev->shdma_dev);
-
-	platform_set_drvdata(pdev, NULL);
 
 	synchronize_rcu();
 
