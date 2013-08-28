98c53a09f9379a210746d3dffaea513bc0d0e9e3
Oleg Nesterov <oleg@redhat.com>
2013-08-28 10:12:19 +1000

    pidns: kill the unnecessary CLONE_NEWPID in copy_process()
    8382fcac ("pidns: Outlaw thread creation after unshare(CLONE_NEWPID)")
    nacks CLONE_NEWPID if the forking process unshared pid_ns.  This is
    correct but unnecessary, copy_pid_ns() does the same check.
    
    Remove the CLONE_NEWPID check to cleanup the code and prepare for the next
    change.
    
    Test-case:
    
    	static int child(void *arg)
    	{
    		return 0;
    	}
    
    	static char stack[16 * 1024];
    
    	int main(void)
    	{
    		pid_t pid;
    
    		assert(unshare(CLONE_NEWUSER | CLONE_NEWPID) == 0);
    
    		pid = clone(child, stack + sizeof(stack) / 2,
    				CLONE_NEWPID | SIGCHLD, NULL);
    		assert(pid < 0 && errno == EINVAL);
    
    		return 0;
    	}
    
    clone(CLONE_NEWPID) correctly fails with or without this change.
    
    Signed-off-by: Oleg Nesterov <oleg@redhat.com>
    Acked-by: Andy Lutomirski <luto@amacapital.net>
    Cc: "Eric W. Biederman" <ebiederm@xmission.com>
    Cc: Colin Walters <walters@redhat.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- kernel/fork.c
+++ kernel/fork.c
@@ -1177,7 +1177,7 @@ static struct task_struct *copy_process(unsigned long clone_flags,
 	 * allow it to share a thread group or signal handlers with the
 	 * forking task.
 	 */
-	if ((clone_flags & (CLONE_SIGHAND | CLONE_NEWPID)) &&
+	if ((clone_flags & CLONE_SIGHAND) &&
 	    (task_active_pid_ns(current) != current->nsproxy->pid_ns))
 		return ERR_PTR(-EINVAL);
 
