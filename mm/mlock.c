af40dee070fb931c8da1c23f75a96defa64bbc9e
Andrew Morton <akpm@linux-foundation.org>
2013-08-28 10:15:10 +1000

    mm-munlock-bypass-per-cpu-pvec-for-putback_lru_page-fix
    clarify comment
    
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/mlock.c
+++ mm/mlock.c
@@ -264,7 +264,10 @@ static bool __putback_lru_fast_prepare(struct page *page, struct pagevec *pvec,
 static void __putback_lru_fast(struct pagevec *pvec, int pgrescued)
 {
 	count_vm_events(UNEVICTABLE_PGMUNLOCKED, pagevec_count(pvec));
-	/* This includes put_page so we don't call it explicitly */
+	/*
+	 *__pagevec_lru_add() calls release_pages() so we don't call
+	 * put_page() explicitly
+	 */
 	__pagevec_lru_add(pvec);
 	count_vm_events(UNEVICTABLE_PGRESCUED, pgrescued);
 }
