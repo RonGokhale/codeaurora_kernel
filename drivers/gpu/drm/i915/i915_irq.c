37a9c3bffeb0766cd8231ec2249b195da1031755
Daniel Vetter <daniel.vetter@ffwll.ch>
2013-09-02 16:22:25 +0200

    drm/i915: fix hpd work vs. flush_work in the pageflip code deadlock
    Historically we've run our own driver hotplug handling in our own
    work-queue, which then launched the drm core hotplug handling in the
    system workqueue. This is important since we flush our own driver
    workqueue in the pageflip code while hodling modeset locks, and only
    the drm hotplug code grabbed these locks. But with
    
    commit 69787f7da6b2adc4054357a661aaa1701a9ca76f
    Author: Daniel Vetter <daniel.vetter@ffwll.ch>
    Date:   Tue Oct 23 18:23:34 2012 +0000
    
        drm: run the hpd irq event code directly
    
    this was changed and now we could deadlock in our flip handler if
    there's a hotplug work blocking the progress of the crucial unpin
    works. So this broke the careful deadlock avoidance implemented in
    
    commit b4a98e57fc27854b5938fc8b08b68e5e68b91e1f
    Author: Chris Wilson <chris@chris-wilson.co.uk>
    Date:   Thu Nov 1 09:26:26 2012 +0000
    
        drm/i915: Flush outstanding unpin tasks before pageflipping
    
    Since the rule thus far has been that work items on our own workqueue
    may never grab modeset locks simply restore that rule again.
    
    v2: Add a comment to the declaration of dev_priv->wq to warn readers
    about the tricky implications of using it. Suggested by Chris Wilson.
    
    Cc: Chris Wilson <chris@chris-wilson.co.uk>
    Cc: Stuart Abercrombie <sabercrombie@chromium.org>
    Reported-by: Stuart Abercrombie <sabercrombie@chromium.org>
    References: http://permalink.gmane.org/gmane.comp.freedesktop.xorg.drivers.intel/26239
    Cc: stable@vger.kernel.org
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/i915_irq.c
+++ drivers/gpu/drm/i915/i915_irq.c
@@ -1027,8 +1027,7 @@ static inline void intel_hpd_irq_handler(struct drm_device *dev,
 		dev_priv->display.hpd_irq_setup(dev);
 	spin_unlock(&dev_priv->irq_lock);
 
-	queue_work(dev_priv->wq,
-		   &dev_priv->hotplug_work);
+	schedule_work(&dev_priv->hotplug_work);
 }
 
 static void gmbus_irq_handler(struct drm_device *dev)
