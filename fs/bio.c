boundarydevices/linux

commit febca1baea1cfe2d7a0271385d89b03d5fb34f94

block: setup bi_vcnt on clones
commit 9fc6286f347d changed the cloning code to make clones cheaper for
the case where we don't need to clone the iovec array.  But,
the new clone needs the bi_vnct from the original.

Signed-off-by: Chris Mason <chris.mason@fusionio.com>
Signed-off-by: Jens Axboe <axboe@kernel.dk>

fs/bio.c
@@ -560,6 +560,7 @@ void __bio_clone(struct bio *bio, struct bio *bio_src)
 	bio->bi_rw = bio_src->bi_rw;
 	bio->bi_iter = bio_src->bi_iter;
 	bio->bi_io_vec = bio_src->bi_io_vec;
+	bio->bi_vcnt = bio_src->bi_vcnt;
 }
 EXPORT_SYMBOL(__bio_clone);
 

RonGokhale/codeaurora_kernel

commit 640ab3657a5d3d7fbbe03223f1f0ecbdf003e9a4

block: fix double increment of bio vec count on bouncing
Tushar/Tomasz both reported a boot failure on an Arndale
board on mmcblock. Turns out the bounce code got broken
with commit febca1baea1c that copied the vec count
from the bio_src to to bio_dest on a bio clone, since
the new clone logic increments vec count as well. This
resulted in a double increment, screwing up DMA mapping
of the bio.

Reported-by: Tushar Behera <tushar.behera@linaro.org>
Reported-by: Tomasz Figa <t.figa@samsung.com>
Tested-by: Stephen Warren <swarren@nvidia.com>
Signed-off-by: Jens Axboe <axboe@kernel.dk>

fs/bio.c
@@ -612,6 +612,7 @@ int bio_clone_biovec(struct bio *bio, gfp_t gfp_mask)
 	unsigned nr_iovecs = 0;
 	struct bio_vec bv, *bvl = NULL;
 	struct bvec_iter iter;
+	int i;
 
 	BUG_ON(!bio->bi_pool);
 	BUG_ON(BIO_POOL_IDX(bio) != BIO_POOL_NONE);
@@ -628,8 +629,9 @@ int bio_clone_biovec(struct bio *bio, gfp_t gfp_mask)
 		bvl = bio->bi_inline_vecs;
 	}
 
+	i = 0;
 	bio_for_each_segment(bv, bio, iter)
-		bvl[bio->bi_vcnt++] = bv;
+		bvl[i++] = bv;
 
 	bio->bi_io_vec = bvl;
 	bio->bi_iter.bi_idx = 0;