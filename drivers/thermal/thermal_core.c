819136a78d4e43d499ca65f888da3e04981632b6
Wei Ni <wni@nvidia.com>
2014-01-08 14:53:00 +0800

    CHROMIUM: thermal: Add PID thermal governor
    Added PID thermal governor to the generic thermal layer.
    This governor throttles the devices based on output values of
    PID controller.
    
    These codes were origianlly wirtten by Jinyong Park, Cc to him.
    
    BUG=None
    TEST=None
    
    Change-Id: I97a22e4ef15a2155ee2e6adfc0334e7013a2fab5
    Signed-off-by: Wei Ni <wni@nvidia.com>
    Cc: Jinyoung Park <jinyoungp@nvidia.com>
    Reviewed-on: https://chromium-review.googlesource.com/177329
    Tested-by: Wei Ni <wni.nvidia@gmail.com>
    Reviewed-by: Dylan Reid <dgreid@chromium.org>
    Commit-Queue: Dylan Reid <dgreid@chromium.org>
    Tested-by: Dylan Reid <dgreid@chromium.org>

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -1934,6 +1934,10 @@ static int __init thermal_register_governors(void)
 	if (result)
 		return result;
 
+	result = thermal_gov_pid_register();
+	if (result)
+		return result;
+
 	return thermal_gov_user_space_register();
 }
 
@@ -1941,6 +1945,7 @@ static void thermal_unregister_governors(void)
 {
 	thermal_gov_step_wise_unregister();
 	thermal_gov_fair_share_unregister();
+	thermal_gov_pid_unregister();
 	thermal_gov_user_space_unregister();
 }
 
