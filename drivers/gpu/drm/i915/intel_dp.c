03338b80363ee4a3866c1c7952ba190d8ce7404a
Imre Deak <imre.deak@intel.com>
2014-01-17 15:46:43 +0200

    drm/i915: don't disable DP port after a failed link training
    Atm after a failed link training we disable the DP port. This can happen
    during a modeset-enable or a DP link re-establishment. The latter can be
    a problem and we shouldn't disable the DP port, see the previous patch for
    the reasoning. In the former case the right thing would be to disable
    the DP port, but also the rest of the pipe.
    
    As a stop-gap solution leave the DP port enabled in both cases. It is an
    improvement on its own (avoiding HW lock ups) and the proper solution
    for the first case requires a bigger change, so let's keep that on the
    TODO list.
    
    v2:
    - fix explanation of change impact (Chris)
    
    Suggested-by: Daniel Vetter <daniel@ffwll.ch>
    Signed-off-by: Imre Deak <imre.deak@intel.com>
    Reviewed-by: Chris Wilson <chris@chris-wilson.co.uk>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_dp.c
+++ drivers/gpu/drm/i915/intel_dp.c
@@ -2660,7 +2660,6 @@ intel_dp_complete_link_train(struct intel_dp *intel_dp)
 
 		if (cr_tries > 5) {
 			DRM_ERROR("failed to train DP, aborting\n");
-			intel_dp_link_down(intel_dp);
 			break;
 		}
 
