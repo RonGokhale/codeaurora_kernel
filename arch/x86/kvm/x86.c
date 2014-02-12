e195154368f25c410a2e7f7e1a5cd359212cfd48
Michael S. Tsirkin <mst@redhat.com>
2014-02-12 17:46:57 +0200

    x86: convert hlt to nop
    ugly hack

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -5655,7 +5655,8 @@ int kvm_emulate_halt(struct kvm_vcpu *vcpu)
 {
 	++vcpu->stat.halt_exits;
 	if (irqchip_in_kernel(vcpu->kvm)) {
-		vcpu->arch.mp_state = KVM_MP_STATE_HALTED;
+		if (kvm_get_rflags(vcpu) & X86_EFLAGS_IF)
+			vcpu->arch.mp_state = KVM_MP_STATE_HALTED;
 		return 1;
 	} else {
 		vcpu->run->exit_reason = KVM_EXIT_HLT;
