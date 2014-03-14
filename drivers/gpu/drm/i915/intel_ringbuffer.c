singulared/chromebook-kernel

drm/i915: Fix fbc + rc6 combination on SNB
On Sandybridge we must set the "PPGTT Render Target Base Address Valid
for FBC" bit as noted in the programming guide. We did this at clock
gating init. Thisbit is not saved and restored with RC6 power context,
so the resetting it at ring flush should fix that.

The effect of not doing this should be corruption, and not a hang - as
has so often been the case.

Note that we should actually clear this bit as well when not blitting to
the scanout (using the blitter for other things), or else all operations

Cc: Stéphane Marchesin <marcheu@chromium.org>
Signed-off-by: Ben Widawsky <ben@bwidawsk.net>

BUG=chromium:303986
TEST=by hand:
i=0; while true; do i=$(($i+1)); echo "*** iteration $i"; X & sleep 2; DISPLAY=:0 xrandr --auto ; sleep 1; killall X; sleep 2; done

Change-Id: Ic0433466c2e466c7e0e934efc03d3f67b9c977d4
Reviewed-on: https://chromium-review.googlesource.com/175541
Reviewed-by: Stéphane Marchesin <marcheu@chromium.org>
Commit-Queue: Stéphane Marchesin <marcheu@chromium.org>
Tested-by: Stéphane Marchesin <marcheu@chromium.org>

drivers/gpu/drm/i915/intel_ringbuffer.c
@@ -1486,9 +1486,33 @@ static void gen6_bsd_ring_write_tail(struct intel_ring_buffer *ring,
 		   _MASKED_BIT_DISABLE(GEN6_BSD_SLEEP_MSG_DISABLE));
 }
 
+static int gen6_ring_fbc_flush(struct intel_ring_buffer *ring)
+{
+	int ret;
+
+	if (!ring->fbc_dirty)
+		return 0;
+
+	ret = intel_ring_begin(ring, 4);
+	if (ret)
+		return ret;
+
+	intel_ring_emit(ring, MI_NOOP);
+	intel_ring_emit(ring, MI_LOAD_REGISTER_IMM(1));
+	intel_ring_emit(ring, GEN6_BLITTER_ECOSKPD);
+	intel_ring_emit(ring,
+			_MASKED_BIT_ENABLE(GEN6_BLITTER_FBC_NOTIFY));
+	intel_ring_advance(ring);
+
+	ring->fbc_dirty = false;
+	return 0;
+}
+
+
 static int gen6_ring_flush(struct intel_ring_buffer *ring,
 			   u32 invalidate, u32 flush)
 {
+	struct drm_device *dev = ring->dev;
 	uint32_t cmd;
 	int ret;
 
@@ -1511,6 +1535,10 @@ static int gen6_ring_flush(struct intel_ring_buffer *ring,
 	intel_ring_emit(ring, 0);
 	intel_ring_emit(ring, MI_NOOP);
 	intel_ring_advance(ring);
+
+	if (IS_GEN6(dev) && flush)
+		return gen6_ring_fbc_flush(ring);
+
 	return 0;
 }
 

RonGokhale/codeaurora_kernel

Original-Change-Id: Ic0433466c2e466c7e0e934efc03d3f67b9c977d4
(cherry picked from commit b21462192a8991a4709a9f661c02ce21d30585e0)
[bleung : 3.8 to 3.10 sync. Cherry-pick to chromeos-3.10]
Conflicts:
	drivers/gpu/drm/i915/intel_ringbuffer.c
[bleung : resolved name change for the gen6_ring function from this commit:
 drm/i915: Rename ring flush functions]
Signed-off-by: Benson Leung <bleung@chromium.org>
Change-Id: If8e60298f4536a1e7a5782a4ecaa21e0acb2f1ec
[benzh: 3.14 rebase. Resolved trivial conflicts]
Signed-off-by: Ben Zhang <benzh@chromium.org>

drivers/gpu/drm/i915/intel_ringbuffer.c
@@ -1703,9 +1703,33 @@ static void gen6_bsd_ring_write_tail(struct intel_ring_buffer *ring,
 		   _MASKED_BIT_DISABLE(GEN6_BSD_SLEEP_MSG_DISABLE));
 }
 
+static int gen6_bsd_ring_fbc_flush(struct intel_ring_buffer *ring)
+{
+	int ret;
+
+	if (!ring->fbc_dirty)
+		return 0;
+
+	ret = intel_ring_begin(ring, 4);
+	if (ret)
+		return ret;
+
+	intel_ring_emit(ring, MI_NOOP);
+	intel_ring_emit(ring, MI_LOAD_REGISTER_IMM(1));
+	intel_ring_emit(ring, GEN6_BLITTER_ECOSKPD);
+	intel_ring_emit(ring,
+			_MASKED_BIT_ENABLE(GEN6_BLITTER_FBC_NOTIFY));
+	intel_ring_advance(ring);
+
+	ring->fbc_dirty = false;
+	return 0;
+}
+
+
 static int gen6_bsd_ring_flush(struct intel_ring_buffer *ring,
 			       u32 invalidate, u32 flush)
 {
+	struct drm_device *dev = ring->dev;
 	uint32_t cmd;
 	int ret;
 
@@ -1735,6 +1759,10 @@ static int gen6_bsd_ring_flush(struct intel_ring_buffer *ring,
 		intel_ring_emit(ring, MI_NOOP);
 	}
 	intel_ring_advance(ring);
+
+	if (IS_GEN6(dev) && flush)
+		return gen6_bsd_ring_fbc_flush(ring);
+
 	return 0;
 }
 