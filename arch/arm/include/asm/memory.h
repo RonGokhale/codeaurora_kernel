e2b5c192c4a6bd8a9ec2ee0400f7e451bb4e8e04
Sebastian Capella <sebastian.capella@linaro.org>
2014-03-25 01:20:29 +0100

    ARM: 8011/1: ARM hibernation / suspend-to-disk
    Enable hibernation for ARM architectures and provide ARM
    architecture specific calls used during hibernation.
    
    The swsusp hibernation framework depends on the
    platform first having functional suspend/resume.
    
    Then, in order to enable hibernation on a given platform, a
    platform_hibernation_ops structure may need to be registered with
    the system in order to save/restore any SoC-specific / cpu specific
    state needing (re)init over a suspend-to-disk/resume-from-disk cycle.
    
    For example:
    
         - "secure" SoCs that have different sets of control registers
           and/or different CR reg access patterns.
    
         - SoCs with L2 caches as the activation sequence there is
           SoC-dependent; a full off-on cycle for L2 is not done
           by the hibernation support code.
    
         - SoCs requiring steps on wakeup _before_ the "generic" parts
           done by cpu_suspend / cpu_resume can work correctly.
    
         - SoCs having persistent state which is maintained during suspend
           and resume, but will be lost during the power off cycle after
           suspend-to-disk.
    
    This is a rebase/rework of Frank Hofmann's v5 hibernation patchset.
    
    Acked-by: Russ Dill <Russ.Dill@ti.com>
    Cc: "Rafael J. Wysocki" <rjw@rjwysocki.net>
    Signed-off-by: Sebastian Capella <sebastian.capella@linaro.org>
    Acked-by: Pavel Machek <pavel@ucw.cz>
    Reviewed-by: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/include/asm/memory.h
+++ arch/arm/include/asm/memory.h
@@ -296,6 +296,7 @@ static inline void *phys_to_virt(phys_addr_t x)
 #define __pa(x)			__virt_to_phys((unsigned long)(x))
 #define __va(x)			((void *)__phys_to_virt((phys_addr_t)(x)))
 #define pfn_to_kaddr(pfn)	__va((pfn) << PAGE_SHIFT)
+#define virt_to_pfn(kaddr)      (__pa(kaddr) >> PAGE_SHIFT)
 
 extern phys_addr_t (*arch_virt_to_idmap)(unsigned long x);
 
