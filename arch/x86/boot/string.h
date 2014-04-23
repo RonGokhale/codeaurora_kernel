382c09f9ecf8084c2feff1e8e8909ae4a52dc249
Jan-Simon Möller <dl9pf@gmx.de>
2014-04-23 17:14:02 +0200

    WORKAROUND NOT-UPSTREAMED x86, boot: Work around clang PR18415.
    Clang's intrinsics ignore -mregparm=3 when they fall back to calling the
    out-of-line implementations. Putting the args on the stack when memcpy()
    expects them in registers is not a recipe for a happy kernel.
    
    This bites with -m32 too, so clang is presumably catastrophically
    broken for the i386 kernel until this is fixed, unless I'm missing
    something.
    
    For information/testing only; do not apply. With this, I can use
    'clang -m16' to build all the kernel's 16-bit code and get a successful
    boot.
    
    Not-signed-off-by: David Woodhouse <David.Woodhouse@intel.com>
    Forward-ported-by: Jan-Simon Möller <dl9pf@gmx.de>

--- arch/x86/boot/string.h
+++ arch/x86/boot/string.h
@@ -14,8 +14,10 @@ int memcmp(const void *s1, const void *s2, size_t len);
  * Access builtin version by default. If one needs to use optimized version,
  * do "undef memcpy" in .c file and link against right string.c
  */
+#ifndef __clang__ /* PR18415 */
 #define memcpy(d,s,l) __builtin_memcpy(d,s,l)
 #define memset(d,c,l) __builtin_memset(d,c,l)
 #define memcmp	__builtin_memcmp
+#endif
 
 #endif /* BOOT_STRING_H */
