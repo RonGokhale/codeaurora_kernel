dce2ceddee7855970f4de7b3461ca2119f446c32
Ville Syrjälä <ville.syrjala@linux.intel.com>
2014-04-25 22:11:51 +0300

    drm/i915: Fix deadlock during driver init on ILK
    We have a struct_mutex deadlock during driver init on ILK
    
    [   54.320273] =============================================
    [   54.320371] [ INFO: possible recursive locking detected ]
    [   54.320471] 3.15.0-rc2-flip_race+ #2 Not tainted
    [   54.320567] ---------------------------------------------
    [   54.320665] modprobe/2178 is trying to acquire lock:
    [   54.320762]  (&dev->struct_mutex){+.+.+.}, at: [<ffffffffa0568b05>] intel_enable_gt_powersave+0xa5/0x9d0 [i915]
    [   54.321111]
    [   54.321111] but task is already holding lock:
    [   54.321250]  (&dev->struct_mutex){+.+.+.}, at: [<ffffffffa05b4c2e>] intel_modeset_init_hw+0x3e/0x60 [i915]
    [   54.321583]
    [   54.321583] other info that might help us debug this:
    [   54.321724]  Possible unsafe locking scenario:
    [   54.321724]
    [   54.321863]        CPU0
    [   54.321954]        ----
    [   54.322046]   lock(&dev->struct_mutex);
    [   54.322221]   lock(&dev->struct_mutex);
    [   54.322397]
    [   54.322397]  *** DEADLOCK ***
    [   54.322397]
    [   54.322638]  May be due to missing lock nesting notation
    [   54.322638]
    [   54.322781] 4 locks held by modprobe/2178:
    [   54.322875]  #0:  (&dev->mutex){......}, at: [<ffffffff813592eb>] __driver_attach+0x5b/0xb0
    [   54.323230]  #1:  (&dev->mutex){......}, at: [<ffffffff813592f9>] __driver_attach+0x69/0xb0
    [   54.323582]  #2:  (drm_global_mutex){+.+.+.}, at: [<ffffffffa04e1e0d>] drm_dev_register+0x2d/0x120 [drm]
    [   54.323945]  #3:  (&dev->struct_mutex){+.+.+.}, at: [<ffffffffa05b4c2e>] intel_modeset_init_hw+0x3e/0x60 [i915]
    
    This regression got introduced in:
     commit 586d5270b60dc1f35cc3ca982d403765bad77965
     Author: Imre Deak <imre.deak@intel.com>
     Date:   Mon Apr 14 20:24:28 2014 +0300
    
        drm/i915: move getting struct_mutex lower in the callstack during GPU reset
    
    Fix the problem by not taking struct_mutex around intel_enable_gt_powersave()
    in intel_modeset_init_hw() since intel_enable_gt_powersave() now grabs the
    mutex itself.
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -11241,9 +11241,7 @@ void intel_modeset_init_hw(struct drm_device *dev)
 
 	intel_reset_dpio(dev);
 
-	mutex_lock(&dev->struct_mutex);
 	intel_enable_gt_powersave(dev);
-	mutex_unlock(&dev->struct_mutex);
 }
 
 void intel_modeset_suspend_hw(struct drm_device *dev)
