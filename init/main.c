9c65189cc6f53b733c019a1a0a313950a7248a3b
Andrew Morton <akpm@linux-foundation.org>
2014-05-02 08:03:18 +1000

    init-mainc-add-initcall_blacklist-kernel-parameter-fix
    tweak printk text
    
    Cc: Andi Kleen <andi@firstfloor.org>
    Cc: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Josh Boyer <jwboyer@fedoraproject.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Prarit Bhargava <prarit@redhat.com>
    Cc: Richard Weinberger <richard.weinberger@gmail.com>
    Cc: Rob Landley <rob@landley.net>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- init/main.c
+++ init/main.c
@@ -683,7 +683,7 @@ static int __init initcall_blacklist(char *str)
 	do {
 		str_entry = strsep(&str, ",");
 		if (str_entry) {
-			pr_debug("initcall blacklisted %s\n", str_entry);
+			pr_debug("blacklisting initcall %s\n", str_entry);
 			entry = alloc_bootmem(sizeof(*entry));
 			entry->buf = alloc_bootmem(strlen(str_entry) + 1);
 			strcpy(entry->buf, str_entry);
