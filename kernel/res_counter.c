2a368cef12bd85b061d9a9edde8361af39d7b4f9
Fabian Frederick <fabf@skynet.be>
2014-05-10 17:18:47 +1000

    kernel/res_counter.c: replace simple_strtoull by kstrtoull
    Signed-off-by: Fabian Frederick <fabf@skynet.be>
    Cc: Michal Hocko <mhocko@suse.cz>
    Cc: Tejun Heo <tj@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- kernel/res_counter.c
+++ kernel/res_counter.c
@@ -188,8 +188,8 @@ int res_counter_memparse_write_strategy(const char *buf,
 
 	/* return RES_COUNTER_MAX(unlimited) if "-1" is specified */
 	if (*buf == '-') {
-		res = simple_strtoull(buf + 1, &end, 10);
-		if (res != 1 || *end != '\0')
+		int rc = kstrtoull(buf + 1, 10, &res);
+		if ((res != 1) || rc)
 			return -EINVAL;
 		*resp = RES_COUNTER_MAX;
 		return 0;
