5756a32b1c427d1d99e4b9396f9b9807899a452c
Andrew Morton <akpm@linux-foundation.org>
2014-05-14 10:01:23 +1000

    arch-unicore32-mm-ioremapc-convert-printk-warn_on-to-warn1-fix
    undo crazy long line
    
    Cc: Fabian Frederick <fabf@skynet.be>
    Cc: Guan Xuetao <gxt@mprc.pku.edu.cn>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- arch/unicore32/mm/ioremap.c
+++ arch/unicore32/mm/ioremap.c
@@ -144,7 +144,10 @@ void __iomem *__uc32_ioremap_pfn_caller(unsigned long pfn,
 	 * Don't allow RAM to be mapped
 	 */
 	if (pfn_valid(pfn))
-		WARN(1, "BUG: Your driver calls ioremap() on\nsystem memory.  This leads to architecturally\nunpredictable behaviour, and ioremap() will fail in\nthe next kernel release. Please fix your driver.\n");
+		WARN(1, "BUG: Your driver calls ioremap() on\n"
+			"system memory.  This leads to architecturally\n"
+			"unpredictable behaviour, and ioremap() will fail in\n"
+			"the next kernel release. Please fix your driver.\n");
 
 	type = get_mem_type(mtype);
 	if (!type)
