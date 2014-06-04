bba5cf04c1b433cbc181315831fab6933952049d
David Sterba <dsterba@suse.cz>
2014-06-04 18:10:45 +0200

    btrfs: make close_ctree return void
    There's no user of the return value and we can get rid of the comment in
    put_super.
    
    Signed-off-by: David Sterba <dsterba@suse.cz>

--- fs/btrfs/disk-io.c
+++ fs/btrfs/disk-io.c
@@ -3616,7 +3616,7 @@ int btrfs_commit_super(struct btrfs_root *root)
 	return btrfs_commit_transaction(trans, root);
 }
 
-int close_ctree(struct btrfs_root *root)
+void close_ctree(struct btrfs_root *root)
 {
 	struct btrfs_fs_info *fs_info = root->fs_info;
 	int ret;
@@ -3704,8 +3704,6 @@ int close_ctree(struct btrfs_root *root)
 
 	btrfs_free_block_rsv(root, root->orphan_block_rsv);
 	root->orphan_block_rsv = NULL;
-
-	return 0;
 }
 
 int btrfs_buffer_uptodate(struct extent_buffer *buf, u64 parent_transid,
