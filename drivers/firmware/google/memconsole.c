b963d053dd748ed9500e9cf716de5c5fc16122cf
Julius Werner <jwerner@chromium.org>
2014-06-10 14:18:32 -0700

    CHROMIUM: firmware: google: Add ARM/ARM64 support to memconsole
    This patch expands the Google firmware memory console driver to also
    work on certain device tree based platforms running coreboot, such as ARM/ARM64
    Chromebooks. It splits the code relying on x86-centric features (ACPI, EBDA,
    DMI) out into an extra file, and adds yet another path to look up the (coreboot)
    memory console through the coreboot table. In order to find that, a second level
    bootloader must have installed the 'coreboot' compatible device tree node that
    describes its base address and size.
    
    BUG=chrome-os-partner:29311
    TEST=Confirmed that it works on both Rush_Ryu and Samus. I have
    no idea how to test the EBDA or CBMEM-without-ACPI code paths, but I
    tried hard to keep all the logic intact and mostly just moved whole
    functions around.
    
    Signed-off-by: Julius Werner <jwerner@chromium.org>
    Signed-off-by: Furquan Shaikh <furquan@google.com>
    Change-Id: Ibca9012c6b72e4cc8ef8a3d4694f8fa6aa58ff0d
    Reviewed-on: https://chromium-review.googlesource.com/223066
    Tested-by: Furquan Shaikh <furquan@chromium.org>
    Reviewed-by: Olof Johansson <olofj@chromium.org>
    Commit-Queue: Furquan Shaikh <furquan@chromium.org>

--- drivers/firmware/google/memconsole.c
+++ drivers/firmware/google/memconsole.c
@@ -1,45 +1,16 @@
 /*
  * memconsole.c
  *
- * Infrastructure for importing the BIOS memory based console
- * into the kernel log ringbuffer.
+ * Architecture-independent parts of the memory based BIOS console.
  *
- * Copyright 2010 Google Inc. All rights reserved.
+ * Copyright 2014 Google Inc.
  */
 
-#include <linux/ctype.h>
 #include <linux/init.h>
-#include <linux/kernel.h>
-#include <linux/string.h>
 #include <linux/sysfs.h>
 #include <linux/kobject.h>
-#include <linux/module.h>
-#include <linux/dmi.h>
-#include <asm/bios_ebda.h>
 
-#define BIOS_MEMCONSOLE_V1_MAGIC	0xDEADBABE
-#define BIOS_MEMCONSOLE_V2_MAGIC	(('M')|('C'<<8)|('O'<<16)|('N'<<24))
-
-struct biosmemcon_ebda {
-	u32 signature;
-	union {
-		struct {
-			u8  enabled;
-			u32 buffer_addr;
-			u16 start;
-			u16 end;
-			u16 num_chars;
-			u8  wrapped;
-		} __packed v1;
-		struct {
-			u32 buffer_addr;
-			/* Misdocumented as number of pages! */
-			u16 num_bytes;
-			u16 start;
-			u16 end;
-		} __packed v2;
-	};
-} __packed;
+#include "memconsole.h"
 
 static char *memconsole_baseaddr;
 static size_t memconsole_length;
@@ -57,110 +28,55 @@ static struct bin_attribute memconsole_bin_attr = {
 	.read = memconsole_read,
 };
 
+/* CBMEM firmware console log descriptor. */
+struct cbmem_cons {
+	u32 buffer_size;
+	u32 buffer_cursor;
+	u8  buffer_body[0];
+} __packed;
 
-static void found_v1_header(struct biosmemcon_ebda *hdr)
+static struct cbmem_cons __iomem *cbmem_console;
+
+void memconsole_setup(void *baseaddr, size_t length)
 {
-	printk(KERN_INFO "BIOS console v1 EBDA structure found at %p\n", hdr);
-	printk(KERN_INFO "BIOS console buffer at 0x%.8x, "
-	       "start = %d, end = %d, num = %d\n",
-	       hdr->v1.buffer_addr, hdr->v1.start,
-	       hdr->v1.end, hdr->v1.num_chars);
-
-	memconsole_length = hdr->v1.num_chars;
-	memconsole_baseaddr = phys_to_virt(hdr->v1.buffer_addr);
+	memconsole_baseaddr = baseaddr;
+	memconsole_length = length;
 }
+EXPORT_SYMBOL(memconsole_setup);
 
-static void found_v2_header(struct biosmemcon_ebda *hdr)
+bool memconsole_coreboot_init(phys_addr_t physaddr)
 {
-	printk(KERN_INFO "BIOS console v2 EBDA structure found at %p\n", hdr);
-	printk(KERN_INFO "BIOS console buffer at 0x%.8x, "
-	       "start = %d, end = %d, num_bytes = %d\n",
-	       hdr->v2.buffer_addr, hdr->v2.start,
-	       hdr->v2.end, hdr->v2.num_bytes);
+	struct cbmem_cons __iomem *tmp_cbmc;
 
-	memconsole_length = hdr->v2.end - hdr->v2.start;
-	memconsole_baseaddr = phys_to_virt(hdr->v2.buffer_addr
-					   + hdr->v2.start);
-}
+	tmp_cbmc = ioremap_cache(physaddr, sizeof(*tmp_cbmc));
 
-/*
- * Search through the EBDA for the BIOS Memory Console, and
- * set the global variables to point to it.  Return true if found.
- */
-static bool found_memconsole(void)
-{
-	unsigned int address;
-	size_t length, cur;
-
-	address = get_bios_ebda();
-	if (!address) {
-		printk(KERN_INFO "BIOS EBDA non-existent.\n");
+	if (tmp_cbmc == NULL)
 		return false;
-	}
 
-	/* EBDA length is byte 0 of EBDA (in KB) */
-	length = *(u8 *)phys_to_virt(address);
-	length <<= 10; /* convert to bytes */
+	cbmem_console = ioremap_cache(physaddr, tmp_cbmc->buffer_size +
+		sizeof(*cbmem_console));	/* Don't forget counting the header. */
 
-	/*
-	 * Search through EBDA for BIOS memory console structure
-	 * note: signature is not necessarily dword-aligned
-	 */
-	for (cur = 0; cur < length; cur++) {
-		struct biosmemcon_ebda *hdr = phys_to_virt(address + cur);
+	iounmap(tmp_cbmc);
 
-		/* memconsole v1 */
-		if (hdr->signature == BIOS_MEMCONSOLE_V1_MAGIC) {
-			found_v1_header(hdr);
-			return true;
-		}
+	if (cbmem_console == NULL)
+		return false;
 
-		/* memconsole v2 */
-		if (hdr->signature == BIOS_MEMCONSOLE_V2_MAGIC) {
-			found_v2_header(hdr);
-			return true;
-		}
-	}
-
-	printk(KERN_INFO "BIOS console EBDA structure not found!\n");
-	return false;
+	memconsole_setup(cbmem_console->buffer_body,
+		min(cbmem_console->buffer_cursor, cbmem_console->buffer_size));
+	return true;
 }
+EXPORT_SYMBOL(memconsole_coreboot_init);
 
-static struct dmi_system_id memconsole_dmi_table[] __initdata = {
-	{
-		.ident = "Google Board",
-		.matches = {
-			DMI_MATCH(DMI_BOARD_VENDOR, "Google, Inc."),
-		},
-	},
-	{}
-};
-MODULE_DEVICE_TABLE(dmi, memconsole_dmi_table);
-
-static int __init memconsole_init(void)
+int memconsole_sysfs_init(void)
 {
-	int ret;
-
-	if (!dmi_check_system(memconsole_dmi_table))
-		return -ENODEV;
-
-	if (!found_memconsole())
-		return -ENODEV;
-
 	memconsole_bin_attr.size = memconsole_length;
-
-	ret = sysfs_create_bin_file(firmware_kobj, &memconsole_bin_attr);
-
-	return ret;
+	return sysfs_create_bin_file(firmware_kobj,
+				     &memconsole_bin_attr);
 }
+EXPORT_SYMBOL(memconsole_sysfs_init);
 
-static void __exit memconsole_exit(void)
+void memconsole_exit(void)
 {
 	sysfs_remove_bin_file(firmware_kobj, &memconsole_bin_attr);
 }
-
-module_init(memconsole_init);
-module_exit(memconsole_exit);
-
-MODULE_AUTHOR("Google, Inc.");
-MODULE_LICENSE("GPL");
+EXPORT_SYMBOL(memconsole_exit);
