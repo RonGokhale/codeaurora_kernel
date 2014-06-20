276152ec2b40cf1cdc7441d77779af37882dd04c
Stephen Rothwell <sfr@canb.auug.org.au>
2014-06-20 15:14:40 +1000

    Revert "mm, CMA: clean-up CMA allocation error path"
    This reverts commit f693a5424631ec4004c0422c2a7a53c3af7e61cd.

--- mm/cma.c
+++ mm/cma.c
@@ -285,12 +285,11 @@ struct page *cma_alloc(struct cma *cma, int count, unsigned int align)
 		if (ret == 0) {
 			page = pfn_to_page(pfn);
 			break;
-		}
-
-		cma_clear_bitmap(cma, pfn, count);
-		if (ret != -EBUSY)
+		} else if (ret != -EBUSY) {
+			cma_clear_bitmap(cma, pfn, count);
 			break;
-
+		}
+		cma_clear_bitmap(cma, pfn, count);
 		pr_debug("%s(): memory range at %p is busy, retrying\n",
 			 __func__, pfn_to_page(pfn));
 		/* try again with a bit different memory target */
