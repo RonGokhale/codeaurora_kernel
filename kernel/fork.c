cbeb5f52ce0afe4efa956c519160f9438d6779d4
Vladimir Davydov <vdavydov@parallels.com>
2014-07-17 10:58:35 +1000

    kernel/fork.c: make mm_init_owner static
    It's only used in fork.c:mm_init().
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- kernel/fork.c
+++ kernel/fork.c
@@ -526,6 +526,13 @@ static void mm_init_aio(struct mm_struct *mm)
 #endif
 }
 
+static void mm_init_owner(struct mm_struct *mm, struct task_struct *p)
+{
+#ifdef CONFIG_MEMCG
+	mm->owner = p;
+#endif
+}
+
 static struct mm_struct *mm_init(struct mm_struct *mm, struct task_struct *p)
 {
 	mm->mmap = NULL;
@@ -1097,13 +1104,6 @@ static void rt_mutex_init_task(struct task_struct *p)
 	p->pi_top_task = NULL;
 #endif
 }
-
-#ifdef CONFIG_MEMCG
-void mm_init_owner(struct mm_struct *mm, struct task_struct *p)
-{
-	mm->owner = p;
-}
-#endif /* CONFIG_MEMCG */
 
 /*
  * Initialize POSIX timer handling for a single task.
