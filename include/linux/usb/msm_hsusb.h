6d45a61f684d5473a3c548857c490384878dfced
Mayank Rana <mrana@codeaurora.org>
2014-08-20 13:01:18 -0700

    HSIC_HOST: Wait for BAM Pipe to be empty before suspend
    HSIC_CONS release notification doesn't confirm that all IPA HSIC BAM pipes'
    are empty used for HSIC host mode functionality. In some of regression
    testing, it has been found that HSIC host is being suspended when there is
    data pending in HSIC IPA BAM pipe which causes BAM data stall issue. Hence
    this change adds required support to check all used BAM pipes' state with
    HSIC Host mode before performing suspend and notifying IPA driver to switch
    of IPA clocks.
    
    This change also checks pipe state before resuming root hub in resume
    sequeunce, and would crash device if there is any pending data i.e. data is
    written to HSIC BAM when HSIC host was suspended.
    
    CRs-Fixed: 711785
    Change-Id: I207a99e9cec0516f41f3991d13f673b6ab371736
    Signed-off-by: Mayank Rana <mrana@codeaurora.org>

--- include/linux/usb/msm_hsusb.h
+++ include/linux/usb/msm_hsusb.h
@@ -545,6 +545,7 @@ void msm_bam_set_hsic_host_dev(struct device *dev);
 void msm_bam_wait_for_hsic_prod_granted(void);
 bool msm_bam_hsic_lpm_ok(void);
 void msm_bam_hsic_notify_on_resume(void);
+bool msm_bam_hsic_host_pipe_empty(void);
 #else
 static inline bool msm_bam_lpm_ok(void) { return true; }
 static inline void msm_bam_notify_lpm_resume(void) {}
@@ -552,6 +553,7 @@ static inline void msm_bam_set_hsic_host_dev(struct device *dev) {}
 static inline void msm_bam_wait_for_hsic_prod_granted(void) {}
 static inline bool msm_bam_hsic_lpm_ok(void) { return true; }
 static inline void msm_bam_hsic_notify_on_resume(void) {}
+static inline bool msm_bam_hsic_host_pipe_empty(void) { return true; }
 #endif
 #ifdef CONFIG_USB_CI13XXX_MSM
 void msm_hw_bam_disable(bool bam_disable);
