0983b6d0905ab319732cad3910caa0a587ff8ecc
Paul McQuade <paulmcquad@gmail.com>
2014-10-03 09:29:36 +1000

    mm/filemap.c: remove trailing whitespace
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/filemap.c
+++ mm/filemap.c
@@ -1744,7 +1744,7 @@ EXPORT_SYMBOL(generic_file_read_iter);
 static int page_cache_read(struct file *file, pgoff_t offset)
 {
 	struct address_space *mapping = file->f_mapping;
-	struct page *page; 
+	struct page *page;
 	int ret;
 
 	do {
@@ -1761,7 +1761,7 @@ static int page_cache_read(struct file *file, pgoff_t offset)
 		page_cache_release(page);
 
 	} while (ret == AOP_TRUNCATED_PAGE);
-		
+
 	return ret;
 }
 
