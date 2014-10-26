dca188dfd35db709cb585d5df838bf5dbd994dbe
Vlastimil Babka <vbabka@suse.cz>
2014-10-27 10:04:38 +1100

    mm, cma: drain single zone pcplists
    CMA allocation drains pcplists so that pages can merge back to buddy
    allocator.  Since it operates on a single zone, we can reduce the pcplists
    drain to the single zone, which is now possible.
    
    The change should make CMA allocations faster and not disturbing unrelated
    pcplists anymore.
    
    Signed-off-by: Vlastimil Babka <vbabka@suse.cz>
    Cc: Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Rik van Riel <riel@redhat.com>
    Cc: Yasuaki Ishimatsu <isimatu.yasuaki@jp.fujitsu.com>
    Cc: Zhang Yanfei <zhangyanfei@cn.fujitsu.com>
    Cc: Xishi Qiu <qiuxishi@huawei.com>
    Cc: Vladimir Davydov <vdavydov@parallels.com>
    Cc: Joonsoo Kim <iamjoonsoo.kim@lge.com>
    Cc: Michal Nazarewicz <mina86@mina86.com>
    Cc: Marek Szyprowski <m.szyprowski@samsung.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -6417,7 +6417,7 @@ int alloc_contig_range(unsigned long start, unsigned long end,
 	 */
 
 	lru_add_drain_all();
-	drain_all_pages(NULL);
+	drain_all_pages(cc.zone);
 
 	order = 0;
 	outer_start = start;
