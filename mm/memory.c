19acef79465a694618eebad7bf7310473dc7bd54
Fengwei Yin <yfw.kernel@gmail.com>
2014-10-30 09:34:24 +1100

    mm: smaps should deal with huge zero page exactly same as normal zero page
    We could see following memory info in /proc/xxxx/smaps with THP enabled.
    
      7bea458b3000-7fea458b3000 r--p 00000000 00:13 39989  /dev/zero
      Size:           4294967296 kB
      Rss:            10612736 kB
      Pss:            10612736 kB
      Shared_Clean:          0 kB
      Shared_Dirty:          0 kB
      Private_Clean:  10612736 kB
      Private_Dirty:         0 kB
      Referenced:     10612736 kB
      Anonymous:             0 kB
      AnonHugePages:  10612736 kB
      Swap:                  0 kB
      KernelPageSize:        4 kB
      MMUPageSize:           4 kB
      Locked:                0 kB
      VmFlags: rd mr mw me
    
    which is wrong becuase just huge_zero_page/normal_zero_page is used for
    /dev/zero.  Most of the value should be 0.
    
    This patch detects huge_zero_page (original implementation just detect
    normal_zero_page) and avoids to update the wrong value for huge_zero_page.
    
    Signed-off-by: Fengwei Yin <yfw.kernel@gmail.com>
    Reported-by: Fengguang Wu <fengguang.wu@intel.com>
    Cc: Dave Hansen <dave.hansen@intel.com>
    Cc: Naoya Horiguchi <n-horiguchi@ah.jp.nec.com>
    Cc: "Kirill A. Shutemov" <kirill@shutemov.name>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/memory.c
+++ mm/memory.c
@@ -41,6 +41,7 @@
 #include <linux/kernel_stat.h>
 #include <linux/mm.h>
 #include <linux/hugetlb.h>
+#include <linux/huge_mm.h>
 #include <linux/mman.h>
 #include <linux/swap.h>
 #include <linux/highmem.h>
@@ -786,6 +787,9 @@ check_pfn:
 		print_bad_pte(vma, addr, pte, NULL);
 		return NULL;
 	}
+
+	if (is_huge_zero_pfn(pfn))
+		return NULL;
 
 	/*
 	 * NOTE! We still have PageReserved() pages in the page tables.
