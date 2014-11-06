9926be5fc358c54d50afbabd1000ac3bc91f2d69
Vladimir Davydov <vdavydov@parallels.com>
2014-11-06 11:53:05 +1100

    memcg: only check memcg_kmem_skip_account in __memcg_kmem_get_cache
    __memcg_kmem_get_cache can recurse if it calls kmalloc (which it does if
    the cgroup's kmem cache doesn't exist), because kmalloc may call
    __memcg_kmem_get_cache internally again.  To avoid the recursion, we use
    the task_struct->memcg_kmem_skip_account flag.
    
    However, there's no need in checking the flag in memcg_kmem_newpage_charge
    or memcg_kmem_recharge_slab, because there's no way how these two
    functions could result in recursion, if called from memcg_kmem_get_cache.
    So let's remove the redundant code.
    
    Signed-off-by: Vladimir Davydov <vdavydov@parallels.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Michal Hocko <mhocko@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -2717,34 +2717,6 @@ __memcg_kmem_newpage_charge(gfp_t gfp, struct mem_cgroup **_memcg, int order)
 
 	*_memcg = NULL;
 
-	/*
-	 * Disabling accounting is only relevant for some specific memcg
-	 * internal allocations. Therefore we would initially not have such
-	 * check here, since direct calls to the page allocator that are
-	 * accounted to kmemcg (alloc_kmem_pages and friends) only happen
-	 * outside memcg core. We are mostly concerned with cache allocations,
-	 * and by having this test at memcg_kmem_get_cache, we are already able
-	 * to relay the allocation to the root cache and bypass the memcg cache
-	 * altogether.
-	 *
-	 * There is one exception, though: the SLUB allocator does not create
-	 * large order caches, but rather service large kmallocs directly from
-	 * the page allocator. Therefore, the following sequence when backed by
-	 * the SLUB allocator:
-	 *
-	 *	memcg_stop_kmem_account();
-	 *	kmalloc(<large_number>)
-	 *	memcg_resume_kmem_account();
-	 *
-	 * would effectively ignore the fact that we should skip accounting,
-	 * since it will drive us directly to this function without passing
-	 * through the cache selector memcg_kmem_get_cache. Such large
-	 * allocations are extremely rare but can happen, for instance, for the
-	 * cache arrays. We bring this test here.
-	 */
-	if (current->memcg_kmem_skip_account)
-		return true;
-
 	memcg = get_mem_cgroup_from_mm(current->mm);
 
 	if (!memcg_kmem_is_active(memcg)) {
@@ -2799,9 +2771,6 @@ int __memcg_kmem_recharge_slab(void *obj, gfp_t gfp)
 	struct page *page;
 	int nr_pages;
 	int ret = 0;
-
-	if (current->memcg_kmem_skip_account)
-		goto out;
 
 	page = virt_to_head_page(obj);
 	page_memcg = ACCESS_ONCE(page->mem_cgroup);
