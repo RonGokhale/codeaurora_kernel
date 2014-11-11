809982e1d85f75e43d434f4e4e4f17a40f696a54
Yijing Wang <wangyijing@huawei.com>
2014-11-11 15:22:45 -0700

    PCI/MSI: Add weak pcibios_msi_controller()
    Add pcibios_msi_controller() to get the msi_controller associated with a
    PCI bus.  This is to allow arches to store the msi_controller in the
    arch-specific PCI sysdata.
    
    [bhelgaas: changelog]
    Suggested-by: Bjorn Helgaas <bhelgaas@google.com>
    Signed-off-by: Yijing Wang <wangyijing@huawei.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>

--- drivers/pci/msi.c
+++ drivers/pci/msi.c
@@ -30,9 +30,24 @@ int pci_msi_ignore_mask;
 
 /* Arch hooks */
 
+struct msi_controller * __weak pcibios_msi_controller(struct pci_bus *bus)
+{
+	return NULL;
+}
+
+static struct msi_controller *pci_msi_controller(struct pci_bus *bus)
+{
+	struct msi_controller *msi_ctrl = bus->msi;
+
+	if (msi_ctrl)
+		return msi_ctrl;
+
+	return pcibios_msi_controller(bus);
+}
+
 int __weak arch_setup_msi_irq(struct pci_dev *dev, struct msi_desc *desc)
 {
-	struct msi_controller *chip = dev->bus->msi;
+	struct msi_controller *chip = pci_msi_controller(dev->bus);
 	int err;
 
 	if (!chip || !chip->setup_irq)
