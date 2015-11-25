250f114b51cc27addacc7a6df2457013eea94eec
Nicolas Boichat <drinkcat@chromium.org>
2015-11-25 13:51:07 +0800

    UPSTREAM: mfd: cros ec: Lock the SPI bus while holding chipselect
    cros_ec_cmd_xfer_spi and cros_ec_pkt_xfer_spi generally work like
    this:
     - Pull CS down (active), wait a bit, then send a command
     - Wait for response (multiple requests)
     - Wait a while, pull CS up (inactive)
    
    These operations, individually, lock the SPI bus, but there is
    nothing preventing the SPI framework from interleaving messages
    intended for other devices as the bus is unlocked in between.
    
    This is a problem as the EC expects CS to be held low for the
    whole duration.
    
    Solution: Lock the SPI bus during the whole transaction, to make
    sure that no other messages can be interleaved.
    
    Signed-off-by: Nicolas Boichat <drinkcat@chromium.org>
    Reviewed-by: Gwendal Grignou <gwendal@chromium.org>
    Signed-off-by: Lee Jones <lee.jones@linaro.org>
    (cherry picked from commit 6d6e44a953161e4ce60d1fe805d165d8290ce9e8)
    
    BUG=chrome-os-partner:46094
    TEST=In 2 different terminals:
      while true; do time flashrom -p ec -r /tmp/ec.bin; done
      cras_test_client --capture_file /usr/local/cras_test_client.wav \
              --rate 16000 --num_channels 1 --pin_device 7
      No "EC failed to respond in time" errors, no flashrom errors.
    
    Change-Id: Ibb7093db9c81052fbfa1f1e935d34e635d1720eb
    Reviewed-on: https://chromium-review.googlesource.com/308626
    Commit-Ready: Nicolas Boichat <drinkcat@chromium.org>
    Tested-by: Nicolas Boichat <drinkcat@chromium.org>
    Reviewed-by: Nicolas Boichat <drinkcat@chromium.org>

--- drivers/mfd/cros_ec_spi.c
+++ drivers/mfd/cros_ec_spi.c
@@ -117,7 +117,7 @@ static int terminate_request(struct cros_ec_device *ec_dev)
 	trans.delay_usecs = ec_spi->end_of_msg_delay;
 	spi_message_add_tail(&trans, &msg);
 
-	ret = spi_sync(ec_spi->spi, &msg);
+	ret = spi_sync_locked(ec_spi->spi, &msg);
 
 	/* Reset end-of-response timer */
 	ktime_get_ts(&ts);
@@ -153,7 +153,7 @@ static int receive_n_bytes(struct cros_ec_device *ec_dev, u8 *buf, int n)
 
 	spi_message_init(&msg);
 	spi_message_add_tail(&trans, &msg);
-	ret = spi_sync(ec_spi->spi, &msg);
+	ret = spi_sync_locked(ec_spi->spi, &msg);
 	if (ret < 0)
 		dev_err(ec_dev->dev, "spi transfer failed: %d\n", ret);
 
@@ -399,10 +399,10 @@ static int cros_ec_pkt_xfer_spi(struct cros_ec_device *ec_dev,
 	}
 
 	rx_buf = kzalloc(len, GFP_KERNEL);
-	if (!rx_buf) {
-		ret = -ENOMEM;
-		goto exit;
-	}
+	if (!rx_buf)
+		return -ENOMEM;
+
+	spi_bus_lock(ec_spi->spi->master);
 
 	/*
 	 * Leave a gap between CS assertion and clocking of data to allow the
@@ -422,7 +422,7 @@ static int cros_ec_pkt_xfer_spi(struct cros_ec_device *ec_dev,
 	trans.len = len;
 	trans.cs_change = 1;
 	spi_message_add_tail(&trans, &msg);
-	ret = spi_sync(ec_spi->spi, &msg);
+	ret = spi_sync_locked(ec_spi->spi, &msg);
 
 	/* Get the response */
 	if (!ret) {
@@ -448,6 +448,9 @@ static int cros_ec_pkt_xfer_spi(struct cros_ec_device *ec_dev,
 	}
 
 	final_ret = terminate_request(ec_dev);
+
+	spi_bus_unlock(ec_spi->spi->master);
+
 	if (!ret)
 		ret = final_ret;
 	if (ret < 0)
@@ -536,10 +539,10 @@ static int cros_ec_cmd_xfer_spi(struct cros_ec_device *ec_dev,
 	}
 
 	rx_buf = kzalloc(len, GFP_KERNEL);
-	if (!rx_buf) {
-		ret = -ENOMEM;
-		goto exit;
-	}
+	if (!rx_buf)
+		return -ENOMEM;
+
+	spi_bus_lock(ec_spi->spi->master);
 
 	/* Transmit phase - send our message */
 	debug_packet(ec_dev->dev, "out", ec_dev->dout, len);
@@ -550,7 +553,7 @@ static int cros_ec_cmd_xfer_spi(struct cros_ec_device *ec_dev,
 	trans.cs_change = 1;
 	spi_message_init(&msg);
 	spi_message_add_tail(&trans, &msg);
-	ret = spi_sync(ec_spi->spi, &msg);
+	ret = spi_sync_locked(ec_spi->spi, &msg);
 
 	/* Get the response */
 	if (!ret) {
@@ -576,6 +579,9 @@ static int cros_ec_cmd_xfer_spi(struct cros_ec_device *ec_dev,
 	}
 
 	final_ret = terminate_request(ec_dev);
+
+	spi_bus_unlock(ec_spi->spi->master);
+
 	if (!ret)
 		ret = final_ret;
 	if (ret < 0)
